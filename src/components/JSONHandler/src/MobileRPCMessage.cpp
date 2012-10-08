c81000da4aa946de0ce66958b21feaa93629671e
Polina Vyshnevska <pvyshnevska@luxoft.com>
2012-10-08 06:53:15 -0700

    Continued work on RPC Messages interface

--- src/components/JSONHandler/src/MobileRPCMessage.cpp
+++ src/components/JSONHandler/src/MobileRPCMessage.cpp
@@ -15,67 +15,30 @@ MobileRPCMessage::MobileRPCMessage( unsigned int protocolVersion,
     MessageType messageType, std::string functionName )
 {
     MobileRPCMessage(protocolVersion, messageType, 
-        0, Parameters(), functionName, 0, 0);
+        0, functionName, 0, 0);
 }
 
 MobileRPCMessage::MobileRPCMessage( unsigned int protocolVersion, 
     MessageType messageType, unsigned int functionID )
 {
+    
     MobileRPCMessage(protocolVersion, messageType, 
-        0, Parameters(), "", functionID, 0);
+        0, "", functionID, 0);
 }
 
 MobileRPCMessage::MobileRPCMessage( unsigned int protocolVersion, MessageType messageType, 
-        unsigned int correlationID, Parameters params, std::string functionName,
+        unsigned int correlationID, std::string functionName,
         unsigned int functionID, unsigned int messageSize )
 :mProtocolVersion( protocolVersion )
 ,mMessageType( messageType )
 ,mCorrelationID( correlationID )
-,mParameters( params )
 ,mFunctionName( functionName )
 ,mFunctionID( functionID )
 ,mJSONMessageSize( messageSize )
 { }
 
-MobileRPCMessage::~MobileRPCMessage() {}
-
-void MobileRPCMessage::setParameters( Parameters params )
-{
-    mParameters.clear();
-    mParameters = params;
-}
-    
-void MobileRPCMessage::addParameter( std::string key, std::string value )
-{
-    mParameters[ key ] = value;
-}
-    
-void MobileRPCMessage::changeParameter( std::string key, std::string newValue )
-{
-    MobileRPCMessage::Parameters::iterator it = mParameters.find( key );
-    if ( it != mParameters.end() )
-    {
-        it->second = newValue;
-    }
-}
-
-MobileRPCMessage::Parameters MobileRPCMessage::getParameters() const
-{
-    return mParameters;
-}
-    
-std::string MobileRPCMessage::getParameter( std::string key ) const
-{
-    MobileRPCMessage::Parameters::const_iterator it = mParameters.find( key );
-    if ( it == mParameters.end() ) 
-    {
-        return "";
-    } 
-    else
-    {
-        return it->second;
-    }
-}
+MobileRPCMessage::~MobileRPCMessage() 
+{}
 
 unsigned int MobileRPCMessage::getProtocolVersion() const
 {
@@ -125,4 +88,15 @@ void MobileRPCMessage::setJSONMessageSize( unsigned int messageSize )
 void MobileRPCMessage::setCorrelationID( unsigned int correlationID )
 {
     mCorrelationID = correlationID;
+}
+
+
+void MobileRPCMessage::setOriginalString( std::string originalString )
+{
+    mOriginalString = originalString;
+}
+    
+std::string MobileRPCMessage::getOriginalString() const
+{
+    return mOriginalString;
 }
--- src/components/JSONHandler/src/MobileRPCRequest.cpp
+++ src/components/JSONHandler/src/MobileRPCRequest.cpp
@@ -2,9 +2,17 @@
 #include "JSONHandler/MobileRPCMessage.h"
 #include "JSONHandler/MobileRPCRequest.h"
 
-MobileRPCRequest::MobileRPCRequest( unsigned int protocolVersion, unsigned int correlationID )
+MobileRPCRequest::MobileRPCRequest( unsigned int protocolVersion )
 :MobileRPCMessage( protocolVersion, REQUEST )
 {}
 
+MobileRPCRequest::MobileRPCRequest( unsigned int protocolVersion, std::string functionName )
+:MobileRPCMessage( protocolVersion, REQUEST, functionName )
+{}
+    
+MobileRPCRequest::MobileRPCRequest( unsigned int protocolVersion, unsigned int functionID )
+:MobileRPCMessage( protocolVersion, REQUEST, functionID )
+{}
+ 
 MobileRPCRequest::~MobileRPCRequest() 
 {}
