963b4104327308a6f54a9e193cddc39cde0a15de
Alexandr Kara <AKara@luxoft.com>
2012-10-19 11:41:17 -0400

    Integrated logger.

---
+++ src/thirdPartyLibs/logger/include/Logger.hpp
@@ -0,0 +1,16 @@
+/* 
+ * 
+ * 
+ */
+
+
+#ifndef LOGGER_HPP_
+#define LOGGER_HPP_
+
+  #include "log4cplus/configurator.h"
+  #include "log4cplus/consoleappender.h"
+  #include "log4cplus/helpers/stringhelper.h"
+  using namespace log4cplus;
+  using namespace log4cplus::helpers;
+
+#endif /* LOGGER_HPP_ */
