645cf7828c8a44da3183032a46bc41f629d418a7
Volodymyr Salo <vsalo@luxoft.com>
2012-10-22 14:44:14 +0300

    Updated with a new log4cplus

---
+++ src/thirdPartyLibs/logger/log4cplus-1.1.0/tests/propertyconfig_test/main.cxx
@@ -0,0 +1,39 @@
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/configurator.h>
+#include <log4cplus/helpers/loglog.h>
+#include <log4cplus/helpers/stringhelper.h>
+#include <log4cplus/loggingmacros.h>
+
+
+using namespace std;
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+
+
+
+int
+main()
+{
+    tcout << LOG4CPLUS_TEXT("Entering main()...") << endl;
+    LogLog::getLogLog()->setInternalDebugging(true);
+    Logger root = Logger::getRoot();
+    try {
+        PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("log4cplus.properties"));
+        Logger fileCat = Logger::getInstance(LOG4CPLUS_TEXT("filelogger"));
+
+	LOG4CPLUS_WARN(root, LOG4CPLUS_TEXT("Testing...."));
+
+	LOG4CPLUS_WARN(root, LOG4CPLUS_TEXT("Writing messages to log...."));
+        for(int i=0; i<10000; ++i) {
+	    LOG4CPLUS_WARN(fileCat, LOG4CPLUS_TEXT("This is a WARNING...") << i);
+        }
+    }
+    catch(...) {
+        tcout << LOG4CPLUS_TEXT("Exception...") << endl;
+	LOG4CPLUS_FATAL(root, LOG4CPLUS_TEXT("Exception occured..."));
+    }
+
+    tcout << LOG4CPLUS_TEXT("Exiting main()...") << endl;
+    return 0;
+}
+
