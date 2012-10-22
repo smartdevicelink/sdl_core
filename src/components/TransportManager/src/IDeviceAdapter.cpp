6a76ff3ba3d9b09f0e27b9691f8570bf25220b43
Yevgeniy Koropatnyuk <YKoropatnyuk@luxoft.com>
2012-10-22 15:56:39 +0300

    Interface for device adapter

---
+++ src/components/TransportManager/src/IDeviceAdapter.cpp
@@ -0,0 +1,5 @@
+#include "IDeviceAdapter.hpp"
+
+AppLink::TransportManager::IDeviceAdapter::~IDeviceAdapter(void)
+{
+}
---
+++ src/components/TransportManager/src/IDeviceAdapter.hpp
@@ -0,0 +1,53 @@
+#ifndef __TRANSPORTMANAGER_IDEVICEADAPTER_HPP__
+#define __TRANSPORTMANAGER_IDEVICEADAPTER_HPP__
+
+#include <vector>
+
+#include "TransportManager/ITransportManager.hpp"
+#include "SInternalDeviceInfo.hpp"
+
+namespace AppLink
+{
+    namespace TransportManager
+    {
+        /**
+         * @brief Interface for device adapter.
+         **/
+        class IDeviceAdapter
+        {
+        public:
+            /**
+             * @brief Destructor.
+             **/
+            virtual ~IDeviceAdapter(void);
+
+            /**
+             * @brief Get list of available devices.
+             *
+             * @param DeviceList Reference to a variable that will
+             *                   receive device list.
+             **/
+            virtual void getDeviceList(std::vector<SInternalDeviceInfo> & DeviceList) const = 0;
+
+            /**
+             * @brief Get type of devices handled by this device adapter.
+             *
+             * @return Device type.
+             **/
+            virtual EDeviceType getDeviceType(void) const = 0;
+
+            /**
+             * @brief Send frame.
+             *
+             * @param ConnectionHandle Connection handle.
+             * @param Data Frame payload data.
+             *
+             * @return Frame sequence number. May be used to identify
+             *         this frame when send result callback is received.
+             **/
+            virtual int sendFrame(tConnectionHandle ConnectionHandle, const Blob & Data) = 0;
+        };
+    }
+}
+
+#endif
