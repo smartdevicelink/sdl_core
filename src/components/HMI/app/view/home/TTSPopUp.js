f3a70c0e7402bef023e398d6c3ffe376ab4cec6a
Andriy Melnik <amelnik@luxoft.com>
2012-10-23 12:07:05 +0300

    Implemented new message displaying from JSON response

---
+++ src/components/HMI/app/view/home/TTSPopUp.js
@@ -0,0 +1,53 @@
+/**
+ * @name MFT.TTSPopUp
+ * 
+ * @desc TTSPopUp module visual representation
+ * 
+ * @category	View
+ * @filesource	app/view/home/TTSPopUp.js
+ * @version		2.0
+ *
+ * @author		Andriy Melnik
+ */
+
+MFT.TTSPopUp = Em.ContainerView.create({
+
+	elementId:			'TTSPopUp',
+
+	classNames:			'TTSPopUp',
+
+	classNameBindings:	['received'],
+
+	childViews: [
+		'popUp',
+		'message'
+	],
+
+	content:			'Messaage',
+
+	received: 			false,
+
+	popUp : Em.View.extend({
+
+		elementId:			'popUp',
+
+		classNames:			'popUp',
+	}),
+
+	message : MFT.Label.extend({
+
+		elementId:			'message',
+
+		classNames:			'message',
+
+		contentBinding:		'parentView.content'
+	}),
+
+	receiveMessage: function(msg){
+		var self = this;
+
+		this.set('content', msg);
+		this.set('received', true);
+		setTimeout(function(){self.set('received', false);}, 2000);
+	}
+});
---
+++ src/components/HMI/ffw/UIBackendRPC.js
@@ -0,0 +1,103 @@
+/*
+ * Reference implementation of UIBackend component.
+ * 
+ * Interface to get or set some essential information from OS.
+ * Since web application is not able to access some OS feature through WebKit
+ * it is necessary to use communication with native code to get some or set values 
+ * and execute some functions. 
+ * 
+ *
+ * @author Andriy Melnik
+ */
+
+FFW.UIBackend = FFW.RPCObserver.create({
+		
+	/*
+     *	access to basic RPC functionality
+ 	 */		
+	 client:		FFW.RPCClient.create({ componentName: "UI" }),
+	
+	
+	/*
+   	 * connect to RPC bus
+ 	 */
+	connect: function() {
+		// to be removed
+		//this.stubGetValues();
+		
+		this.client.connect(this);
+	},
+
+	/*
+   	 * disconnect from RPC bus
+ 	 */
+	disconnect: function() {
+		this.client.disconnect();
+	},
+
+	/*
+     * Client is registered - we can send request starting from this point of time
+ 	 */	
+	onRPCRegistered: function () {
+		Em.Logger.log("FFW.UIBackend.onRPCRegistered");
+		this._super();
+	},
+	
+	/*
+     * Client is unregistered - no more requests
+ 	 */	
+	onRPCUnregistered: function () {
+		Em.Logger.log("FFW.UIBackend.onRPCUnregistered");
+		this._super();
+	},
+
+	/*
+	 * Client disconnected.
+	 */
+	onRPCDisconnected: function() {
+
+	},
+
+	/*
+     * when result is received from RPC component this function is called
+	 * It is the propriate place to check results of reuqest execution
+	 * Please use previously store reuqestID to determine to which request repsonse belongs to
+ 	 */	
+	onRPCResult: function(response) {
+		Em.Logger.log("FFW.UIBackend.onRPCResult");
+		this._super();
+	 },
+	
+	/*
+	 * handle RPC erros here
+ 	 */	
+	onRPCError: function(error) {
+		Em.Logger.log("FFW.UIBackend.onRPCError");
+		this._super();
+	},
+
+	/*
+	 * handle RPC notifications here 
+ 	 */	
+	onRPCNotification: function(notification) {
+		Em.Logger.log("FFW.UIBackend.onRPCNotification");
+		this._super();
+	},
+	
+	/*
+	 * handle RPC requests here
+ 	 */	
+	onRPCRequest: function(request) {
+		Em.Logger.log("FFW.UIBackend.onRPCRequest");
+		this._super();
+/*
+		if (request.method == "UIBackendClient.onFullScreenChanged") {
+			this.resizeVideo = true;
+			this.FullScreenRequestId = request.id;
+			Em.Logger.log("resizeVideo = " + this.resizeVideo);
+			Em.Logger.log("FullScreenRequestId = " + this.FullScreenRequestId);
+			this.set("isFullScreen", request.params.isFullScreen);
+		}
+*/
+	}
+})
