225e07ea8b35081ce7844792e33f193ce53f4eab
Yevgeniy Koropatnyuk <YKoropatnyuk@luxoft.com>
2012-10-25 11:53:30 +0300

    Bluetooth device discovery.

--- src/components/TransportManager/src/CBluetoothAdapter.cpp
+++ src/components/TransportManager/src/CBluetoothAdapter.cpp
@@ -1,26 +1,94 @@
+#include <errno.h>
+#include <iomanip>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <bluetooth/sdp.h>
+#include <bluetooth/sdp_lib.h>
+
+#include "IDeviceAdapterListener.hpp"
 #include "CBluetoothAdapter.hpp"
+
+NsAppLink::NsTransportManager::CBluetoothAdapter::SBluetoothDevice::SBluetoothDevice(void):
+mAddress(),
+mName(),
+mAppLinkRFCOMMPorts()
+{
+}
+
+NsAppLink::NsTransportManager::CBluetoothAdapter::SBluetoothDevice::SBluetoothDevice(const NsAppLink::NsTransportManager::CBluetoothAdapter::SBluetoothDevice & Other):
+mAddress(Other.mAddress),
+mName(Other.mName),
+mAppLinkRFCOMMPorts(Other.mAppLinkRFCOMMPorts)
+{
+}
+
+NsAppLink::NsTransportManager::CBluetoothAdapter::SBluetoothDevice::SBluetoothDevice(const bdaddr_t & Address, const char * Name, const std::vector<uint8_t> & AppLinkRFCOMMPorts):
+mAddress(Address),
+mName(Name),
+mAppLinkRFCOMMPorts(AppLinkRFCOMMPorts)
+{
+}
 
 NsAppLink::NsTransportManager::CBluetoothAdapter::CBluetoothAdapter(NsAppLink::NsTransportManager::IDeviceAdapterListener & Listener, IHandleGenerator & HandleGenerator):
 mListener(Listener),
 mHandleGenerator(HandleGenerator),
-mLogger(log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("BluetoothAdapter")))
+mLogger(log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("BluetoothAdapter"))),
+mShutdownFlag(false),
+mDeviceDiscoveryThread(),
+mDeviceDiscoveryThreadStarted(false),
+mDevices(),
+mDevicesMutex()
 {
     LOG4CPLUS_INFO_EXT(mLogger, "BluetoothAdapter constructed");
+
+    pthread_mutex_init(&mDevicesMutex, 0);
 }
 
 NsAppLink::NsTransportManager::CBluetoothAdapter::~CBluetoothAdapter(void)
 {
     LOG4CPLUS_INFO_EXT(mLogger, "BluetoothAdapter destructor");
+
+    mShutdownFlag = true;
+
+    if (true == mDeviceDiscoveryThreadStarted)
+    {
+        LOG4CPLUS_INFO_EXT(mLogger, "Waiting for discovery thread termination");
+        pthread_join(mDeviceDiscoveryThread, 0);
+        LOG4CPLUS_INFO_EXT(mLogger, "Discovery thread terminated");
+    }
+
+    pthread_mutex_destroy(&mDevicesMutex);
 }
 
 void NsAppLink::NsTransportManager::CBluetoothAdapter::run(void)
 {
-    LOG4CPLUS_ERROR_EXT(mLogger, "Not implemented");
+    LOG4CPLUS_INFO_EXT(mLogger, "Initializing bluetooth adapter");
+
+    int errorCode = pthread_create(&mDeviceDiscoveryThread, 0, &deviceDiscoveryThreadStartRoutine, this);
+
+    if (0 == errorCode)
+    {
+        mDeviceDiscoveryThreadStarted = true;
+        LOG4CPLUS_INFO_EXT(mLogger, "Device discovery thread started");
+    }
+    else
+    {
+        LOG4CPLUS_ERROR_EXT(mLogger, "Device discovery thread start failed, error code " << errorCode);
+    }
 }
 
 void NsAppLink::NsTransportManager::CBluetoothAdapter::getDeviceList(NsAppLink::NsTransportManager::tInternalDeviceList & DeviceList) const
 {
-    LOG4CPLUS_ERROR_EXT(mLogger, "Not implemented");
+    pthread_mutex_lock(&mDevicesMutex);
+
+    for (tBluetoothDevicesMap::const_iterator di = mDevices.begin(); di != mDevices.end(); ++di)
+    {
+        DeviceList.push_back(SInternalDeviceInfo(di->first, di->second.mName));
+    }
+
+    pthread_mutex_unlock(&mDevicesMutex);
 }
 
 NsAppLink::NsTransportManager::EDeviceType NsAppLink::NsTransportManager::CBluetoothAdapter::getDeviceType(void) const
@@ -33,4 +101,244 @@ int NsAppLink::NsTransportManager::CBluetoothAdapter::sendFrame(NsAppLink::NsTra
     LOG4CPLUS_ERROR_EXT(mLogger, "Not implemented");
 
     return -1;
+}
+
+void * NsAppLink::NsTransportManager::CBluetoothAdapter::deviceDiscoveryThreadStartRoutine(void * Data)
+{
+    if (0 != Data)
+    {
+        static_cast<CBluetoothAdapter*>(Data)->deviceDiscoveryThread();
+    }
+
+    return 0;
+}
+
+void NsAppLink::NsTransportManager::CBluetoothAdapter::deviceDiscoveryThread(void)
+{
+    LOG4CPLUS_INFO_EXT(mLogger, "Device discovery thread initialized");
+
+    const size_t maxDevices = 256u;
+    const uint8_t inquiryTime = 8u; // Time unit is 1.28 seconds
+    inquiry_info * inquiryInfoList = new inquiry_info[maxDevices];
+    uint8_t appLinkServiceUUIDData[] = {0x93, 0x6D, 0xA0, 0x1F, 0x9A, 0xBD, 0x4D, 0x9D, 0x80, 0xC7, 0x02, 0xAF, 0x85, 0xC8, 0x22, 0xA8};
+    uuid_t appLinkServiceUUID;
+    sdp_uuid128_create(&appLinkServiceUUID, appLinkServiceUUIDData);
+
+    while (false == mShutdownFlag)
+    {
+        int deviceID = hci_get_route(0);
+
+        if (deviceID >= 0)
+        {
+            bdaddr_t adapterAddress = {{0, 0, 0, 0, 0, 0}};
+
+            if (0 != hci_devba(deviceID, &adapterAddress))
+            {
+                LOG4CPLUS_ERROR_EXT(mLogger, "Address of bluetooth adapter " << deviceID << " can't be determined, error code " << errno << " (" << strerror(errno) << ")");
+            }
+            else
+            {
+                int deviceHandle = hci_open_dev(deviceID);
+
+                if (deviceHandle >= 0)
+                {
+                    char adapterAddressString[16];
+                    ba2str(&adapterAddress, adapterAddressString);
+                    LOG4CPLUS_INFO_EXT(mLogger, "Starting hci_inquiry on device " << deviceID << " (" << adapterAddressString << ")");
+
+                    int numberOfDevices = hci_inquiry(deviceID, inquiryTime, maxDevices, 0, &inquiryInfoList, IREQ_CACHE_FLUSH);
+                    std::vector<SBluetoothDevice> discoveredDevices;
+
+                    if (numberOfDevices >= 0)
+                    {
+                        LOG4CPLUS_INFO_EXT(mLogger, "hci_inquiry: found " << numberOfDevices << " devices");
+
+                        for (int i = 0; i < numberOfDevices; ++i)
+                        {
+                            bdaddr_t anyAddress = {{0, 0, 0, 0, 0, 0}};
+                            std::vector<uint8_t> appLinkRFCOMMPorts;
+                            sdp_session_t * sdpSession = sdp_connect(&anyAddress, &inquiryInfoList[i].bdaddr, SDP_RETRY_IF_BUSY);
+
+                            if (0 != sdpSession)
+                            {
+                                sdp_list_t * searchList = sdp_list_append(0, &appLinkServiceUUID);
+                                uint32_t range = 0x0000ffff;
+                                sdp_list_t * attrList = sdp_list_append(0, &range);
+                                sdp_list_t * responseList = 0;
+
+                                if (0 == sdp_service_search_attr_req(sdpSession, searchList, SDP_ATTR_REQ_RANGE, attrList, &responseList))
+                                {
+                                    for (sdp_list_t * r = responseList; 0 != r; r = r->next)
+                                    {
+                                        sdp_record_t * sdpRecord = static_cast<sdp_record_t*>(r->data);
+                                        sdp_list_t * protoList = 0;
+
+                                        if (0 == sdp_get_access_protos(sdpRecord, &protoList))
+                                        {
+                                            for (sdp_list_t * p = protoList; 0 != p; p = p->next)
+                                            {
+                                                sdp_list_t * pdsList = static_cast<sdp_list_t*>(p->data);
+
+                                                for (sdp_list_t * pds = pdsList; 0 != pds; pds = pds->next)
+                                                {
+                                                    sdp_data_t * sdpData = static_cast<sdp_data_t*>(pds->data);
+                                                    int proto = 0;
+
+                                                    for (sdp_data_t * d = sdpData; 0 != d; d = d->next)
+                                                    {
+                                                        switch (d->dtd)
+                                                        {
+                                                            case SDP_UUID16:
+                                                            case SDP_UUID32:
+                                                            case SDP_UUID128:
+                                                                proto = sdp_uuid_to_proto(&d->val.uuid);
+                                                                break;
+
+                                                            case SDP_UINT8:
+                                                                if (RFCOMM_UUID == proto)
+                                                                {
+                                                                    appLinkRFCOMMPorts.push_back(d->val.uint8);
+                                                                }
+                                                                break;
+                                                        }
+                                                    }
+                                                }
+
+                                                sdp_list_free(pdsList, 0);
+                                            }
+
+                                            sdp_list_free(protoList, 0);
+                                        }
+                                    }
+
+                                    if (false == appLinkRFCOMMPorts.empty())
+                                    {
+                                        char deviceAddressString[16];
+                                        char deviceName[256];
+                                        ba2str(&inquiryInfoList[i].bdaddr, deviceAddressString);
+                                        std::stringstream rfcommPortsString;
+
+                                        for (std::vector<uint8_t>::const_iterator pi = appLinkRFCOMMPorts.begin(); pi != appLinkRFCOMMPorts.end(); ++pi)
+                                        {
+                                            if (pi != appLinkRFCOMMPorts.begin())
+                                            {
+                                                rfcommPortsString << ", ";
+                                            }
+
+                                            rfcommPortsString << static_cast<int>(*pi);
+                                        }
+
+                                        if (0 != hci_read_remote_name(deviceHandle, &inquiryInfoList[i].bdaddr, sizeof(deviceName) / sizeof(deviceName[0]), deviceName, 0))
+                                        {
+                                            LOG4CPLUS_WARN_EXT(mLogger, "hci_read_remote_name failed, error code " << errno << " (" << strerror(errno) << ")");
+                                            strncpy(deviceName, deviceAddressString, sizeof(deviceName) / sizeof(deviceName[0]));
+                                        }
+
+                                        LOG4CPLUS_INFO_EXT(mLogger, "hci_inquiry: device " << i << ": " << deviceAddressString << " (\"" << deviceName << "\") service found at port(s): " << rfcommPortsString.str().c_str());
+
+                                        discoveredDevices.push_back(SBluetoothDevice(inquiryInfoList[i].bdaddr, deviceName, appLinkRFCOMMPorts));
+                                    }
+                                }
+
+                                sdp_list_free(searchList, 0);
+                                sdp_list_free(attrList, 0);
+                                sdp_list_free(responseList, 0);
+                                sdp_close(sdpSession);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        LOG4CPLUS_ERROR_EXT(mLogger, "hci_inquiry failed");
+                    }
+
+                    tBluetoothDevicesMap newDevices;
+                    pthread_mutex_lock(&mDevicesMutex);
+                    for (std::vector<SBluetoothDevice>::const_iterator discoveredDeviceIterator = discoveredDevices.begin(); discoveredDeviceIterator != discoveredDevices.end(); ++discoveredDeviceIterator)
+                    {
+                        const SBluetoothDevice & discoveredDevice = *discoveredDeviceIterator;
+                        tDeviceHandle deviceHandle = InvalidDeviceHandle;
+
+                        for (tBluetoothDevicesMap::iterator deviceIterator = mDevices.begin(); deviceIterator != mDevices.end(); ++deviceIterator)
+                        {
+                            SBluetoothDevice & exisingDevice = deviceIterator->second;
+
+                            if (0 == memcmp(&exisingDevice.mAddress, &discoveredDevice.mAddress, sizeof(bdaddr_t)))
+                            {
+                                deviceHandle = deviceIterator->first;
+
+                                LOG4CPLUS_INFO_EXT(mLogger, "Updating exising device " << deviceHandle << " (\"" << discoveredDevice.mName << "\")");
+                            }
+                        }
+
+                        if (InvalidDeviceHandle == deviceHandle)
+                        {
+                            deviceHandle = mHandleGenerator.generateNewDeviceHandle();
+
+                            LOG4CPLUS_INFO_EXT(mLogger, "Adding new device " << deviceHandle << " (\"" << discoveredDevice.mName << "\")");
+                        }
+
+                        newDevices[deviceHandle] = discoveredDevice;
+                    }
+
+                    bool needUpdateClientDeviceList = (newDevices.size() != mDevices.size());
+
+                    if (false == needUpdateClientDeviceList)
+                    {
+                        for (tBluetoothDevicesMap::const_iterator di = mDevices.begin(); di != mDevices.end(); ++di)
+                        {
+                            tBluetoothDevicesMap::const_iterator ndi = newDevices.find(di->first);
+                            bool matchingDeviceFound = (ndi != newDevices.end());
+                            if (true == matchingDeviceFound)
+                            {
+                                matchingDeviceFound = (di->second.mName == ndi->second.mName);
+                            }
+
+                            if (false == matchingDeviceFound)
+                            {
+                                needUpdateClientDeviceList = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    mDevices = newDevices;
+
+                    LOG4CPLUS_INFO_EXT(mLogger, "Discovered " << mDevices.size() << " device" << ((1u == mDevices.size()) ? "" : "s") << " with AppLink service. New devices map:");
+
+                    for (tBluetoothDevicesMap::const_iterator deviceIterator = mDevices.begin(); deviceIterator != mDevices.end(); ++deviceIterator)
+                    {
+                        char deviceAddressString[16];
+                        ba2str(&deviceIterator->second.mAddress, deviceAddressString);
+                        LOG4CPLUS_INFO_EXT(mLogger, std::setw(10) << deviceIterator->first << std::setw(0) << ": " << deviceAddressString << ", " << deviceIterator->second.mName.c_str());
+                    }
+
+                    pthread_mutex_unlock(&mDevicesMutex);
+
+                    close(deviceHandle);
+
+                    if (true == needUpdateClientDeviceList)
+                    {
+                        LOG4CPLUS_INFO_EXT(mLogger, "Client device list updated");
+
+                        tInternalDeviceList clientDeviceList;
+                        getDeviceList(clientDeviceList);
+                        mListener.onDeviceListUpdated(this, clientDeviceList);
+                    }
+                }
+                else
+                {
+                    LOG4CPLUS_ERROR_EXT(mLogger, "Failed to open bluetooth adapter device");
+                }
+            }
+        }
+        else
+        {
+            LOG4CPLUS_ERROR_EXT(mLogger, "No bluetooth adapter device found");
+        }
+    }
+
+    delete [] inquiryInfoList;
+
+    LOG4CPLUS_INFO_EXT(mLogger, "Device discovery thread finished");
 }
--- src/components/TransportManager/src/CBluetoothAdapter.hpp
+++ src/components/TransportManager/src/CBluetoothAdapter.hpp
@@ -1,7 +1,12 @@
 #ifndef __TRANSPORTMANAGER_CBLUETOOTHADAPTER_HPP__
 #define __TRANSPORTMANAGER_CBLUETOOTHADAPTER_HPP__
 
+#include <bluetooth/bluetooth.h>
+#include <pthread.h>
+
 #include "Logger.hpp"
+
+#include "TransportManager/SDeviceInfo.hpp"
 
 #include "IDeviceAdapter.hpp"
 #include "IHandleGenerator.hpp"
@@ -66,6 +71,67 @@ namespace NsAppLink
 
         private:
             /**
+             * @brief Internal structure describing bluetooth device.
+             **/
+            struct SBluetoothDevice
+            {
+                /**
+                 * @brief Default constructor.
+                 **/
+                SBluetoothDevice(void);
+
+                /**
+                 * @brief Copy constructor.
+                 *
+                 * @param Other Bluetooth device that must be copied.
+                 **/
+                SBluetoothDevice(const SBluetoothDevice & Other);
+
+                /**
+                 * @brief Constructor.
+                 *
+                 * @param Address Bluetooth address.
+                 * @param Name User-friendly device name.
+                 * @param AppLinkRFCOMMPorts List of RFCOMM ports where AppLink service has been discovered.
+                 **/
+                SBluetoothDevice(const bdaddr_t & Address, const char * Name, const std::vector<uint8_t> & AppLinkRFCOMMPorts);
+
+                /**
+                 * @brief Device bluetooth address.
+                 **/
+                bdaddr_t mAddress;
+
+                /**
+                 * @brief Device user-friendly name.
+                 **/
+                std::string mName;
+
+                /**
+                 * @brief List of RFCOMM ports where AppLink service has been discovered.
+                 **/
+                std::vector<uint8_t> mAppLinkRFCOMMPorts;
+            };
+
+            /**
+             * @brief Bluetooth devices map.
+             **/
+            typedef std::map<tDeviceHandle, SBluetoothDevice> tBluetoothDevicesMap;
+
+            /**
+             * @brief Start routine for device discovery thread.
+             *
+             * @param Data Must be pointer to CBluetoothAdapter instance.
+             *
+             * @return Thread return value.
+             **/
+            static void * deviceDiscoveryThreadStartRoutine(void * Data);
+
+            /**
+             * @brief Device discovery thread.
+             **/
+            void deviceDiscoveryThread(void);
+
+            /**
              * @brief Listener for device adapter notifications.
              **/
             IDeviceAdapterListener & mListener;
@@ -79,6 +145,37 @@ namespace NsAppLink
              * @brief Logger.
              **/
             const log4cplus::Logger mLogger;
+
+            /**
+             * @brief Shutdown flag.
+             *
+             * This flag is set to true on shutdown to inform all bluetooth adapter
+             * threads that bluetooth adapter shutdown is in progress. After setting
+             * this flag bluetooth adapter waits until all its threads are terminated.
+             **/
+            bool mShutdownFlag;
+
+            /**
+             * @brief ID of device discovery thread.
+             **/
+            pthread_t mDeviceDiscoveryThread;
+
+            /**
+             * @brief Flag indicating whether the device discovery thread has been started successfully.
+             **/
+            bool mDeviceDiscoveryThreadStarted;
+
+            /**
+             * @brief Map of device handle to bluetooth device.
+             *
+             * This map contains all currently available bluetooth devices.
+             **/
+            tBluetoothDevicesMap mDevices;
+
+            /**
+             * @brief Mutex restricting access to device map.
+             **/
+            mutable pthread_mutex_t mDevicesMutex;
         };
     }
 }
