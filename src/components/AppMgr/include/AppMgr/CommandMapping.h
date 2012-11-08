8b1c47efae17cab81e93bc2e0dd6c4578d6a4c1a
Volodymyr Salo <vsalo@luxoft.com>
2012-11-08 22:50:14 +0200

    Refactored CommandMapping so the key would be a tuple of command type and command id, making us able to specify one command for different types; DeleteCommand now deletes both VR and UI commands in case the respective AddCommand created both of them;

--- src/components/AppMgr/include/AppMgr/CommandMapping.h
+++ src/components/AppMgr/include/AppMgr/CommandMapping.h
@@ -2,6 +2,8 @@
 #define COMMANDMAPPING_H
 
 #include <map>
+#include <tuple>
+#include <vector>
 
 namespace log4cplus
 {
@@ -20,8 +22,10 @@ public:
     {
         UNDEFINED = -1,
         UI        = 0,
+        FIRST     = UI,
         VR        = 1,
-        TTS       = 2
+        TTS       = 2,
+        LAST      = TTS
     } Type;
 
     CommandType();
@@ -29,25 +33,34 @@ public:
     CommandType(const Type& type);
     bool operator==(const Type& type) const;
     bool operator==(const CommandType& type) const;
+    bool operator<(const Type& type) const;
+    bool operator<(const CommandType& type) const;
+    bool operator>(const Type& type) const;
+    bool operator>(const CommandType& type) const;
+    bool operator!=(const Type& type) const;
+    bool operator!=(const CommandType& type) const;
+    CommandType& operator++ ();
+    CommandType operator++ (int);
     const Type& getType() const;
 
 private:
     Type mType;
 };
 
-typedef std::pair<CommandType, RegistryItem*> Command;
-typedef std::map<unsigned int, Command> CommandMap;
-typedef std::pair<unsigned int, Command> CommandMapItem;
+typedef std::tuple<unsigned int, CommandType> CommandKey;
+typedef std::map<CommandKey, RegistryItem*> CommandMap;
+typedef std::pair<CommandKey, RegistryItem*> CommandMapItem;
+typedef std::vector<CommandType> CommandTypes;
 
 class CommandMapping
 {
 public:
     CommandMapping();
     void addCommand( unsigned int commandId, CommandType type, RegistryItem* app );
-    void removeCommand(unsigned int commandId);
+    void removeCommand(unsigned int commandId, CommandType type);
     void removeItem( RegistryItem* app );
-    CommandType getType( unsigned int commandId ) const;
-    RegistryItem *findRegistryItemAssignedToCommand(unsigned int commandId) const;
+    void getTypes(unsigned int commandId, CommandTypes& types ) const;
+    RegistryItem *findRegistryItemAssignedToCommand(unsigned int commandId, CommandType type) const;
 
 private:
     CommandMapping(const CommandMapping&);
