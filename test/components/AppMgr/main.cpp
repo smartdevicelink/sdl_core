deaa9f630afadd34ca66424aceb7423822163b82
Volodymyr Salo <vsalo@luxoft.com>
2012-11-09 11:24:45 +0200

    Started to implement automatic tests

--- test/components/AppMgr/main.cpp
+++ test/components/AppMgr/main.cpp
@@ -1,7 +1,42 @@
 #include "AppMgr/AppMgr.h"
+#include "LoggerHelper.hpp"
+#include "ProtocolHandler.hpp"
+
+#include "JSONHandler/JSONHandler.h"
+#include "JSONHandler/JSONRPC2Handler.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <iostream>
 
 int main()
 {
-	NsAppManager::AppMgr& appManager = NsAppManager::AppMgr::getInstance();
+    Logger logger = Logger::getInstance(LOG4CPLUS_TEXT("testAppMgr"));
+    PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("log4cplus.properties"));
+    LOG4CPLUS_INFO_EXT(logger, " Application started!");
+
+ //   NsTransportLayer::CBTAdapter btadapter;
+
+//    JSONHandler jsonHandler;
+
+//    AxisCore::ProtocolHandler* pProtocolHandler = new AxisCore::ProtocolHandler(&jsonHandler, &btadapter, 1);
+
+//    jsonHandler.setProtocolHandler(pProtocolHandler);
+
+    NsAppManager::AppMgr& appMgr = NsAppManager::AppMgr::getInstance();
+
+ //   jsonHandler.setRPCMessagesObserver(&appMgr);
+
+ //   appMgr.setJsonHandler(&jsonHandler);
+
+
+    LOG4CPLUS_INFO(logger, "Start AppMgr threads!");
+    appMgr.executeThreads();
+
+    return EXIT_SUCCESS;
+
+
 	return 0;
 }
