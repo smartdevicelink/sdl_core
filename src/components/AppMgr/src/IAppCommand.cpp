b8a68839c58eb89f408ec22c03b66240d92df2f7
Volodymyr Salo <vsalo@luxoft.com>
2012-11-12 19:07:25 +0200

    Documented IAppCommand API;

--- src/components/AppMgr/src/IAppCommand.cpp
+++ src/components/AppMgr/src/IAppCommand.cpp
@@ -6,24 +6,39 @@ namespace NsAppManager
 	
 log4cplus::Logger IAppCommand::mLogger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("AppMgrCommands"));
 
+/**
+ * \brief Class constructor
+ * \param receiver a registry item associated with application that will execute command
+ */
 IAppCommand::IAppCommand(const RegistryItem *receiver)
 	:mReceiver(receiver)
 	,mParams(NULL)
 {
 }
 
+/**
+ * \brief Copy constructor
+ */
 IAppCommand::IAppCommand(const IAppCommand &)
     :mReceiver(0)
     ,mParams(0)
 {
 }
 
+/**
+ * \brief Class constructor
+ * \param receiver a registry item associated with application that will execute command
+ * \param params command params
+ */
 IAppCommand::IAppCommand(const RegistryItem* receiver, const void* params)
 	:mReceiver(receiver)
 	,mParams(params)
 {
 }
 
+/**
+ * \brief Default destructor
+ */
 IAppCommand::~IAppCommand()
 {	
     if(mParams)
