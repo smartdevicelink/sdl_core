e2b055fa247575140b493caec67235f20bdb1937
Volodymyr Salo <vsalo@luxoft.com>
2012-11-12 17:43:08 +0200

    Documented AppPolicy API;

--- src/components/AppMgr/src/AppPolicy.cpp
+++ src/components/AppMgr/src/AppPolicy.cpp
@@ -12,30 +12,48 @@ namespace NsAppManager
 {
 log4cplus::Logger AppPolicy::mLogger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("AppPolicy"));
 	
+/**
+ * \brief Class constructor
+ * \param policy text representation
+ */
 AppPolicy::AppPolicy( const std::string& policy )
 : mPolicy(policy)
 {
     LOG4CPLUS_INFO_EXT(mLogger, " Created an app policy "<<policy);
 }
 
+/**
+ * \brief Default class destructor
+ */
 AppPolicy::~AppPolicy( )
 {
     LOG4CPLUS_INFO_EXT(mLogger, " Destroyed an app policy "<<mPolicy);
 }
 
+/**
+ * \brief comparison operator
+ * \return comparison result
+ */
 bool AppPolicy::operator <( const AppPolicy& item2 ) const
 {
 	return this->getPolicyHash() < item2.getPolicyHash();
 }
 
+/**
+ * \brief gets application policy text representation
+ * \return application policy test representation
+ */
 const std::string& AppPolicy::getPolicyHash( ) const
 {
     return mPolicy;
 }
 
+/**
+ * \brief Default class copy constructor
+ */
 AppPolicy::AppPolicy(const AppPolicy &policy)
     : mPolicy(policy.getPolicyHash())
 {
 }
 
-};
+}
