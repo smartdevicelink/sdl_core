e2e064ca91384e713d39bd68251c449471347396
Yevgeniy Koropatnyuk <YKoropatnyuk@luxoft.com>
2012-11-12 17:24:54 +0200

    Separated common device adapter logic from bluetooth adapter.
    Common device adapter logic is now in CDeviceAdapter. Bluetooth
    adapter now implements only bluetooth-specific logic.

--- src/components/TransportManager/src/CBluetoothAdapter.hpp
+++ src/components/TransportManager/src/CBluetoothAdapter.hpp
@@ -3,27 +3,22 @@
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/sdp.h>
-#include <pthread.h>
 #include <time.h>
 
 #include "Logger.hpp"
 
 #include "TransportManager/SDeviceInfo.hpp"
 
-#include "IDeviceAdapter.hpp"
-#include "IHandleGenerator.hpp"
+#include "CDeviceAdapter.hpp"
 
-class A;
 namespace NsAppLink
 {
     namespace NsTransportManager
     {
-        class IDeviceAdapterListener;
-
         /**
          * @brief Bluetooth device adapter.
          **/
-        class CBluetoothAdapter: public IDeviceAdapter
+        class CBluetoothAdapter: public CDeviceAdapter
         {
         public:
             /**
@@ -40,96 +35,17 @@ namespace NsAppLink
             virtual ~CBluetoothAdapter(void);
 
             /**
-             * @brief Run device adapter.
-             *
-             * Called from transport manager to start device adapter.
-             **/
-            virtual void run(void);
-
-            /**
-             * @brief Start scanning for new devices.
-             *
-             * List of new devices will be supplied in onDeviceListUpdated callback.
-             **/
-            virtual void scanForNewDevices(void);
-
-            /**
-             * @brief Connect to all applications discovered on device.
-             *
-             * @param DeviceHandle Handle of device to connect to.
-             **/
-            virtual void connectDevice(const tDeviceHandle DeviceHandle);
-
-            /**
-             * @brief Disconnect from all applications connected on device.
-             *
-             * @param DeviceHandle Handle of device to disconnect from.
-             **/
-            virtual void disconnectDevice(const tDeviceHandle DeviceHandle);
-
-            /**
              * @brief Get type of devices handled by this device adapter.
              *
              * @return Device type.
              **/
             virtual EDeviceType getDeviceType(void) const;
 
-            /**
-             * @brief Send frame.
-             *
-             * @param ConnectionHandle Connection handle.
-             * @param Data Frame data.
-             * @param DataSize Size of data in bytes.
-             *
-             * @return Frame sequence number. May be used to identify
-             *         this frame when send result callback is received.
-             **/
-            virtual int sendFrame(tConnectionHandle ConnectionHandle, const uint8_t * Data, size_t DataSize);
-
         private:
             /**
              * @brief Interval between service discovery for connected devices in seconds.
              **/
             static const time_t cConnectedDevicesServiceDiscoveryInterval = 10;
-
-            /**
-             * @brief Frame.
-             *
-             * Used to store data frames that must be sent to remote device.
-             **/
-            struct SFrame
-            {
-                /**
-                 * @brief Constructor.
-                 *
-                 * @param Data Frame data. SFrame stores a copy of this data,
-                 *             i.e. data may be freed after SFrame object is constructed.
-                 * @param DataSize Size of frame data in bytes.
-                 **/
-                SFrame(const uint8_t * Data, const size_t DataSize);
-
-                /**
-                 * @brief Destructor.
-                 *
-                 * Frees stored frame data.
-                 **/
-                ~SFrame(void);
-
-                /**
-                 * @brief Frame data.
-                 **/
-                uint8_t * mData;
-
-                /**
-                 * @brief Frame data size in bytes.
-                 **/
-                size_t mDataSize;
-            };
-
-            /**
-             * @brief Map of frame sequence number to frame data.
-             **/
-            typedef std::map<int, SFrame*> tFrameMap;
 
             /**
              * @brief Vector of RFCOMM channels.
@@ -139,20 +55,8 @@ namespace NsAppLink
             /**
              * @brief Internal structure describing bluetooth device.
              **/
-            struct SBluetoothDevice
+            struct SBluetoothDevice: public SDevice
             {
-                /**
-                 * @brief Default constructor.
-                 **/
-                SBluetoothDevice(void);
-
-                /**
-                 * @brief Copy constructor.
-                 *
-                 * @param Other Bluetooth device that must be copied.
-                 **/
-                SBluetoothDevice(const SBluetoothDevice & Other);
-
                 /**
                  * @brief Constructor.
                  *
@@ -163,41 +67,32 @@ namespace NsAppLink
                 SBluetoothDevice(const bdaddr_t & Address, const char * Name, const tRFCOMMChannelVector & AppLinkRFCOMMChannels);
 
                 /**
+                 * @brief Compare devices.
+                 *
+                 * This method checks whether two SBluetoothDevice structures
+                 * refer to the same device.
+                 *
+                 * @param OtherDevice Device to compare with.
+                 *
+                 * @return true if devices are equal, false otherwise.
+                 **/
+                virtual bool isSameAs(const SDevice * OtherDevice) const;
+
+                /**
                  * @brief Device bluetooth address.
                  **/
                 bdaddr_t mAddress;
 
                 /**
-                 * @brief Device user-friendly name.
-                 **/
-                std::string mName;
-
-                /**
-                * @brief Unique device identifier across all devices.
-                **/
-                std::string mUniqueDeviceId;
-
-                /**
                  * @brief List of RFCOMM channels where AppLink service has been discovered.
                  **/
                 tRFCOMMChannelVector mAppLinkRFCOMMChannels;
-
-                /**
-                 * @brief Flag indicating that device is connected.
-                 *
-                 * This flag is set by connectDevice and reset by disconnectDevice.
-                 * If device is connected service discovery is periodically performed on this
-                 * device. All newly discovered AppLink applications are automatically
-                 * connected and all connections for applications that are no longer
-                 * discoverable are disconnected.
-                 **/
-                bool mIsConnected;
             };
 
             /**
              * @brief RFCOMM connection.
              **/
-            struct SRFCOMMConnection
+            struct SRFCOMMConnection: public SConnection
             {
                 /**
                  * @brief Constructor.
@@ -208,145 +103,39 @@ namespace NsAppLink
                 SRFCOMMConnection(const tDeviceHandle DeviceHandle, const uint8_t RFCOMMChannel);
 
                 /**
-                 * @brief Destructor.
+                 * @brief Compare connections.
                  *
-                 * Clears map of frames to send.
+                 * This method compares whether two SRFCOMMConnection structures
+                 * refer to the same connection.
+                 *
+                 * @param OtherConnection Connection to compare with.
+                 *
+                 * @return true if connections are equal, false otherwise.
                  **/
-                ~SRFCOMMConnection(void);
-
-                /**
-                 * @brief Device handle.
-                 **/
-                const tDeviceHandle mDeviceHandle;
+                virtual bool isSameAs(const SConnection * OtherConnection) const;
 
                 /**
                  * @brief RFCOMM channel of remote device.
                  **/
                 const uint8_t mRFCOMMChannel;
-
-                /**
-                 * @brief Thread that handles connection.
-                 **/
-                pthread_t mConnectionThread;
-
-                /**
-                 * @brief File descriptors of notification pipe.
-                 *
-                 * Notification pipe is used to wake up connection thread
-                 * on external event (e.g. new data is available to send or
-                 * connection is requested to be terminated).
-                 *
-                 * mNotificationPipeFds[0] is a descriptor of the read end of the pipe
-                 * (the one that is used in poll() by connection thread) and
-                 * mNotificationPipeFds[1] is a descriptor of the write end of the pipe
-                 * (the one that is used in methods exposed to transport manager to
-                 * wake up connection thread when necessary).
-                 *
-                 * @note eventfd cannot be used instead of a pipe because it does not
-                 *       conform to POSIX (eventfd is Linux-specific).
-                 **/
-                int mNotificationPipeFds[2];
-
-                /**
-                 * @brief Sequence number for next frame.
-                 **/
-                int mNextFrameSequenceNumber;
-
-                /**
-                 * @brief Map of frames that must be sent to remote device.
-                 **/
-                tFrameMap mFramesToSend;
-
-                /**
-                 * @brief Terminate flag.
-                 *
-                 * This flag is set to notify connection thread that connection
-                 * must be closed and connection thread must be terminated.
-                 **/
-                bool mTerminateFlag;
             };
 
             /**
-             * @brief Parameters for establishing RFCOMM connection.
+             * @brief Create list of connections possible for specified device.
              *
-             * Used to pass connection parameters to connection thread.
+             * This method is called from connectDevice(). This implementation will
+             * perform service discovery and create connection structures for all
+             * RFCOMM channels where AppLink service is discovered.
+             *
+             * @param DeviceHandle Device handle.
+             * @param ConnectionsList Reference to connections list that must be filled.
              **/
-            struct SRFCOMMConnectionParameters
-            {
-                /**
-                 * @brief Constructor.
-                 *
-                 * @param BluetoothAdapter Reference to bluetooth adapter.
-                 * @param ConnectionHandle Connection handle.
-                 **/
-                SRFCOMMConnectionParameters(CBluetoothAdapter & BluetoothAdapter, tConnectionHandle ConnectionHandle);
-
-                /**
-                 * @brief Reference to bluetooth adapter.
-                 **/
-                CBluetoothAdapter & mBluetoothAdapter;
-
-                /**
-                 * @brief Connection handle.
-                 **/
-                tDeviceHandle mConnectionHandle;
-            };
-
-            /**
-             * @brief Bluetooth devices map.
-             **/
-            typedef std::map<tDeviceHandle, SBluetoothDevice> tBluetoothDevicesMap;
-
-            /**
-             * @brief RFCOMM connection map.
-             **/
-            typedef std::map<tConnectionHandle, SRFCOMMConnection *> tRFCOMMConnectionMap;
-
-            /**
-             * @brief Start RFCOMM connection.
-             *
-             * Initialize connection structure in RFCOMM connections map
-             * and start connection thread for handling communication.
-             *
-             * @param DeviceHandle Handle of device to connect to.
-             * @param RFCOMMChannel RFCOMM channel of AppLink service on remote device.
-             **/
-            void startRFCOMMConnection(const tDeviceHandle DeviceHandle, const uint8_t RFCOMMChannel);
-
-            /**
-             * @brief Stop RFCOMM connection.
-             *
-             * This method only initiates connection termination. It returns immediately
-             * without waiting for actual termination of the connection.
-             *
-             * @param ConnectionHandle Handle of connection to stop.
-             **/
-            void stopRFCOMMConnection(const tConnectionHandle ConnectionHandle);
-
-            /**
-             * @brief Start routine for device discovery thread.
-             *
-             * @param Data Must be pointer to CBluetoothAdapter instance.
-             *
-             * @return Thread return value.
-             **/
-            static void * deviceDiscoveryThreadStartRoutine(void * Data);
+            virtual void createConnectionsListForDevice(const tDeviceHandle DeviceHandle, std::vector<SConnection*> & ConnectionsList);
 
             /**
              * @brief Device discovery thread.
              **/
-            void deviceDiscoveryThread(void);
-
-            /**
-             * @brief Connection thread start routine.
-             *
-             * @param Data Must be pointer to SRFCOMMConnectionParameters. Ownership
-             *             of connection parameters is passed to connection thread, i.e.
-             *             connection thread is responsible for freeing this object.
-             *
-             * @return Thread return value.
-             **/
-            static void * connectionThreadStartRoutine(void * Data);
+            virtual void mainThread(void);
 
             /**
              * @brief Connection thread.
@@ -357,7 +146,7 @@ namespace NsAppLink
              *
              * @param ConnectionHandle Connection handle.
              **/
-            void connectionThread(const tConnectionHandle ConnectionHandle);
+            virtual void connectionThread(const tConnectionHandle ConnectionHandle);
 
             /**
              * @brief Get unique device ID.
@@ -383,80 +172,6 @@ namespace NsAppLink
              *                              with AppLink service discovered.
              **/
             void discoverAppLinkRFCOMMChannels(const bdaddr_t & DeviceAddress, tRFCOMMChannelVector & AppLinkRFCOMMChannels);
-
-            /**
-             * @brief Listener for device adapter notifications.
-             **/
-            IDeviceAdapterListener & mListener;
-
-            /**
-             * @brief Handle generator implementation.
-             **/
-            IHandleGenerator & mHandleGenerator;
-
-            /**
-             * @brief Logger.
-             **/
-            const log4cplus::Logger mLogger;
-
-            /**
-             * @brief Shutdown flag.
-             *
-             * This flag is set to true on shutdown to inform all bluetooth adapter
-             * threads that bluetooth adapter shutdown is in progress. After setting
-             * this flag bluetooth adapter waits until all its threads are terminated.
-             **/
-            bool mShutdownFlag;
-
-            /**
-             * @brief ID of device discovery thread.
-             **/
-            pthread_t mDeviceDiscoveryThread;
-
-            /**
-             * @brief Flag indicating whether the device discovery thread has been started successfully.
-             **/
-            bool mDeviceDiscoveryThreadStarted;
-
-            /**
-             * @brief Flag indicating that device scan was requested.
-             *
-             * This flag is set in scanForNewDevices and reset after requested
-             * device scan is completed.
-             **/
-            bool mDeviceScanRequested;
-
-            /**
-             * @brief Mutex restricting access to DeviceScanRequested flag.
-             **/
-            pthread_mutex_t mDeviceScanRequestedMutex;
-
-            /**
-             * @brief Conditional variable for signaling discovery thread about requested device scan.
-             **/
-            pthread_cond_t mDeviceScanRequestedCond;
-
-            /**
-             * @brief Map of device handle to bluetooth device.
-             *
-             * This map contains all currently available bluetooth devices.
-             **/
-            tBluetoothDevicesMap mDevices;
-
-            /**
-             * @brief Mutex restricting access to device map.
-             **/
-            mutable pthread_mutex_t mDevicesMutex;
-
-            /**
-             * @brief Map of RFCOMM connections.
-             **/
-            tRFCOMMConnectionMap mRFCOMMConnections;
-
-            /**
-             * @brief Mutex restricting access to RFCOMM connections map.
-             **/
-            mutable pthread_mutex_t mRFCOMMConnectionsMutex;
 
             /**
              * @brief UUID of AppLink service.
