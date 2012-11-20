2762ff681e13a5a4b3a86cd7989a92b7567f2b3a
Artem Petrosyan <apetrosyan@luxoft.com>
2012-11-20 11:46:25 +0200

    HMI: menu logic implemetation

--- src/components/HMI/app/view/info/nonMediaView.js
+++ src/components/HMI/app/view/info/nonMediaView.js
@@ -77,54 +77,158 @@ MFT.InfoNonMedia = Em.ContainerView.create( MFT.LoadableView, {
 		
 		innerMenu: Em.ContainerView.extend({
 			
-			classNames: ['inner_menu'],
+			classNames: ['ffw_list_menu'],
+			
+			attributeBindings: ['elementHeight:style'],
+			
+			// Position of current page
+			page:	0,
+			
+			// Items per one page
+			itemsOnPage: 5,
+			
+			// Height of one item
+			ITEM_HEIGHT: 50,
+			
+			pageHeight: function() {
+				return this.itemsOnPage*this.ITEM_HEIGHT;
+			}.property('this.itemsOnPage'),
+			
+			elementHeight: function() {
+				return 'height:' + String( this.get('pageHeight') + this.ITEM_HEIGHT - 2 ) + 'px;';
+			}.property('this.pageHeight'),
+			
+			// Position of content block
+			contentPositon: function() {
+				return 'top:' + String( -( this.get('page') * this.get('pageHeight') ) ) + 
+				'px; height:' + String( this.get('pageHeight') - (Boolean(this.get('onLastPage') && this.get('page')) * this.ITEM_HEIGHT) ) + 'px;';
+			}.property('onLastPage'),
+			
+			// Property for show or hide 'down' button
+			onLastPage: function() {
+				return ( ( this.get('page')  * this.itemsOnPage ) < ( this.get('content.childViews.length') - this.itemsOnPage ) );
+			}.property(
+				'page',
+				'content.childViews.length'
+			),
+			
+			
+			// Handeler to go previous page
+			pageUp: function() {
+				if ( this.get('page') ) {
+					this.set( 'page', ( this.get('page') - 1 ) );
+				}
+			},
+			
+			// Handeler to go next page
+			pageDown: function() {
+				if ( this.get('onLastPage') ) {
+					this.set( 'page', ( this.get('page') + 1) );
+				}				
+			},
 			
 			childViews: [
-				'optionsButton',
-				'routeInfoButton',
-				'softButton1',
-				'softButton2',
-				'softButton3',
-				'softButton4',
-				'softButton5'
+				'upButton',
+				'content',
+				'downButton'
 			],
 			
-			optionsButton: MFT.Button.extend({
-				text: 'Options',
+			upButton: MFT.Button.extend({
 				
-				templateName: 'arrow',
+				classNames:	['control','up_button'],
 				
-				goToState:	'options',
-				action:		'onChildState',
-				target:		'MFT.InfoController'
-			}),
-			
-			routeInfoButton: MFT.Button.extend({
-				text: 'Route Info',
+				hidden: function() {
+					if ( this.get('parentView.page') ) {
+						return false;
+					} else {
+						return true;
+					}
+				}.property('parentView.page'),
 				
-				hidden:	true,
 				
-				templateName: 'arrow'
+				templateName: 'icon',
+				
+				icon:	'images/media/ico_arrow_up.png',
+				
+				action:	'pageUp',
+				
+				target:	'parentView'				
 			}),
 			
-			softButton1: MFT.Button.extend({
-				text: 'Soft Button'
+			downButton: MFT.Button.extend({
+				
+				classNames:	['control','down_button'],
+				
+				hidden: function() {
+					if ( this.get('parentView.onLastPage') ) {
+						return false;
+					} else {
+						return true;
+					}
+				}.property('parentView.onLastPage'),
+				
+				templateName: 'icon',
+				
+				icon:	'images/media/ico_arrow_down.png',
+				
+				action:	'pageDown',
+				
+				target:	'parentView'				
 			}),
 			
-			softButton2: MFT.Button.extend({
-				text: 'Soft Button'
-			}),
-			
-			softButton3: MFT.Button.extend({
-				text: 'Soft Button'
-			}),
-			
-			softButton4: MFT.Button.extend({
-				text: 'Soft Button'
-			}),
-			
-			softButton5: MFT.Button.extend({
-				text: 'Soft Button'
+			content: Em.ContainerView.extend({
+				
+				classNames: ['content'],
+				
+				attributeBindings: ['parentView.contentPositon:style'],
+				
+				childViews: [
+					'optionsButton',
+					'routeInfoButton',
+					'softButton1',
+					'softButton2',
+					'softButton3',
+					'softButton4',
+					'softButton5'
+				],
+				
+				optionsButton: MFT.Button.extend({
+					text: 'Options',
+					
+					templateName: 'arrow',
+					
+					goToState:	'options',
+					action:		'onChildState',
+					target:		'MFT.InfoController'
+				}),
+				
+				routeInfoButton: MFT.Button.extend({
+					text: 'Route Info',
+					
+					//hidden:	true,
+					
+					templateName: 'arrow'
+				}),
+				
+				softButton1: MFT.Button.extend({
+					text: 'Soft Button 1'
+				}),
+				
+				softButton2: MFT.Button.extend({
+					text: 'Soft Button 2'
+				}),
+				
+				softButton3: MFT.Button.extend({
+					text: 'Soft Button 3'
+				}),
+				
+				softButton4: MFT.Button.extend({
+					text: 'Soft Button 4'
+				}),
+				
+				softButton5: MFT.Button.extend({
+					text: 'Soft Button 5'
+				})
 			})
 		})
 	})
