6ac1b6406335bf9cf14ba8ef7b20757dbdc6bbb0
Artem Petrosyan <apetrosyan@luxoft.com>
2012-12-11 15:52:52 +0200

    Add base app controller and model

---
+++ src/components/HMI/app/controller/applink/AppController.js
@@ -0,0 +1,35 @@
+/**
+ * @name MFT.ApplinkAppController
+ * 
+ * @desc Applink abstract application controller
+ * 
+ * @category	Controller
+ * @filesource	app/controller/applink/AppController.js
+ * @version		1.0
+ *
+ * @author		Artem Petrosyan
+ */
+ 
+MFT.ApplinkAppController = Em.Object.create({
+	
+	/**
+	 * Handeler for command button press
+	 * 
+	 * @param element:		MFT.Button
+	 */
+	onCommand: function( element ) {
+		FFW.UI.onCommand( element.commandId, element.appId );
+	},
+	
+	/**
+	 * Handeler for command submenu press
+	 * 
+	 * @param element:		MFT.Button
+	 */
+	onCommandSubMenu: function( element ) {
+		MFT.ApplinkController.getApplicationModel( element.appId ).set('currentSubMenuId',		element.menuId );
+		MFT.ApplinkController.getApplicationModel( element.appId ).set('currentSubMenuLabel',	element.text );
+		
+		MFT.States.goToState( MFT.States.get('currentState.path') + '.subMenu' );
+	}
+});
---
+++ src/components/HMI/app/model/applink/AppModel.js
@@ -0,0 +1,29 @@
+/**
+ * @name MFT.ApplinkAppModel
+ * 
+ * @desc Abstarct model for Applink applications
+ * 
+ * @category	Model
+ * @filesource	app/model/applink/AppModel.js
+ * @version		1.0
+ *
+ * @author		Artem Petrosyan
+ */
+ 
+MFT.ApplinkAppModel = Em.Object.extend({
+	
+	/**
+	 * Current command submenu identificator
+	 *
+	 * @type:	Number
+	 */
+	currentSubMenuId:		0,
+	
+	/**
+	 * Current command submenu text label
+	 *
+	 * @type:	String
+	 */	
+	currentSubMenuLabel:	''
+});
+ 
