b23917c62958ae0a941d30943b8da73484b16880
Evgenii Nikolskii <enikolskii@luxoft.com>
2012-12-19 15:29:53 +0200

    Fix AppLink-BUG-23: SyncProxyTester: the user can specify VR synonyms for choice set's commands

--- mobile/android/SyncProxyTester/src/com/ford/syncV4/android/activity/SyncProxyTester.java
+++ mobile/android/SyncProxyTester/src/com/ford/syncV4/android/activity/SyncProxyTester.java
@@ -908,6 +908,9 @@ public class SyncProxyTester extends Activity implements OnClickListener {
 							final EditText command1 = (EditText) layout.findViewById(R.id.createcommands_command1);
 							final EditText command2 = (EditText) layout.findViewById(R.id.createcommands_command2);
 							final EditText command3 = (EditText) layout.findViewById(R.id.createcommands_command3);
+							final EditText vr1 = (EditText) layout.findViewById(R.id.createcommands_vr1);
+							final EditText vr2 = (EditText) layout.findViewById(R.id.createcommands_vr2);
+							final EditText vr3 = (EditText) layout.findViewById(R.id.createcommands_vr3);
 							final CheckBox choice1 = (CheckBox) layout.findViewById(R.id.createcommands_choice1);
 							final CheckBox choice2 = (CheckBox) layout.findViewById(R.id.createcommands_choice2);
 							final CheckBox choice3 = (CheckBox) layout.findViewById(R.id.createcommands_choice3);
@@ -922,7 +925,7 @@ public class SyncProxyTester extends Activity implements OnClickListener {
 										one.setChoiceID(autoIncChoiceSetIdCmdId++);
 										one.setMenuName(command1.getText().toString());
 										one.setVrCommands(new Vector<String>(Arrays.asList(new String[] { command1.getText().toString(),
-												"Command one" })));
+												vr1.getText().toString() })));
 										commands.add(one);
 									}
 									
@@ -931,7 +934,7 @@ public class SyncProxyTester extends Activity implements OnClickListener {
 										two.setChoiceID(autoIncChoiceSetIdCmdId++);
 										two.setMenuName(command2.getText().toString());
 										two.setVrCommands(new Vector<String>(Arrays.asList(new String[] { command2.getText().toString(),
-												"Command two" })));
+												vr2.getText().toString() })));
 										commands.add(two);
 									}
 									
@@ -940,7 +943,7 @@ public class SyncProxyTester extends Activity implements OnClickListener {
 										three.setChoiceID(autoIncChoiceSetIdCmdId++);
 										three.setMenuName(command3.getText().toString());
 										three.setVrCommands(new Vector<String>(Arrays.asList(new String[] { command3.getText().toString(),
-												"Command three" })));
+												vr3.getText().toString() })));
 										commands.add(three);
 									}
 									
