56a644625615f8d2b73c4de825887f000ac12e58
Volodymyr Salo <vsalo@luxoft.com>
2012-12-20 18:02:45 +0200

    Correct error response upon application not being registered invoking SetGlobalProperties for protocol v1;

--- src/components/AppMgr/src/AppMgrCore.cpp
+++ src/components/AppMgr/src/AppMgrCore.cpp
@@ -522,6 +522,10 @@ namespace NsAppManager
                     if(!app)
                     {
                         LOG4CPLUS_ERROR_EXT(mLogger, "No application associated with the registry item with session key " << sessionKey );
+                        NsAppLinkRPC::SetGlobalProperties_response* response = new NsAppLinkRPC::SetGlobalProperties_response();
+                        response->set_success(false);
+                        response->set_resultCode(NsAppLinkRPC::Result::APPLICATION_NOT_REGISTERED);
+                        MobileHandler::getInstance().sendRPCMessage(response, sessionKey);
                         break;
                     }
                     if(NsAppLinkRPC::HMILevel::HMI_NONE == app->getApplicationHMIStatusLevel())
