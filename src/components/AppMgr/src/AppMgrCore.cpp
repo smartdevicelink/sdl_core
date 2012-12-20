65d481b8f4058ab87039d5fc242b427e1e6915a6
Volodymyr Salo <vsalo@luxoft.com>
2012-12-20 17:10:49 +0200

    Show no more executed if application's HMI status is HMI_NONE for protocol v1;

--- src/components/AppMgr/src/AppMgrCore.cpp
+++ src/components/AppMgr/src/AppMgrCore.cpp
@@ -434,6 +434,21 @@ namespace NsAppManager
                     LOG4CPLUS_INFO_EXT(mLogger, " A Show request has been invoked");
                     LOG4CPLUS_INFO_EXT(mLogger, "message " << mobileMsg->getMethodId() );
                     NsAppLinkRPC::Show_request* object = (NsAppLinkRPC::Show_request*)mobileMsg;
+                    Application_v1* app = (Application_v1*)core->getApplicationFromItemCheckNotNull(AppMgrRegistry::getInstance().getItem(sessionKey));
+                    if(!app)
+                    {
+                        LOG4CPLUS_ERROR_EXT(mLogger, "No application associated with the registry item with session key " << sessionKey );
+                        break;
+                    }
+                    if(NsAppLinkRPC::HMILevel::HMI_NONE == app->getApplicationHMIStatusLevel())
+                    {
+                        LOG4CPLUS_ERROR_EXT(mLogger, "An application " << app->getName() << " with session key " << sessionKey << " has not been activated yet!" );
+                        NsAppLinkRPC::Show_response* response = new NsAppLinkRPC::Show_response;
+                        response->set_success(false);
+                        response->set_resultCode(NsAppLinkRPC::Result::REJECTED);
+                        MobileHandler::getInstance().sendRPCMessage(response, sessionKey);
+                        break;
+                    }
                     NsRPC2Communication::UI::Show* showRPC2Request = new NsRPC2Communication::UI::Show();
                     showRPC2Request->setId(HMIHandler::getInstance().getJsonRPC2Handler()->getNextMessageId());
                     LOG4CPLUS_INFO_EXT(mLogger, "showrpc2request created");
