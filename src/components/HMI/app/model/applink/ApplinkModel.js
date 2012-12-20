eb19e3155045da9250b078118ea7b0511f058dc1
Artem Petrosyan <apetrosyan@luxoft.com>
2012-12-20 10:05:49 +0200

    HMI: Move Interaction methods to abstract app model

--- src/components/HMI/app/model/applink/ApplinkModel.js
+++ src/components/HMI/app/model/applink/ApplinkModel.js
@@ -41,58 +41,6 @@ MFT.ApplinkModel = Em.Object.create({
 			message.duration,
 			message.playTone
 		);
-	},
-
-	/**
-	 * Applink UI PreformInteraction response handeler
-	 * open Perform Interaction screen and show choices
-	 *
-	 * @param message:Object
-	 */
-	onPreformInteraction: function( message ) {
-		/* test message
-		message = JSON.parse('{"appId":1,"helpPrompt":[{"text":"help me, Im melting","type":"TEXT"}],"initialPrompt":[{"text":"Pick a command","type":"TEXT"}],"initialText":"Pick number:","interactionChoiceSetIDList":[4]}');
-		*/
-		
-		var i = 0,
-			length = message.interactionChoiceSetIDList.length;
-		
-		
-		MFT.InteractionChoicesView.clean();
-		
-		for ( i = 0; i < length; i++ ) {
-			MFT.InteractionChoicesView.preformChoices(
-				MFT.ApplinkController.getApplicationModel(message.appId).interactionChoises[ message.interactionChoiceSetIDList[i] ]
-			);		
-		}
-		
-		MFT.InteractionChoicesView.activate(message.initialText);
-	},
-	
-	/**
-	 * Applink UI CreateInteraction response handeler
-	 * push set of commands to voice recognition list
-	 *
-	 * @param message:Object
-	 */
-	onCreateInteraction: function( message ) {
-		
-		/* test message
-		message = JSON.parse('{"appId":1,"choiceSet":[{"choiceID":10,"menuName":"1st item","vrCommands":["1st item","Command one"]},{"choiceID":11,"menuName":"2nd item","vrCommands":["2nd item","Command two"]},{"choiceID":12,"menuName":"3rd item","vrCommands":["3rd item","Command three"]}],"interactionChoiceSetID":4}');
-		*/
-		
-		MFT.ApplinkController.getApplicationModel(message.appId).interactionChoises[message.interactionChoiceSetID] = message.choiceSet;
-	},
-	
-	/**
-	 * Applink UI DeleteInteraction response handeler
-	 * close current interaction set window (if opened)
-	 * and delete current set commands from voice recognition list
-	 *
-	 * @param message:Object
-	 */
-	onDeleteInteraction: function( message ) {		
-		delete MFT.ApplinkController.getApplicationModel(message.appId).interactionChoises[message.interactionChoiceSetID];
 	}
 });
  
