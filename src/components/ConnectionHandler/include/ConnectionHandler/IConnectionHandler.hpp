58d22fbff85a3494cb9ef46ee2f0119604e682b6
Alexandr Kara <AKara@luxoft.com>
2012-12-25 12:44:49 +0200

    Added interface for ConnectionHandler component.

---
+++ src/components/ConnectionHandler/include/ConnectionHandler/IConnectionHandler.hpp
@@ -0,0 +1,47 @@
+/**
+ * \file IConnectionHandler.hpp
+ * \brief Connection handler interface class.
+ * \author AKara
+ */
+
+#ifndef ICONNECTIONHANDLER_H 
+#define ICONNECTIONHANDLER_H 
+
+#include "TransportManager/ITransportManagerDeviceListener.hpp"
+#include "TransportManager/SDeviceInfo.hpp"
+#include "ProtocolHandler/ISessionObserver.h"
+#include "ConnectionHandler/IConnectionHandlerObserver.hpp"
+#include "ConnectionHandler/CDevice.hpp"
+#include "ConnectionHandler/CConnection.hpp"
+#include "ConnectionHandler/IDevicesDiscoveryStarter.hpp"
+
+/**
+ * \namespace NsConnectionHandler
+ * \brief AppLink ConnectionHandler namespace.
+ */ 
+namespace NsConnectionHandler
+{
+    /**
+    * \class IConnectionHandler
+    * \brief AppLink ConnectionHandler interface class
+    */
+    class IConnectionHandler
+    {
+    public:
+        /**
+         * \brief Sets observer pointer for ConnectionHandler.
+         * \param observer Pointer to observer object.
+         **/
+        virtual void setConnectionHandlerObserver(IConnectionHandlerObserver * observer)=0;
+        virtual void setTransportManager( NsAppLink::NsTransportManager::ITransportManager * transportManager )=0;
+
+    protected:
+        /**
+         * \brief Destructor
+         */
+        virtual ~IConnectionHandler(){};
+
+    };
+}/* namespace NsConnectionHandler */
+
+#endif /* ICONNECTIONHANDLER_H */
