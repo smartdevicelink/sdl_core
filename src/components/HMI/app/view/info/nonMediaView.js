86075659e450d38fa83813d48583a5b77514454a
Artem Petrosyan <apetrosyan@luxoft.com>
2013-01-31 13:42:42 +0200

    HMI: Fix softbuttons (media v2) and revert previus commit mistake

--- src/components/HMI/app/view/info/nonMediaView.js
+++ src/components/HMI/app/view/info/nonMediaView.js
@@ -79,7 +79,7 @@ MFT.InfoNonMedia = Em.ContainerView.create( MFT.LoadableView, {
 		innerMenu: MFT.MenuList.extend({
 			
 			refreshItems: function() {
-                if ( MFT.ApplinkAppController.model.appId == MFT.ApplinkMediaController.currentAppId ) {
+                if ( MFT.ApplinkAppController.model.appId == MFT.NonMediaController.currentAppId ) {
                     this.addItems(MFT.ApplinkAppController.model.softButtons);
                 }                    
             }.observes('MFT.ApplinkAppController.model.softButtons.@each'),
