a077b673620faa644ff501deafedfe3967cdd0b8
Yuriy Skriyka <yskriyka@luxoft.com>
2013-02-23 23:16:22 +0200

    Basic test extended with array test

--- test/components/SmartObjects/SmartObjectBasicTest.cpp
+++ test/components/SmartObjects/SmartObjectBasicTest.cpp
@@ -49,6 +49,47 @@ namespace test { namespace components { namespace SmartObjects { namespace Smart
         }
     }
 
+
+    // TODO: Figure out why the test fails
+    TEST(BasicArray, test_SmartObjectBasicTest)
+    {
+        CSmartObject obj;
+
+        ASSERT_EQ(0, static_cast<int>(obj[0])) << "Wrong value at accessing non existent index";
+        ASSERT_EQ(0, static_cast<int>(obj["non_existent_key"])) << "Wrong value at accessing non existent key";
+
+        obj[0] = 1;
+        ASSERT_EQ(1, static_cast<int>(obj[0])) << "Wrong value at 0 index";
+        obj[1] = 2;
+        ASSERT_EQ(2, static_cast<int>(obj[1])) << "Wrong value at 1 index";
+
+        obj[0][0] = 3;
+        obj[1][0] = 1;
+        ASSERT_EQ(3, static_cast<int>(obj[0][0])) << "Wrong value at index 0, 0";
+
+        obj[0][0][0] = 4;
+        obj[0][1][0] = 5;   // FIXME: Segmentation fault
+        ASSERT_EQ(4, static_cast<int>(obj[0][0][0])) << "Wrong value at index 0, 0, 0";
+
+        const int size = 2;
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                for (int k = 0; k < size; k++)
+                {
+                    std::cout << "i, j, k: " << i << ", " << j << ", " << k << std::endl;
+                    obj[i][j][k] = i + j + k;
+                }
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                for (int k = 0; k < size; k++)
+                {
+                    ASSERT_EQ(i + j + k, static_cast<int>(obj[i][j][k])) <<
+                            "Wrong value in the array at index: " << i << ", " << j << ", " << k;
+                }
+    }
+
+
     int main(int argc, char **argv)
     {
         ::testing::InitGoogleTest(&argc, argv);
