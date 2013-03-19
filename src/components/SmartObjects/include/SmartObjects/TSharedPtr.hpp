80f1bd7a22f736283ecdfed2eefbc905cb5f502d
Yevgeniy Koropatnyuk <YKoropatnyuk@luxoft.com>
2013-03-19 13:43:01 +0200

    Added conversion to another pointer type for shared pointer.

--- src/components/SmartObjects/include/SmartObjects/TSharedPtr.hpp
+++ src/components/SmartObjects/include/SmartObjects/TSharedPtr.hpp
@@ -34,9 +34,19 @@ namespace NsAppLink
              * Initialize shared pointer with another shared pointer.
              * Reference counter will be incremented.
              *
+             * @tparam OtherObjectType Type of other object pointer. This
+             *                         allows creating a shared pointer to an
+             *                         intstance of a base class from a shared
+             *                         pointer to an instance of a class
+             *                         inherited from this base class.
+             *                         If OtherObjectType is not implicitly
+             *                         convertible to ObjectType it will
+             *                         cause a compile error.
+             *
              * @param Other Other shared pointer.
              **/
-            TSharedPtr(const TSharedPtr<ObjectType> & Other);
+            template <typename OtherObjectType>
+            TSharedPtr(const TSharedPtr<OtherObjectType> & Other);
 
             /**
              * @brief Destructor.
@@ -52,12 +62,40 @@ namespace NsAppLink
              * Drop reference to currently referenced object and add
              * reference to assigned object.
              *
+             * @tparam OtherObjectType Type of other object pointer. This
+             *                         allows creating a shared pointer to an
+             *                         intstance of a base class from a shared
+             *                         pointer to an instance of a class
+             *                         inherited from this base class.
+             *                         If OtherObjectType is not implicitly
+             *                         convertible to ObjectType it will
+             *                         cause a compile error.
+             *
              * @param Other Shared pointer to an object
              *              that must be referenced.
              *
              * @return Reference to this shared pointer.
              **/
-            TSharedPtr<ObjectType> & operator =(const TSharedPtr<ObjectType> & Other);
+            template <typename OtherObjectType>
+            TSharedPtr<ObjectType> & operator =(const TSharedPtr<OtherObjectType> & Other);
+
+            /**
+             * @brief Type conversion operator.
+             *
+             * Convert shared pointer to a shared pointer to another object type.
+             * This allows creating a shared pointer to an intstance of a base
+             * class from a shared pointer to an instance of a class inherited
+             * from this base class.
+             *
+             * If ObjectType is not implicitly convertible to OtherObjectType
+             * it will cause a compile error.
+             *
+             * @tparam OtherObjectType Type of other object pointer.
+             *
+             * @return Shared pointer to OtherObjectType.
+             **/
+            template <typename OtherObjectType>
+            operator TSharedPtr<OtherObjectType>(void);
 
             /**
              * @brief Member access operator.
