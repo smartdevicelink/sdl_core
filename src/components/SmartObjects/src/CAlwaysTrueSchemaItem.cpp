887a6994d00f22aed98e21398833c2a04371b2d9
Yevgeniy Koropatnyuk <YKoropatnyuk@luxoft.com>
2013-03-20 09:58:25 +0200

    Added constructor and create method for CAlwaysTrueSchemaItem.

--- src/components/SmartObjects/src/CAlwaysTrueSchemaItem.cpp
+++ src/components/SmartObjects/src/CAlwaysTrueSchemaItem.cpp
@@ -1,5 +1,14 @@
 #include "SmartObjects/CAlwaysTrueSchemaItem.hpp"
 
+NsAppLink::NsSmartObjects::CAlwaysTrueSchemaItem::CAlwaysTrueSchemaItem(void)
+{
+}
+
+NsAppLink::NsSmartObjects::TSharedPtr<NsAppLink::NsSmartObjects::CAlwaysTrueSchemaItem> NsAppLink::NsSmartObjects::CAlwaysTrueSchemaItem::create(void)
+{
+    return new CAlwaysTrueSchemaItem();
+}
+
 NsAppLink::NsSmartObjects::Errors::eType NsAppLink::NsSmartObjects::CAlwaysTrueSchemaItem::validate(const NsAppLink::NsSmartObjects::CSmartObject& object)
 {
     return NsAppLink::NsSmartObjects::Errors::OK;
