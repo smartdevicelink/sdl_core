41e520ca04ca4c21094e4b1c48d1f76fcacbff9f
Yuriy Skriyka <yskriyka@luxoft.com>
2013-04-03 10:52:33 +0300

    Extended Json ALRPCv2 formatter unit test

--- test/components/JSONHandler/formatters/FormatterJsonALRPCv2Test.cpp
+++ test/components/JSONHandler/formatters/FormatterJsonALRPCv2Test.cpp
@@ -14,7 +14,7 @@ namespace test { namespace components { namespace JSONHandler { namespace format
 
     using namespace NsAppLink::NsJSONHandler::strings;
 
-    TEST_F(CFormatterTestHelper, test_JsonALRPCv2)
+    TEST_F(CFormatterTestHelper, test_fromObjToALRPCv2AndBack)
     {
         Json::Value value;  // just a quick workaround to avoid undefined reference to Json
 
@@ -36,6 +36,44 @@ namespace test { namespace components { namespace JSONHandler { namespace format
 
 
         compareObjects(srcObj, dstObj);
+    }
+
+    TEST_F(CFormatterTestHelper, test_fromJsonALRPCv2)
+    {
+        std::string str = "\
+        {\
+            \"appID\": \"APP ID\",\
+            \"appName\": \"APP NAME\",\
+            \"appType\": [\"SYSTEM\", \"COMMUNICATION\"],\
+            \"hmiDisplayLanguageDesired\": \"RU-RU\",\
+            \"isMediaApplication\": true,\
+            \"languageDesired\": \"EN-US\",\
+            \"ngnMediaScreenAppName\": \"SCREEN NAME\",\
+            \"syncMsgVersion\": {\
+                \"majorVersion\": 2,\
+                \"minorVersion\": 10\
+            },\
+            \"ttsName\": [{\
+                \"text\": \"ABC\",\
+                \"type\": \"TEXT\"\
+            }],\
+            \"vrSynonyms\": [\"Synonym 1\", \"Synonym 2\"]\
+        }";
+
+        NsAppLink::NsSmartObjects::CSmartObject obj;
+        bool result;
+
+        result = NsAppLink::NsJSONHandler::Formatters::CFormatterJsonALRPCv2::
+                fromString<std::string, std::string>(str, obj, "some name", "request", 12);
+
+        ASSERT_TRUE(result) << "Error parsing JSON string";
+
+        ASSERT_EQ("some name", (std::string)obj[S_PARAMS][S_FUNCTION_ID]);
+        ASSERT_EQ(12, (int)obj[S_PARAMS][S_CORRELATION_ID]);
+        ASSERT_EQ(2, (int)obj[S_PARAMS][S_PROTOCOL_VERSION]);
+        ASSERT_EQ(10, (int)obj[S_MSG_PARAMS]["syncMsgVersion"]["minorVersion"]);
+        ASSERT_EQ("TEXT", (std::string)obj[S_MSG_PARAMS]["ttsName"][0]["type"]);
+        ASSERT_EQ("Synonym 2", (std::string)obj[S_MSG_PARAMS]["vrSynonyms"][1]);
     }
 
 }}}}
