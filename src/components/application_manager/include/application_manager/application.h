6b10a599aaa8ebff8ce0bff5fe34b47439544632
Vladislav Semenyuk <VSemenyuk@luxoft.com>
2013-05-10 07:28:04 -0700

    Commit is not subject to review: trivial changes

--- src/components/application_manager/include/application_manager/application.h
+++ src/components/application_manager/include/application_manager/application.h
@@ -66,7 +66,7 @@ public:
 	virtual const smart_objects::CSmartObject* activeMessage() const = 0;
 	virtual void clearActiveMessage() = 0;
 	virtual const Version& version() const = 0;
-	virtual int appId() const = 0;
+	virtual int app_id() const = 0;
 	virtual const std::string& name() const = 0;
 protected:
 	virtual ~Application() {}
