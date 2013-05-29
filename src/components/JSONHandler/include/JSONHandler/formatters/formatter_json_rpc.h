d39e06721b050ab005cbd7da1186c1d1df38ae29
Yevgeniy Koropatnyuk <YKoropatnyuk@luxoft.com>
2013-05-29 16:29:50 +0300

    Draft implementation of JSON RPC formatter.

---
+++ src/components/JSONHandler/include/JSONHandler/formatters/formatter_json_rpc.h
@@ -0,0 +1,397 @@
+/**
+ * @file formatter_json_rpc.h
+ * @brief FormatterJsonRpc header file.
+ */
+// Copyright (c) 2013, Ford Motor Company
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided with the
+// distribution.
+//
+// Neither the name of the Ford Motor Company nor the names of its contributors
+// may be used to endorse or promote products derived from this software
+// without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 'A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SMARTDEVICELINK_COMPONENTS_JSONHANDLER_INCLUDE_FORMATTERS_FORMATTER_JSON_RPC_H_
+#define SMARTDEVICELINK_COMPONENTS_JSONHANDLER_INCLUDE_FORMATTERS_FORMATTER_JSON_RPC_H_
+
+#include <string>
+#include <sys/stat.h>
+
+#include "SmartObjects/CSmartObject.hpp"
+#include <SmartObjects/TEnumSchemaItem.hpp>
+#include "json/json.h"
+
+#include "CFormatterJsonBase.hpp"
+#include "JSONHandler/CSmartFactory.hpp"
+
+namespace NsSmartDeviceLink {
+namespace NsJSONHandler {
+namespace Formatters {
+
+/**
+ * @brief Formatter for JSON RPC format.
+ */
+class FormatterJsonRpc: public CFormatterJsonBase
+{
+ public:
+   /**
+    * @brief No errors occured during the parsing of the input JSON string.
+    */
+   static const int kSuccess = 0;
+
+   /**
+    * @brief Input JSON string cannot be parsed.
+    */
+   static const int kParsingError = 1;
+
+   /**
+    * @brief Input JSON string has invalid format.
+    */
+   static const int kInvalidFormat = 2;
+
+   /**
+    * @brief Method in input JSON string is not
+    *        specified or specified incorrectly.
+    */
+   static const int kMethodNotSpecified = 4;
+
+   /**
+    * @brief Method is unknown.
+    */
+   static const int kUnknownMethod = 8;
+
+   /**
+    * @brief Message type is unknown.
+    */
+   static const int kUnknownMessageType = 16;
+
+   /**
+    * @brief Id of request or response is invalid.
+    */
+   static const int kInvalidId = 32;
+
+   /**
+    * @brief Response code is not available.
+    */
+   static const int kResponseCodeNotAvailable = 64;
+
+   /**
+    * @brief Creates a JSON string from a SmartObject.
+    *
+    * @param obj Input SmartObject.
+    * @param out_str Resulting JSON string.
+    *
+    * @return true if success, false otherwise.
+    */
+  static bool ToString(const NsSmartObjects::CSmartObject &obj,
+                       std::string &out_str);
+
+  /**
+    * @brief Creates a SmartObject from a JSON string.
+    *
+    * @tparam FunctionId Type of function id enumeration.
+    * @tparam MessageType Type of message type enumeration.
+    *
+    * @param str input JSON string.
+    * @param out The resulting SmartObject.
+    *
+    * @return An integer that is a bitwise-or of all error codes occured
+    *         during the parsing of the input string. 0 if no errors occured.
+    */
+  template <typename FunctionId, typename MessageType>
+  static int FromString(const std::string &str,
+                        NsSmartObjects::CSmartObject &out);
+ private:
+  /**
+   * @brief Request.
+   */
+  static const char *kRequest;
+
+  /**
+   * @brief Response.
+   */
+  static const char *kResponse;
+
+  /**
+   * @brief Notification.
+   */
+  static const char *kNotification;
+
+  /**
+   * @brief Name of "jsonrpc" message field.
+   */
+  static const char *kJsonRpc;
+
+  /**
+   * @brief Expected value of "jsonrpc" message field ("2.0").
+   */
+  static const char *kJsonRpcExpectedValue;
+
+  /**
+   * @brief Name of "id" message field.
+   */
+  static const char *kId;
+
+  /**
+   * @brief Name of "method" field.
+   */
+  static const char *kMethod;
+
+  /**
+   * @brief Name of "params" field.
+   */
+  static const char *kParams;
+
+  /**
+   * @brief Name of "error" field.
+   */
+  static const char *kError;
+
+  /**
+   * @brief Name of "code" field.
+   */
+  static const char *kCode;
+
+  /**
+   * @brief Name of "data" field.
+   */
+  static const char *kData;
+
+  /**
+   * @brief Constructor.
+   */
+  FormatterJsonRpc();
+
+  /**
+   * @brief Copy constructor.
+   *
+   * @param unused Unused parameter.
+   */
+  FormatterJsonRpc(const FormatterJsonRpc &unused);
+
+  /**
+   * @brief Parse function id.
+   *
+   * @tparam FunctionId Type of function id enumeration.
+   *
+   * @param method_value JSON value with function id.
+   * @param out The resulting SmartObject.
+   *
+   * @return An integer that is a bitwise-or of all error codes occured
+   *         during the parsing of the function id. 0 if no errors occured.
+   */
+  template <typename FunctionId>
+  static int ParseFunctionId(const Json::Value &method_value,
+                             NsSmartObjects::CSmartObject &out);
+
+  /**
+   * @brief Set method.
+   *
+   * Try to extract function id from the message object and set the "method"
+   * field of the container.
+   *
+   * @param params Message parameters object.
+   * @param method_container Container for the "method" field.
+   *
+   * @return true if function id was extracted successfully and set as a
+   *         value of "method" field.
+   */
+  static bool SetMethod(const NsSmartObjects::CSmartObject &params,
+                        Json::Value &method_container);
+
+  /**
+   * @brief Set id.
+   *
+   * Try to extract request/response id from the message object and set the
+   * "id" field of the container.
+   *
+   * @param params Message parameters object.
+   * @param id_container Container for the "id" field.
+   *
+   * @return true if request/response id was extracted successfully and set
+   *         as a value of "id" field.
+   */
+  static bool SetId(const NsSmartObjects::CSmartObject &params,
+                    Json::Value &id_container);
+};
+
+template <typename FunctionId, typename MessageType>
+int FormatterJsonRpc::FromString(const std::string &str,
+                                 NsSmartObjects::CSmartObject &out) {
+  Json::Value root;
+  Json::Reader reader;
+  int result = kSuccess;
+  namespace strings = NsSmartDeviceLink::NsJSONHandler::strings;
+
+  if (false == reader.parse(str, root)) {
+    result = kParsingError;
+  } else {
+    if (false == root.isMember(kJsonRpc)){
+      result |= kInvalidFormat;
+    } else {
+      const Json::Value &jsonRpcValue = root[kJsonRpc];
+
+      if ((false == jsonRpcValue.isString()) ||
+          (jsonRpcValue.asString() != kJsonRpcExpectedValue)) {
+        result |= kInvalidFormat;
+      }
+    }
+
+    std::string message_type_string;
+    Json::Value response_value;
+
+    if (false == root.isMember(kId)) {
+      message_type_string = kNotification;
+
+      if (false == root.isMember(kMethod)) {
+        result |= kMethodNotSpecified;
+      } else {
+        result |= ParseFunctionId<FunctionId>(root[kMethod], out);
+      }
+    } else {
+      const Json::Value &id_value = root[kId];
+
+      if (true == id_value.isString()) {
+        out[strings::S_PARAMS][strings::S_CORRELATION_ID] =
+            id_value.asString();
+      } else if (true == id_value.isInt()) {
+        out[strings::S_PARAMS][strings::S_CORRELATION_ID] =
+            id_value.asInt();
+      } else if (true == id_value.isDouble()) {
+        out[strings::S_PARAMS][strings::S_CORRELATION_ID] =
+            id_value.asDouble();
+      } else if (true == id_value.isNull()) {
+        out[strings::S_PARAMS][strings::S_CORRELATION_ID] =
+            NsSmartObjects::CSmartObject(NsSmartObjects::SmartType_Null);
+      } else {
+        result |= kInvalidFormat | kInvalidId;
+      }
+
+      if (true == root.isMember(kMethod)) {
+        message_type_string = kRequest;
+        result |= ParseFunctionId<FunctionId>(root[kMethod], out);
+      } else {
+        Json::Value method_container;
+
+        if (true == root.isMember(kParams)) {
+          response_value = root[kParams];
+          method_container = response_value;
+        } else if (true == root.isMember(kError)) {
+          response_value = root[kError];
+
+          if (true == response_value.isMember(kData)) {
+            method_container = response_value[kData];
+          }
+        }
+
+        if (false == method_container.isObject()) {
+          result |= kInvalidFormat | kUnknownMessageType;
+        } else {
+          if (false == method_container.isMember(kMethod)) {
+            result |= kInvalidFormat | kUnknownMessageType;
+          } else {
+            message_type_string = kResponse;
+            result |= ParseFunctionId<FunctionId>(method_container[kMethod],
+                                                  out);
+          }
+        }
+      }
+    }
+
+    if (0 == (result & kUnknownMessageType)) {
+      MessageType message_type;
+
+      if (false == NsSmartObjects::TEnumSchemaItem<MessageType>::stringToEnum(
+          message_type_string, message_type)) {
+        result |= kUnknownMessageType;
+      } else {
+        out[strings::S_PARAMS][strings::S_MESSAGE_TYPE] = message_type;
+      }
+    }
+
+    if (true == root.isMember(kParams)) {
+      jsonValueToObj(root[kParams], out[strings::S_MSG_PARAMS]);
+    } else {
+      out[strings::S_MSG_PARAMS] = NsSmartObjects::CSmartObject(
+          NsSmartObjects::SmartType_Map);
+    }
+
+    if (kResponse == message_type_string) {
+      out[strings::S_MSG_PARAMS].erase(kMethod);
+      out[strings::S_MSG_PARAMS].erase(kCode);
+
+      if (true == root.isMember(kParams)) {
+        if (false == response_value.isObject()) {
+          result |= kResponseCodeNotAvailable;
+        } else {
+          if (false == response_value.isMember(kCode)) {
+            result |= kResponseCodeNotAvailable;
+          } else {
+            const Json::Value &code_value = response_value[kCode];
+
+            if (false == code_value.isInt()) {
+              result |= kResponseCodeNotAvailable;
+            } else {
+              out[strings::S_PARAMS][strings::kCode] = code_value.asInt();
+            }
+          }
+        }
+      }
+    }
+  }
+
+  out[strings::S_PARAMS][strings::S_PROTOCOL_TYPE] = 1;
+  out[strings::S_PARAMS][strings::S_PROTOCOL_VERSION] = 2;
+
+  return result;
+}
+
+template <typename FunctionId>
+int FormatterJsonRpc::ParseFunctionId(const Json::Value &method_value,
+                                      NsSmartObjects::CSmartObject &out) {
+  int result = kSuccess;
+
+  if (false == method_value.isString()) {
+    result |= kInvalidFormat | kUnknownMethod;
+  } else {
+    FunctionId function_id;
+
+    if (false == NsSmartObjects::TEnumSchemaItem<FunctionId>::stringToEnum(
+        method_value.asString(), function_id)) {
+      result |= kUnknownMethod;
+    } else {
+      namespace strings = NsSmartDeviceLink::NsJSONHandler::strings;
+
+      out[strings::S_PARAMS][strings::S_FUNCTION_ID] = function_id;
+    }
+  }
+
+  return result;
+}
+
+} // namespace Formatters
+} // namespace NsJSONHandler
+} // namespace NsSmartDeviceLink
+
+#endif // SMARTDEVICELINK_COMPONENTS_JSONHANDLER_INCLUDE_FORMATTERS_FORMATTER_JSON_RPC_H_
---
+++ src/components/JSONHandler/src/formatters/formatter_json_rpc.cc
@@ -0,0 +1,160 @@
+/**
+ * @file formatter_json_rpc.cc
+ * @brief formatter_json_rpc source file.
+ */
+// Copyright (c) 2013, Ford Motor Company
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided with the
+// distribution.
+//
+// Neither the name of the Ford Motor Company nor the names of its contributors
+// may be used to endorse or promote products derived from this software
+// without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 'A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "JSONHandler/formatters/formatter_json_rpc.h"
+
+namespace NsSmartDeviceLink {
+namespace NsJSONHandler {
+namespace Formatters {
+
+namespace strings = NsJSONHandler::strings;
+
+const char *FormatterJsonRpc::kRequest = "request";
+const char *FormatterJsonRpc::kResponse = "response";
+const char *FormatterJsonRpc::kNotification = "notification";
+const char *FormatterJsonRpc::kJsonRpc = "jsonrpc";
+const char *FormatterJsonRpc::kJsonRpcExpectedValue = "2.0";
+const char *FormatterJsonRpc::kId = "id";
+const char *FormatterJsonRpc::kMethod = "method";
+const char *FormatterJsonRpc::kParams = "params";
+const char *FormatterJsonRpc::kError = "error";
+const char *FormatterJsonRpc::kCode = "code";
+const char *FormatterJsonRpc::kData = "data";
+
+bool FormatterJsonRpc::ToString(const NsSmartObjects::CSmartObject &obj,
+                                std::string &out_str) {
+  Json::Value root(Json::objectValue);
+
+  root[kJsonRpc] = kJsonRpcExpectedValue;
+
+  NsSmartObjects::CSmartObject formatted_object(obj);
+  formatted_object.getSchema().unapplySchema(formatted_object);
+
+  bool result = formatted_object.keyExists(strings::S_MSG_PARAMS);
+  if (true == result) {
+    const NsSmartObjects::CSmartObject &msg_params =
+        formatted_object.getElement(strings::S_MSG_PARAMS);
+
+    result = (NsSmartObjects::SmartType_Map == msg_params.getType());
+    if (true == result) {
+      Json::Value msg_params_json(Json::objectValue);
+      objToJsonValue(msg_params, msg_params_json);
+      root[kParams] = msg_params_json;
+    }
+  }
+
+  if (false == formatted_object.keyExists(strings::S_PARAMS)) {
+    result = false;
+  } else {
+    const NsSmartObjects::CSmartObject &params = formatted_object.getElement(
+        strings::S_PARAMS);
+    if (NsSmartObjects::SmartType_Map != params.getType()) {
+      result = false;
+    } else {
+      const NsSmartObjects::CSmartObject &message_type_object =
+          formatted_object.getElement(strings::S_MESSAGE_TYPE);
+
+      if (NsSmartObjects::SmartType_String != message_type_object.getType()) {
+        result = false;
+      } else {
+        const std::string message_type = message_type_object;
+
+        if (kRequest == message_type) {
+          result = result && SetMethod(params, root);
+          result = result && SetId(params, root);
+        } else if (kResponse == message_type) {
+          result = result && SetMethod(params, root[kParams]);
+          result = result && SetId(params, root);
+
+          if (false == params.keyExists(strings::kCode)) {
+            result = false;
+          } else {
+            const NsSmartObjects::CSmartObject &code = params.getElement(
+                strings::kCode);
+
+            if (NsSmartObjects::SmartType_Integer != code.getType()) {
+              result = false;
+            } else {
+              root[kParams][kCode] = code.asInt();
+            }
+          }
+        } else if (kNotification == message_type) {
+          result = result && SetMethod(params, root);
+        } else {
+          result = false;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+bool FormatterJsonRpc::SetMethod(const NsSmartObjects::CSmartObject &params,
+                                 Json::Value &method_container) {
+  bool result = false;
+
+  if (true == params.keyExists(strings::S_FUNCTION_ID)) {
+    const NsSmartObjects::CSmartObject &function_id =
+        params.getElement(strings::S_FUNCTION_ID);
+
+    if (NsSmartObjects::SmartType_String == function_id.getType()) {
+      method_container[kMethod] = function_id.asString();
+      result = true;
+    }
+  }
+
+  return result;
+}
+
+bool FormatterJsonRpc::SetId(const NsSmartObjects::CSmartObject &params,
+                             Json::Value &id_container) {
+  bool result = false;
+
+  if (true == params.keyExists(strings::S_CORRELATION_ID)) {
+    const NsSmartObjects::CSmartObject &id = params.getElement(
+        strings::S_CORRELATION_ID);
+
+    if (NsSmartObjects::SmartType_Integer == id.getType()) {
+      id_container[kId] = id.asString();
+      result = true;
+    }
+  }
+
+  return result;
+}
+
+} // namespace Formatters
+} // namespace NsJSONHandler
+} // namespace NsSmartDeviceLink
