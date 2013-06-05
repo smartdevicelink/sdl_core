e0370c1224f24e2484677a255ce6c995906acb58
Dmitry Klimenko <DKlimenko@luxoft.com>
2013-06-05 03:51:57 -0700

    APPLINK-1260;APPLINK-1261: GetVehicleData API initial impl

--- src/components/application_manager/src/basic_command_factory.cc
+++ src/components/application_manager/src/basic_command_factory.cc
@@ -75,6 +75,8 @@
 #include "application_manager/commands/subscribe_vehicle_data_response_command.h"
 #include "application_manager/commands/unsubscribe_vehicle_data_command.h"
 #include "application_manager/commands/unsubscribe_vehicle_data_response_command.h"
+#include "application_manager/commands/get_vehicle_data_command.h"
+#include "application_manager/commands/get_vehicle_data_response_command.h"
 #include "application_manager/commands/alert_maneuver_command.h"
 #include "application_manager/commands/alert_maneuver_response_command.h"
 #include "application_manager/commands/set_icon_command.h"
@@ -260,6 +262,14 @@ CommandSharedPtr BasicCommandFactory::CreateCommand(
            command.reset(new commands::UnsubscribeVehicleDataCommandResponse(message));
          } else {
            command.reset(new commands::UnsubscribeVehicleDataCommandRequest(message));
+         }
+         break;
+    }
+    case NsSmartDeviceLinkRPC::V2::FunctionID::eType::GetVehicleDataID: {
+         if ((*message)[strings::params][strings::message_type] == MessageType::kResponse) {
+           command.reset(new commands::GetVehicleDataCommandResponse(message));
+         } else {
+           command.reset(new commands::GetVehicleDataCommandRequest(message));
          }
          break;
     }
