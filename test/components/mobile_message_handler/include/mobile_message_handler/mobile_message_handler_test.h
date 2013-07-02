24014212379551007b86c052ab069ed878f5d4cd
Kandul Alexander <akandul@luxoft.com>
2013-07-02 17:52:07 +0300

    MobileMessageHandlerTest fixed

--- test/components/mobile_message_handler/include/mobile_message_handler/mobile_message_handler_test.h
+++ test/components/mobile_message_handler/include/mobile_message_handler/mobile_message_handler_test.h
@@ -1,3 +1,41 @@
+/**
+ * \file request_watchdog_test.cc
+ * \brief RequetWatchdog test source file.
+ *
+ * Copyright (c) 2013, Ford Motor Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ford Motor Company nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef TEST_COMPONENTS_MOBILE_MESSAGE_HANDLER_INCLUDE_MOBILE_MESSAGE_HANDLER_MOBILE_MESSAGE_HANDLER_TEST_H_
+#define TEST_COMPONENTS_MOBILE_MESSAGE_HANDLER_INCLUDE_MOBILE_MESSAGE_HANDLER_MOBILE_MESSAGE_HANDLER_TEST_H_
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
@@ -13,7 +51,6 @@
 
 //! ---------------------------------------------------------------------------
 
-namespace {
 log4cxx::LoggerPtr logger = log4cxx::LoggerPtr(
     log4cxx::Logger::getLogger("mobile_message_handler_test"));
 
@@ -21,10 +58,10 @@ sync_primitives::SynchronisationPrimitives synchronisation;
 sync_primitives::Timer* timer;
 const unsigned int kTimeout = 2;
 bool flag = false;
-}
 
 //! ---------------------------------------------------------------------------
-
+namespace test {
+namespace components {
 namespace mobile_message_handler_test {
 /**
  * @class MobileMessageHandlerTester implements ApplicationManager
@@ -59,7 +96,9 @@ class MobileMessageHandlerTester :
 
  private:
   mobile_message_handler::MobileMessageHandlerImpl* mmh_;
-  MobileMessage message_;DISALLOW_COPY_AND_ASSIGN(MobileMessageHandlerTester);
+  MobileMessage message_;
+
+  DISALLOW_COPY_AND_ASSIGN(MobileMessageHandlerTester);
 };
 
 /**
@@ -67,14 +106,13 @@ class MobileMessageHandlerTester :
  */
 class MobileMessageHandlerTestObserverThread : public threads::ThreadDelegate {
  public:
-  MobileMessageHandlerTestObserverThread(const MobileMessage& message)
+  explicit MobileMessageHandlerTestObserverThread(const MobileMessage& message)
       : message_(message) {
   }
   ~MobileMessageHandlerTestObserverThread() {
   }
 
   void threadMain() {
-
     mobile_message_handler::MobileMessageHandlerImpl* mmh =
         mobile_message_handler::MobileMessageHandlerImpl::instance();
     DCHECK(mmh);
@@ -127,12 +165,8 @@ TEST(mobile_message_handler_test, component_test) {
   observer_thread->start();
   observer_thread->join();
 }
+}  // namespace mobile_message_handler_test
+}  // namespace components
+}  // namespace test
 
-//! ---------------------------------------------------------------------------
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleMock(&argc, argv);
-  log4cxx::PropertyConfigurator::configure("log4cxx.properties");
-
-  return RUN_ALL_TESTS();
-}}
+#endif  // TEST_COMPONENTS_MOBILE_MESSAGE_HANDLER_INCLUDE_MOBILE_MESSAGE_HANDLER_MOBILE_MESSAGE_HANDLER_TEST_H_
--- test/test_suit.cc
+++ test/test_suit.cc
@@ -10,6 +10,7 @@
 #include "SmartObjectStressTest.h"
 #include "SmartObjectUnitTest.h"
 #include "TSharedPtrTest.h"
+#include "mobile_message_handler/mobile_message_handler_test.h"
 //#include "SmartObjectConvertionTimeTest.h"
 //#include "jsoncpp/json_reader_test.h"
 //#include "json_handler/smart_schema_draft_test.h"
@@ -19,6 +20,7 @@
 //#include "json_handler/formatters/formatter_json_rpcv2_test.h"
 
 
+using namespace test::components::mobile_message_handler_test;
 using namespace test::components::request_watchdog_test;
 using namespace test::components::utils;
 using namespace test::components::protocol_handler_test;
