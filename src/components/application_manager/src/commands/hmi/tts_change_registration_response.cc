98189dd34b77ee7811db03c9a25358f0fb5fa403
Dmitry Klimenko <DKlimenko@luxoft.com>
2013-07-10 00:18:47 -0700

    APPLINK-1972: Rename JSONHandler to formatters

--- src/components/application_manager/src/commands/hmi/tts_change_registration_response.cc
+++ src/components/application_manager/src/commands/hmi/tts_change_registration_response.cc
@@ -32,7 +32,7 @@
 #include "application_manager/commands/hmi/tts_change_registration_response.h"
 #include "application_manager/application_manager_impl.h"
 #include "application_manager/message_chaining.h"
-#include "interfaces/MOBILE_API.h"
+#include "interfaces/HMI_API.h"
 
 namespace application_manager {
 
