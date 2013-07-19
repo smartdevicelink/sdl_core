29e4cbae4467007ca7bd0470277dce23f241d024
Andrew_Batutin <abatutin@luxoft.com>
2013-07-19 13:47:48 +0300

    Added pacage to contain test classes

---
+++ mobile/android/SyncProxyTester/src/instrumentalTest/com/ford/syncV4/android/videostreaming/VideoStreamingTestCase.java
@@ -0,0 +1,17 @@
+package instrumentalTest.com.ford.syncV4.android.videostreaming;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by admin on 7/19/13.
+ */
+public class VideoStreamingTestCase extends TestCase {
+
+    public VideoStreamingTestCase() {
+    }
+
+    public void testTest() throws Exception {
+        assertEquals("", "ss");
+
+    }
+}
