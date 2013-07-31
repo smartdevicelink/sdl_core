504f850519d1deff2752550b9a7f0402ac6ded8d
Eugene Nikolsky <enikolskii@luxoft.com>
2013-07-31 09:42:03 +0300

    APPLINK-2207: SyncProxyAndroid SDK: update FunctionID values
    NB: The SDLCore must use the same values to be compatible!
    
    The commit is not subject to review: trivial changes.

--- mobile/android/SyncProxyAndroid/src/com/ford/syncV4/protocol/enums/FunctionID.java
+++ mobile/android/SyncProxyAndroid/src/com/ford/syncV4/protocol/enums/FunctionID.java
@@ -1,83 +1,109 @@
 package com.ford.syncV4.protocol.enums;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import com.ford.syncV4.proxy.constants.Names;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FunctionID {
-	static ArrayList<String> functionID = null;
-	
-	public FunctionID() {
-	}
-	
-	static public String getFunctionName(int i) {
-		if (functionID == null) {
-			initFunctionIds();
-		}
-		return functionID.get(i);
-	}
-	
-	static public void initFunctionIds() {
-		String [] functionIds = new String[60];
-		functionIds[1] = Names.RegisterAppInterface;
-		functionIds[2] = Names.UnregisterAppInterface;
-		functionIds[3] = Names.SetGlobalProperties;
-		functionIds[4] = Names.ResetGlobalProperties;
-		functionIds[5] = Names.AddCommand;
-		functionIds[6] = Names.DeleteCommand;
-		functionIds[7] = Names.AddSubMenu;
-		functionIds[8] = Names.DeleteSubMenu;
-		functionIds[9] = Names.CreateInteractionChoiceSet;
-		functionIds[10] = Names.PerformInteraction;
-		functionIds[11] = Names.DeleteInteractionChoiceSet;
-		functionIds[12] = Names.Alert;
-		functionIds[13] = Names.Show;
-		functionIds[14] = Names.Speak;
-		functionIds[15] = Names.SetMediaClockTimer;
-		functionIds[16] = Names.EncodedSyncPData;
-		functionIds[17] = Names.DialNumber;
-		functionIds[18] = Names.PerformAudioPassThru;
-		functionIds[19] = Names.EndAudioPassThru;
-		functionIds[20] = Names.SubscribeButton;
-		functionIds[21] = Names.UnsubscribeButton;
-		functionIds[22] = Names.SubscribeVehicleData;
-		functionIds[23] = Names.UnsubscribeVehicleData;
-		functionIds[24] = Names.GetVehicleData;
-		functionIds[25] = Names.ReadDID;
-		functionIds[26] = Names.GetDTCs;
-		functionIds[27] = Names.ScrollableMessage;
-		functionIds[28] = Names.Slider;
-		functionIds[29] = Names.ShowConstantTBT;
-		functionIds[30] = Names.AlertManeuver;
-		functionIds[31] = Names.UpdateTurnList;
-		functionIds[32] = Names.ChangeRegistration;
-		functionIds[33] = Names.GenericResponse;
-		functionIds[34] = Names.PutFile;
-		functionIds[35] = Names.DeleteFile;
-		functionIds[36] = Names.ListFiles;
-		functionIds[37] = Names.SetAppIcon;
-		functionIds[38] = Names.SetDisplayLayout;
-		functionIds[39] = Names.OnHMIStatus;
-		functionIds[40] = Names.OnAppInterfaceUnregistered;
-		functionIds[41] = Names.OnButtonEvent;
-		functionIds[42] = Names.OnButtonPress;
-		functionIds[43] = Names.OnVehicleData;
-		functionIds[44] = Names.OnCommand;
-		functionIds[45] = Names.OnEncodedSyncPData;
-		functionIds[46] = Names.OnTBTClientState;
-		functionIds[47] = Names.OnDriverDistraction;
-		functionIds[48] = Names.OnPermissionsChange;
-		functionIds[49] = Names.OnAudioPassThru;
-		functionIds[50] = Names.OnLanguageChange;	
-		
-		functionID = new ArrayList<String>(Arrays.asList(functionIds));
-	}
-	
-	static public int getFunctionID(String functionName) {
-		if (functionID == null) {
-			initFunctionIds();
-		}
-		return functionID.indexOf(functionName);
-	}
+    private static Map<String, Integer> functionIDMap = null;
+
+    public FunctionID() {
+    }
+
+    static public String getFunctionName(int i) {
+        if (null == functionIDMap) {
+            initFunctionIds();
+        }
+
+        for (Map.Entry<String, Integer> entry : functionIDMap.entrySet()) {
+            if (i == entry.getValue()) {
+                return entry.getKey();
+            }
+        }
+
+        return null;
+    }
+
+    static public int getFunctionID(String functionName) {
+        if (null == functionIDMap) {
+            initFunctionIds();
+        }
+
+        return functionIDMap.get(functionName);
+    }
+
+    static public void initFunctionIds() {
+
+        functionIDMap = new HashMap<String, Integer>(60) {{
+            /*
+                Base Request / Response RPCs
+                Range = 0x 0000 0001 - 0x 0000 7FFF
+             */
+            put(Names.RegisterAppInterface, 1);
+            put(Names.UnregisterAppInterface, 2);
+            put(Names.SetGlobalProperties, 3);
+            put(Names.ResetGlobalProperties, 4);
+            put(Names.AddCommand, 5);
+            put(Names.DeleteCommand, 6);
+            put(Names.AddSubMenu, 7);
+            put(Names.DeleteSubMenu, 8);
+            put(Names.CreateInteractionChoiceSet, 9);
+            put(Names.PerformInteraction, 10);
+            put(Names.DeleteInteractionChoiceSet, 11);
+            put(Names.Alert, 12);
+            put(Names.Show, 13);
+            put(Names.Speak, 14);
+            put(Names.SetMediaClockTimer, 15);
+            put(Names.PerformAudioPassThru, 16);
+            put(Names.EndAudioPassThru, 17);
+            put(Names.SubscribeButton, 18);
+            put(Names.UnsubscribeButton, 19);
+            put(Names.SubscribeVehicleData, 20);
+            put(Names.UnsubscribeVehicleData, 21);
+            put(Names.GetVehicleData, 22);
+            put(Names.ReadDID, 23);
+            put(Names.GetDTCs, 24);
+            put(Names.ScrollableMessage, 25);
+            put(Names.Slider, 26);
+            put(Names.ShowConstantTBT, 27);
+            put(Names.AlertManeuver, 28);
+            put(Names.UpdateTurnList, 29);
+            put(Names.ChangeRegistration, 30);
+            put(Names.GenericResponse, 31);
+            put(Names.PutFile, 32);
+            put(Names.DeleteFile, 33);
+            put(Names.ListFiles, 34);
+            put(Names.SetAppIcon, 35);
+            put(Names.SetDisplayLayout, 36);
+
+            /*
+                Base Notifications
+                Range = 0x 0000 8000 - 0x 0000 FFFF
+             */
+            put(Names.OnHMIStatus, 32768);
+            put(Names.OnAppInterfaceUnregistered, 32769);
+            put(Names.OnButtonEvent, 32770);
+            put(Names.OnButtonPress, 32771);
+            put(Names.OnVehicleData, 32772);
+            put(Names.OnCommand, 32773);
+            put(Names.OnTBTClientState, 32774);
+            put(Names.OnDriverDistraction, 32775);
+            put(Names.OnPermissionsChange, 32776);
+            put(Names.OnAudioPassThru, 32777);
+            put(Names.OnLanguageChange, 32778);
+
+            /*
+                Ford Specific Request / Response RPCs
+                Range = 0x 0001 0000 - 0x 0001 7FFF
+             */
+            put(Names.EncodedSyncPData, 65536);
+
+            /*
+                Ford Specific Notifications
+                Range = 0x 0001 8000 - 0x 0001 FFFF
+             */
+            put(Names.OnEncodedSyncPData, 98304);
+        }};
+    }
 }
