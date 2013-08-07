908f02443f9ca92b67fc5068b3324d02c61e2190
Andrew_Batutin <abatutin@luxoft.com>
2013-08-07 11:48:32 +0300

    added massive method to encode - decode

--- mobile/android/AndroidVideoStreaming/src/main/java/com/batutin/android/androidvideostreaming/activity/AvcEncoder.java
+++ mobile/android/AndroidVideoStreaming/src/main/java/com/batutin/android/androidvideostreaming/activity/AvcEncoder.java
@@ -7,29 +7,43 @@ import android.media.MediaCodecInfo;
 import android.media.MediaCodecList;
 import android.media.MediaFormat;
 import android.util.Log;
+import android.view.Surface;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class AvcEncoder {
 
     public static final String MIME_TYPE = "video/avc";
+    private static final String TAG = "EncodeDecodeTest";
+    // movie length, in frames
+    private static final int NUM_FRAMES = 30;               // two seconds of video
+    private static final boolean VERBOSE = false;           // lots of logging
+    private static final boolean DEBUG_SAVE_FILE = true;   // save copy of encoded movie
+    private static final String DEBUG_FILE_NAME_BASE = "/sdcard/test.";
     public EncodedFrameListener frameListener;
+    public ParameterSetsListener parameterSetsListener;
     private MediaCodec mediaCodec;
     private byte[] sps;
     private byte[] pps;
     private MediaCodecInfo codecInfo;
-    public ParameterSetsListener parameterSetsListener;
+    private CamcorderProfile camcorderProfile;
+    private MediaFormat mediaFormat;
+    private int colorFormat;
+    private byte[] frameData;
+    private Surface surface;
+    private static final int FRAME_RATE = 15;               // 15fps
 
     public AvcEncoder() {
         try {
             mediaCodec = MediaCodec.createEncoderByType(MIME_TYPE);
             codecInfo = selectCodec(MIME_TYPE);
-            CamcorderProfile p = getCamcorderProfileHigh();
-            MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc", p.videoFrameWidth, p.videoFrameHeight);
-            mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, p.videoBitRate);
-            mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, p.videoFrameRate);
-            int colorFormat = selectColorFormat(codecInfo, MIME_TYPE);
+            camcorderProfile = getCamcorderProfileHigh();
+            mediaFormat = MediaFormat.createVideoFormat("video/avc", camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
+            mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, camcorderProfile.videoBitRate);
+            mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, camcorderProfile.videoFrameRate);
+            colorFormat = selectColorFormat(codecInfo, MIME_TYPE);
             mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, colorFormat);
             mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 10);
             mediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
@@ -77,34 +91,23 @@ public class AvcEncoder {
         return null;
     }
 
-    private static int selectColorFormat(MediaCodecInfo codecInfo, String mimeType) throws NullPointerException{
+    private static int selectColorFormat(MediaCodecInfo codecInfo, String mimeType) throws NullPointerException {
         MediaCodecInfo.CodecCapabilities capabilities = codecInfo.getCapabilitiesForType(mimeType);
-        if ( capabilities.colorFormats == null || capabilities.colorFormats.length ==0 ){
+        if (capabilities.colorFormats == null || capabilities.colorFormats.length == 0) {
             throw new NullPointerException("Unable to get color formats");
         }
         for (int i = 0; i < capabilities.colorFormats.length; i++) {
             int colorFormat = capabilities.colorFormats[i];
-                return colorFormat;
+            return colorFormat;
         }
         return 0;   // not reached
     }
 
     /**
-     * Returns true if this is a color format that this test code understands (i.e. we know how
-     * to read and generate frames in this format).
+     * Generates the presentation time for frame N, in microseconds.
      */
-    private static boolean isRecognizedFormat(int colorFormat) {
-        switch (colorFormat) {
-            // these are the formats we know how to handle for this test
-            case MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar:
-            case MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420PackedPlanar:
-            case MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar:
-            case MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420PackedSemiPlanar:
-            case MediaCodecInfo.CodecCapabilities.COLOR_TI_FormatYUV420PackedSemiPlanar:
-                return true;
-            default:
-                return false;
-        }
+    private static long computePresentationTime(int frameIndex) {
+        return 132 + frameIndex * 1000000 / FRAME_RATE;
     }
 
     public void close() throws IOException {
@@ -161,5 +164,229 @@ public class AvcEncoder {
             t.printStackTrace();
         }
 
+    }
+
+    private void doEncodeDecodeVideoFromBuffer(MediaCodec encoder, int encoderColorFormat,
+                                               MediaCodec decoder) {
+
+        long startMs = System.currentTimeMillis();
+
+        final int TIMEOUT_USEC = 10000;
+        ByteBuffer[] encoderInputBuffers = encoder.getInputBuffers();
+        ByteBuffer[] encoderOutputBuffers = encoder.getOutputBuffers();
+        ByteBuffer[] decoderInputBuffers = null;
+        ByteBuffer[] decoderOutputBuffers = null;
+        MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+        MediaFormat decoderOutputFormat = null;
+        int generateIndex = 0;
+        int checkIndex = 0;
+        int badFrames = 0;
+        boolean decoderConfigured = false;
+
+        // Just out of curiosity.
+        long rawSize = 0;
+        long encodedSize = 0;
+
+        // Save a copy to disk.  Useful for debugging the test.  Note this is a raw elementary
+        // stream, not a .mp4 file, so not all players will know what to do with it.
+        FileOutputStream outputStream = null;
+        if (DEBUG_SAVE_FILE) {
+            String fileName = DEBUG_FILE_NAME_BASE + camcorderProfile.videoFrameWidth + "x" + camcorderProfile.videoFrameHeight + ".mp4";
+            try {
+                outputStream = new FileOutputStream(fileName);
+                Log.d(TAG, "encoded output will be saved as " + fileName);
+            } catch (IOException ioe) {
+                Log.w(TAG, "Unable to create debug output file " + fileName);
+                throw new RuntimeException(ioe);
+            }
+        }
+
+        // Loop until the output side is done.
+        boolean inputDone = false;
+        boolean encoderDone = false;
+        boolean outputDone = false;
+        while (!outputDone) {
+            if (VERBOSE) Log.d(TAG, "loop");
+
+            // If we're not done submitting frames, generate a new one and submit it.  By
+            // doing this on every loop we're working to ensure that the encoder always has
+            // work to do.
+            //
+            // We don't really want a timeout here, but sometimes there's a delay opening
+            // the encoder device, so a short timeout can keep us from spinning hard.
+            if (!inputDone) {
+                int inputBufIndex = encoder.dequeueInputBuffer(TIMEOUT_USEC);
+                if (VERBOSE) Log.d(TAG, "inputBufIndex=" + inputBufIndex);
+                if (inputBufIndex >= 0) {
+                    long ptsUsec = computePresentationTime(generateIndex);
+                    if (generateIndex == NUM_FRAMES) {
+                        // Send an empty frame with the end-of-stream flag set.  If we set EOS
+                        // on a frame with data, that frame data will be ignored, and the
+                        // output will be short one frame.
+                        encoder.queueInputBuffer(inputBufIndex, 0, 0, ptsUsec,
+                                MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                        inputDone = true;
+                        if (VERBOSE) Log.d(TAG, "sent input EOS (with zero-length frame)");
+                    } else {
+
+
+                        ByteBuffer inputBuf = encoderInputBuffers[inputBufIndex];
+
+                        inputBuf.clear();
+                        inputBuf.put(frameData);
+
+                        encoder.queueInputBuffer(inputBufIndex, 0, frameData.length, ptsUsec, 0);
+                        if (VERBOSE) Log.d(TAG, "submitted frame " + generateIndex + " to enc");
+                    }
+                    generateIndex++;
+                } else {
+                    // either all in use, or we timed out during initial setup
+                    if (VERBOSE) Log.d(TAG, "input buffer not available");
+                }
+            }
+
+            // Check for output from the encoder.  If there's no output yet, we either need to
+            // provide more input, or we need to wait for the encoder to work its magic.  We
+            // can't actually tell which is the case, so if we can't get an output buffer right
+            // away we loop around and see if it wants more input.
+            //
+            // Once we get EOS from the encoder, we don't need to do this anymore.
+            if (!encoderDone) {
+                int encoderStatus = encoder.dequeueOutputBuffer(info, TIMEOUT_USEC);
+                if (encoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
+                    // no output available yet
+                    if (VERBOSE) Log.d(TAG, "no output from encoder available");
+                } else if (encoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+                    // not expected for an encoder
+                    encoderOutputBuffers = encoder.getOutputBuffers();
+                    if (VERBOSE) Log.d(TAG, "encoder output buffers changed");
+                } else if (encoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                    // not expected for an encoder
+                    MediaFormat newFormat = encoder.getOutputFormat();
+                    if (VERBOSE) Log.d(TAG, "encoder output format changed: " + newFormat);
+                } else if (encoderStatus < 0) {
+                    Log.e(TAG, "unexpected result from encoder.dequeueOutputBuffer: " + encoderStatus);
+                } else { // encoderStatus >= 0
+                    ByteBuffer encodedData = encoderOutputBuffers[encoderStatus];
+                    if (encodedData == null) {
+                        Log.e(TAG, ("encoderOutputBuffer " + encoderStatus + " was null");
+                    }
+
+                    // It's usually necessary to adjust the ByteBuffer values to match BufferInfo.
+                    encodedData.position(info.offset);
+                    encodedData.limit(info.offset + info.size);
+
+                    encodedSize += info.size;
+                    if (outputStream != null) {
+                        byte[] data = new byte[info.size];
+                        encodedData.get(data);
+                        encodedData.position(info.offset);
+                        try {
+                            outputStream.write(data);
+                        } catch (IOException ioe) {
+                            Log.w(TAG, "failed writing debug data to file");
+                            throw new RuntimeException(ioe);
+                        }
+                    }
+                    if ((info.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+                        // Codec config info.  Only expected on first packet.  One way to
+                        // handle this is to manually stuff the data into the MediaFormat
+                        // and pass that to configure().  We do that here to exercise the API.
+
+                        MediaFormat format =
+                                MediaFormat.createVideoFormat(MIME_TYPE, camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
+                        format.setByteBuffer("csd-0", encodedData);
+                        decoder.configure(format, surface,
+                                null, 0);
+                        decoder.start();
+                        decoderInputBuffers = decoder.getInputBuffers();
+                        decoderOutputBuffers = decoder.getOutputBuffers();
+                        decoderConfigured = true;
+                        if (VERBOSE) Log.d(TAG, "decoder configured (" + info.size + " bytes)");
+                    } else {
+                        // Get a decoder input buffer, blocking until it's available.
+
+                        int inputBufIndex = decoder.dequeueInputBuffer(-1);
+                        ByteBuffer inputBuf = decoderInputBuffers[inputBufIndex];
+                        inputBuf.clear();
+                        inputBuf.put(encodedData);
+                        decoder.queueInputBuffer(inputBufIndex, 0, info.size,
+                                info.presentationTimeUs, info.flags);
+
+                        encoderDone = (info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0;
+                        if (VERBOSE) Log.d(TAG, "passed " + info.size + " bytes to decoder"
+                                + (encoderDone ? " (EOS)" : ""));
+                    }
+
+                    encoder.releaseOutputBuffer(encoderStatus, false);
+                }
+            }
+
+            // Check for output from the decoder.  We want to do this on every loop to avoid
+            // the possibility of stalling the pipeline.  We use a short timeout to avoid
+            // burning CPU if the decoder is hard at work but the next frame isn't quite ready.
+            //
+            // If we're decoding to a Surface, we'll get notified here as usual but the
+            // ByteBuffer references will be null.  The data is sent to Surface instead.
+            if (decoderConfigured) {
+                int decoderStatus = decoder.dequeueOutputBuffer(info, TIMEOUT_USEC);
+                if (decoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
+                    // no output available yet
+                    if (VERBOSE) Log.d(TAG, "no output from decoder available");
+                } else if (decoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+                    // The storage associated with the direct ByteBuffer may already be unmapped,
+                    // so attempting to access data through the old output buffer array could
+                    // lead to a native crash.
+                    if (VERBOSE) Log.d(TAG, "decoder output buffers changed");
+                    decoderOutputBuffers = decoder.getOutputBuffers();
+                } else if (decoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                    // this happens before the first frame is returned
+                    decoderOutputFormat = decoder.getOutputFormat();
+                    if (VERBOSE) Log.d(TAG, "decoder output format changed: " +
+                            decoderOutputFormat);
+                } else if (decoderStatus < 0) {
+                    Log.e(TAG, "unexpected result from deocder.dequeueOutputBuffer: " + decoderStatus);
+                } else {  // decoderStatus >= 0
+
+                    if (VERBOSE) Log.d(TAG, "surface decoder given buffer " + decoderStatus +
+                            " (size=" + info.size + ")");
+                    rawSize += info.size;
+                    if ((info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                        if (VERBOSE) Log.d(TAG, "output EOS");
+                        outputDone = true;
+                    }
+
+
+                    // We use a very simple clock to keep the video FPS, or the video
+                    // playback will be too fast
+                    while (info.presentationTimeUs / 1000 > System.currentTimeMillis() - startMs) {
+                        try {
+                            this.wait(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                    // As soon as we call releaseOutputBuffer, the buffer will be forwarded
+                    // to SurfaceTexture to convert to a texture.  The API doesn't guarantee
+                    // that the texture will be available before the call returns, so we
+                    // need to wait for the onFrameAvailable callback to fire.
+                    decoder.releaseOutputBuffer(decoderStatus, true);
+
+
+                }
+            }
+        }
+
+        if (VERBOSE) Log.d(TAG, "decoded " + checkIndex + " frames at "
+                + camcorderProfile.videoFrameWidth + "x" + camcorderProfile.videoFrameHeight + ": raw=" + rawSize + ", enc=" + encodedSize);
+        if (outputStream != null) {
+            try {
+                outputStream.close();
+            } catch (IOException ioe) {
+                Log.w(TAG, "failed closing debug file");
+                throw new RuntimeException(ioe);
+            }
+        }
     }
 }
--- mobile/android/AndroidVideoStreaming/src/main/java/com/batutin/android/androidvideostreaming/activity/DecodeActivity.java
+++ mobile/android/AndroidVideoStreaming/src/main/java/com/batutin/android/androidvideostreaming/activity/DecodeActivity.java
@@ -2,11 +2,8 @@ package com.batutin.android.androidvideostreaming.activity;
 
 import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
-import android.media.CamcorderProfile;
 import android.media.MediaCodec;
 import android.media.MediaCodec.BufferInfo;
-import android.media.MediaCodecInfo;
-import android.media.MediaCodecList;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.os.Bundle;
@@ -17,76 +14,27 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import com.batutin.android.androidvideostreaming.R;
-import com.batutin.android.androidvideostreaming.screenshot.ScreenShotUtils;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
+
+    // parameters for the encoder
+    private static final String MIME_TYPE = "video/avc";    // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 15;               // 15fps
+    private static final int IFRAME_INTERVAL = 10;          // 10 seconds between I-frames
     private static final String SAMPLE = Environment.getExternalStorageDirectory() + "/video.mp4";
     private PlayerThread mPlayer = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        test();
         SurfaceView sv = new SurfaceView(this);
         sv.getHolder().addCallback(this);
         setContentView(sv);
-
-    }
-
-    private void test() {
-
-        int start = CamcorderProfile.QUALITY_HIGH;
-        for (int i = 0; i< CamcorderProfile.QUALITY_QVGA; i++){
-            if (CamcorderProfile.hasProfile(i) == true){
-                start = i;
-            }
-        }
-
-        boolean hasProf = CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_HIGH);
-        if (start > -1) {
-            CamcorderProfile cp = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
-
-            int width = 320, height = 240;
-            int bitRate = 128000;
-            int frameRate = 15;
-            ScreenShotUtils u = new ScreenShotUtils();
-            String mimeType = "video/avc";
-            int numCodecs = MediaCodecList.getCodecCount();
-            MediaCodecInfo codecInfo = u.getMediaCodecInfo(mimeType, numCodecs);
-            //String s = CodecCapabilitiesReader.get();
-            //MediaCodecInfo.CodecCapabilities capabilities = codecInfo.getCapabilitiesForType(mimeType);
-            int stride = u.calcStrideForWidth(width, codecInfo);
-            int sliceHeight = u.calcSliceHeightForHeight(height, codecInfo);
-            //int colorFormat = u.getCapabilities(capabilities);
-            int frameSize = u.calcFrameStride(stride, sliceHeight, stride);
-            MediaFormat inputFormat = MediaFormat.createVideoFormat("video/avc", width, height);
-            inputFormat.setInteger(MediaFormat.KEY_BIT_RATE, bitRate);
-            inputFormat.setInteger(MediaFormat.KEY_FRAME_RATE, frameRate);
-            inputFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar);
-            inputFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 75);
-            inputFormat.setInteger("stride", stride);
-            inputFormat.setInteger("slice-height", sliceHeight);
-            String name = codecInfo.getName();
-            try {
-                MediaCodec encoder = MediaCodec.createByCodecName(name); // need to find name in media codec list, it is chipset-specific
-                encoder.configure(inputFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
-                encoder.start();
-                ByteBuffer[] encoderInputBuffers = encoder.getInputBuffers();
-                ByteBuffer[] encoderOutputBuffers = encoder.getOutputBuffers();
-            }
-            catch (Exception r){
-                r.printStackTrace();
-            }
-
-
-
-
-            byte[] inputFrame = new byte[frameSize];
-        }
-
     }
 
     protected void onDestroy() {
@@ -121,46 +69,15 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
             this.surface = surface;
         }
 
-        private  MediaCodecInfo selectCodec(String mimeType) {
-            int numCodecs = MediaCodecList.getCodecCount();
-            for (int i = 0; i < numCodecs; i++) {
-                MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(i);
-
-                if (!codecInfo.isEncoder()) {
-                    continue;
-                }
-
-                String[] types = codecInfo.getSupportedTypes();
-                for (int j = 0; j < types.length; j++) {
-                    if (types[j].equalsIgnoreCase(mimeType)) {
-                        return codecInfo;
-                    }
-                }
-            }
-            return null;
-        }
 
 
 
         @Override
         public void run() {
-
-            //test();
-        }
-
-        public void work() {
-            int width = 328, height = 328;
-            int bitRate = 1000000;
-            int frameRate = 15;
-
             extractor = new MediaExtractor();
             AssetFileDescriptor testFd = getResources().openRawResourceFd(R.raw.test_video);
-            try {
-                extractor.setDataSource(testFd.getFileDescriptor(), testFd.getStartOffset(),
-                        testFd.getLength());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            extractor.setDataSource(testFd.getFileDescriptor(), testFd.getStartOffset(),
+                    testFd.getLength());
 
             for (int i = 0; i < extractor.getTrackCount(); i++) {
                 MediaFormat format = extractor.getTrackFormat(i);
@@ -178,24 +95,7 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
                 return;
             }
 
-            String mimeType = "video/avc";
-            ScreenShotUtils u = new ScreenShotUtils();
-            int numCodecs = MediaCodecList.getCodecCount();
-            MediaCodecInfo codecInfo = u.getMediaCodecInfo(mimeType, numCodecs);
-
-            MediaCodecInfo.CodecCapabilities capabilities = codecInfo.getCapabilitiesForType(mimeType);
-            int colorFormat = u.getCapabilities(capabilities);
-
-
-            width = u.calcWidth(codecInfo, width);
-            int stride = u.calcStrideForWidth(width, codecInfo);
-            int sliceHeight = u.calcSliceHeightForHeight(height, codecInfo);
-
             decoder.start();
-
-            int chromaStride = u.calcChromaStride(stride);
-            int frameSize = u.calcFrameStride(stride, sliceHeight, chromaStride);
-            byte[] inputFrame = u.createInputFrame(width, height, colorFormat, stride, sliceHeight, chromaStride, frameSize);
 
             ByteBuffer[] inputBuffers = decoder.getInputBuffers();
             ByteBuffer[] outputBuffers = decoder.getOutputBuffers();
@@ -205,10 +105,19 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
 
             while (!Thread.interrupted()) {
                 if (!isEOS) {
-                    int inIndex = decoder.dequeueInputBuffer(5000);
+                    int inIndex = decoder.dequeueInputBuffer(10000);
                     if (inIndex >= 0) {
                         ByteBuffer buffer = inputBuffers[inIndex];
+
                         int sampleSize = extractor.readSampleData(buffer, 0);
+                        StringBuilder b= new StringBuilder();
+
+                        for (int i = 0; i< buffer.remaining(); i++){
+                            char buf = (char) buffer.get(i);
+                            b.append(buf);
+                        }
+
+                        String r = b.toString();
                         if (sampleSize < 0) {
                             // We shouldn't stop the playback at this point, just pass the EOS
                             // flag to decoder, we will get it again from the
@@ -217,15 +126,13 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
                             decoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
                             isEOS = true;
                         } else {
-                            buffer.clear();
-                            buffer.put(inputFrame);
                             decoder.queueInputBuffer(inIndex, 0, sampleSize, extractor.getSampleTime(), 0);
                             extractor.advance();
                         }
                     }
                 }
 
-                int outIndex = decoder.dequeueOutputBuffer(info, 5000);
+                int outIndex = decoder.dequeueOutputBuffer(info, 10000);
                 switch (outIndex) {
                     case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
                         Log.d("DecodeActivity", "INFO_OUTPUT_BUFFERS_CHANGED");
@@ -267,4 +174,6 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
             extractor.release();
         }
     }
+
+
 }
