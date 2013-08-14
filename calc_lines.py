bc7b5af8384c07f2b16fe2d3677623939f57c695
Pavel Savyelyev <psavyelyev@luxoft.com>
2013-08-14 18:17:07 +0300

    Minor fixes in python script to calulate coverage by comments. This commit is not subject to review.

--- calc_lines.py
+++ calc_lines.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- mode: python; coding: utf-8 -*-
 # 
 # Calculates number of lines, number of commented lines and commens coverage
 # metric.
@@ -12,18 +11,10 @@ import string
 
 # Scan following directories (recursive)
 applink_source = ('src/appMain', 'src/components')
-# Scan files which have following extensions
-source_exts = 'c cc cpp cxx'.split()
-header_exts = 'h hpp'.split()
 
-#
-# Returns file exension
-#
-def getFileExt(file):
-    (root, ext) = os.path.splitext(file)
-    if len(ext) > 0:
-        return ext[1:]
-    return ''
+# Scan files which have following extensions
+source_exts = ('.c', '.cc', '.cpp', '.cxx')
+header_exts = ('.h', '.hpp')
 
 #
 # Returns list of c++ files in specified directory (recursively)
@@ -32,7 +23,7 @@ def getFiles(dir):
     fileList = [[], []]
     for root, dirs, files in os.walk(dir):
         for file in files:
-            fileExt = getFileExt(file)
+            (fileName, fileExt) = os.path.splitext(file)
             if fileExt in source_exts:
                 fileList[1].append(root + '/' + file)
             if fileExt in header_exts:
@@ -40,9 +31,9 @@ def getFiles(dir):
     return fileList
 
 #
-# Checks if comment is not empty
+# Checks if comment is not empty: contains any alphanumeric characters
 #
-def isCommentHasContent(comment):
+def hasContent(comment):
     alnum = set(string.letters + string.digits)
     return len(set(comment) & alnum) > 0
 
@@ -55,50 +46,51 @@ def proceedFile(file):
         
     f = open(file, 'r')
         
-    incomment = False
+    inComment = False
         
     for line in f:
         line = line.strip()
         if len(line) == 0:
             continue
+        
         # comment is opened with /*
-        if incomment:
-            end = line.find('*/')
-            if end < 0:
-                if isCommentHasContent(line):
+        if inComment:
+            indEnd = line.find('*/')
+            if indEnd < 0:
+                if hasContent(line):
                     countComments += 1
                 continue
             else:
-                if isCommentHasContent(line[:end]):
+                if hasContent(line[:indEnd]):
                     countComments += 1
-                incomment = False
-                line = line[end + 2:].strip()
+                inComment = False
+                line = line[indEnd + 2:].strip()
                 if len(line) < 1:
                     continue
 
-        ind = line.find('/*')
-        if ind >= 0:
-            incomment = True
-            ind2 = line[ind + 2:].find('*/')
+        indStart = line.find('/*')
+        if indStart >= 0:
+            inComment = True
+            indEnd = line[indStart + 2:].find('*/')
             #TODO: Lines as "/* ... */ /* ... */" won't be handled correctly
-            if ind2 >= 0:
-                incomment = False
-                if isCommentHasContent(line[ind + 2:ind + 2 + ind2]):
+            if indEnd >= 0:
+                inComment = False
+                if hasContent(line[indStart + 2:indStart + indEnd + 2]):
                     countComments += 1
-                if (ind > 0) or (ind2 + 2 < len(line)):
+                if (indStart > 0) or (indEnd + 2 < len(line)):
                     countLines += 1
             else:
-                if isCommentHasContent(line[ind + 2:]):
+                if hasContent(line[indStart + 2:]):
                     countComments += 1
-                if ind > 0:
+                if indStart > 0:
                     countLines += 1
             continue
         else:
-            ind = line.find('//')
-            if ind >= 0:
-                if isCommentHasContent(line[ind + 2:]):
+            indStart = line.find('//')
+            if indStart >= 0:
+                if hasContent(line[indStart + 2:]):
                     countComments += 1
-            if ind == 0:
+            if indStart == 0:
                 continue
         countLines += 1
     return [countLines, countComments]
@@ -107,40 +99,43 @@ def proceedFile(file):
 # Proceed files in the specified directory
 #
 def proceedDir(dir):
-    hLines = 0
-    cLines = 0
-    hCLines = 0
-    cCLines = 0
-    fileList = getFiles(dir)
-    for f in fileList[0]:
-        lines = proceedFile(f)
-        hLines += lines[0]
-        hCLines += lines[1]
-    for f in fileList[1]:
-        lines = proceedFile(f)
-        cLines += lines[0]
-        cCLines += lines[1]
+    linesHeaderCode = 0
+    linesHeaderComments = 0
+    linesSourceCode = 0
+    linesSourceComments = 0
+    (listHeaders, listSources) = getFiles(dir)
+    for f in listHeaders:
+        (sources, comments) = proceedFile(f)
+        linesHeaderCode += sources
+        linesHeaderComments += comments
+    for f in listSources:
+        (sources, comments) = proceedFile(f)
+        linesSourceCode += sources
+        linesSourceComments += comments
+
+    linesCode = linesHeaderCode + linesSourceCode
+    linesComments = linesHeaderComments + linesSourceComments
 
     print 'Directory: ' + dir
-    print 'Number of C++ header files: %d' % len(fileList[0])
-    print 'Number of C++ source files: %d' % len(fileList[1])
-    print 'Number of source code lines: %d (%d lines in header files, %d lines in source files)' % (hLines + cLines, hLines, cLines)
-    print 'Number of comment lines: %d (%d lines in header files, %d lines in source files)' % (hCLines + cCLines, hCLines, cCLines)
+    print 'Number of scanned C++ header files: %d' % len(listHeaders)
+    print 'Number of scanned C++ source files: %d' % len(listSources)
+    print 'Number of source code lines: %d (header files: %d, source files: %d)' % (linesCode, linesHeaderCode, linesSourceCode)
+    print 'Number of comment lines: %d (header files: %d, source files: %d)' % (linesComments, linesHeaderComments, linesSourceComments)
     print '----'
 
     pt = 0
     ph = 0
     pc = 0
-    if (hLines + cLines) > 0:
-        pt = (float)(hCLines + cCLines)/(float)(hLines + cLines) * 100.0
-    if (hLines + hLines) > 0:
-        ph = (float)(hCLines)/(float)(hLines) * 100.0
-    if (hLines + cLines) > 0:
-        pc = (float)(cCLines)/(float)(cLines) * 100.0
-    print 'Comments code coverage: %f%% (%f%% of headers, %f%% of sources)' % (pt, ph, pc)
+    if (linesCode) > 0:
+        pt = (float)(linesComments)/(float)(linesCode) * 100.0
+    if (linesHeaderCode) > 0:
+        ph = (float)(linesHeaderComments)/(float)(linesHeaderCode) * 100.0
+    if (linesSourceCode) > 0:
+        pc = (float)(linesSourceComments)/(float)(linesSourceCode) * 100.0
+    print 'Code coverage by comments: %f%% (header files: %f%%, source files: %f%%)' % (pt, ph, pc)
     print 
     
-    return [hCLines + cCLines, hLines + cLines]
+    return [linesComments, linesCode]
 
 #
 # main 
@@ -149,9 +144,9 @@ summary_lines = 0
 summary_comments = 0
 coverage = 0.0
 for dir in applink_source:
-    lines = proceedDir(dir)
-    summary_comments += lines[0]
-    summary_lines += lines[1]
+    (comments, code) = proceedDir(dir)
+    summary_comments += comments
+    summary_lines += code
 if summary_lines > 0:
     coverage = (float)(summary_comments) / (float)(summary_lines) * 100.0 
 
