86d1d983263c7ae30fb3fa99a90b0033e1f43b2a
Andrew_Batutin <abatutin@luxoft.com>
2013-08-20 14:27:56 +0300

    added test case for mobile navigation session class

---
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/test/proxy/session/MobileNavSessionTest.java
@@ -0,0 +1,16 @@
+package com.ford.syncV4.test.proxy.session;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Andrew Batutin on 8/20/13.
+ */
+public class MobileNavSessionTest extends AndroidTestCase {
+
+    public MobileNavSessionTest() {
+    }
+
+    public void testDumb() throws Exception {
+        assertTrue(false);
+    }
+}
---
+++ mobile/android/SyncProxyAndroid/src/com/ford/syncV4/proxy/session/MobileNavSession.java
@@ -0,0 +1,12 @@
+package com.ford.syncV4.proxy.session;
+
+/**
+ * Created by Andrew Batutin on 8/20/13.
+ * <p/>
+ * Abstraction Over Mobile Navigation Session Concept
+ */
+
+public class MobileNavSession {
+
+
+}
