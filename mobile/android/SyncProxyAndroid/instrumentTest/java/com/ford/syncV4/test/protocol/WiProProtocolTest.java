e755d91c926d82f23323f9d684891329a004d69b
Andrew_Batutin <abatutin@luxoft.com>
2013-08-21 18:29:09 +0300

    restored source of exeption for crazy test

--- mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/test/protocol/WiProProtocolTest.java
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/test/protocol/WiProProtocolTest.java
@@ -9,6 +9,8 @@ import com.ford.syncV4.protocol.ProtocolMessage;
 import com.ford.syncV4.protocol.WiProProtocol;
 import com.ford.syncV4.protocol.enums.SessionType;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Hashtable;
 
@@ -25,6 +27,7 @@ import static org.mockito.Mockito.when;
  */
 public class WiProProtocolTest extends InstrumentationTestCase {
 
+    Method currentCheckMethod;
     private WiProProtocol sut;
     private ProtocolFrameHeader currentFrameHeader;
     private byte[] currentData;
@@ -54,11 +57,7 @@ public class WiProProtocolTest extends InstrumentationTestCase {
             @Override
             protected void handleProtocolFrameToSend(ProtocolFrameHeader header, byte[] data, int offset, int length) {
                 super.handleProtocolFrameToSend(header, data, offset, length);
-                try {
-                    checkCurrentArguments(data, header);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                callCheck(currentCheckMethod, data, header);
             }
         };
         sut.setVersion((byte) 2);
@@ -68,6 +67,7 @@ public class WiProProtocolTest extends InstrumentationTestCase {
         ProtocolMessage message = generateMobileNavProtocolMessage(8);
         currentData = generateByteArray(8);
         currentFrameHeader = ProtocolFrameHeaderFactory.createSingleSendData(SessionType.Mobile_Nav, (byte) 48, currentData.length, 1, (byte) 2);
+        currentCheckMethod = generateCurrentCheckMethod("checkCurrentArgumentsSmallFrame");
         sut.SendMessage(message);
     }
 
@@ -75,7 +75,16 @@ public class WiProProtocolTest extends InstrumentationTestCase {
         ProtocolMessage message = generateMobileNavProtocolMessage(WiProProtocol.MAX_DATA_SIZE * 2);
         currentData = generateByteArray(WiProProtocol.MAX_DATA_SIZE * 2);
         currentFrameHeader = ProtocolFrameHeaderFactory.createSingleSendData(SessionType.Mobile_Nav, (byte) 48, currentData.length, 1, (byte) 2);
+        currentCheckMethod = generateCurrentCheckMethod("checkCurrentArgumentsSmallBigFrame");
+        //callCheck(currentCheckMethod, null, null);
         sut.SendMessage(message);
+    }
+
+    private Method generateCurrentCheckMethod(String checkMethodName) throws NoSuchMethodException {
+        Class[] parameterTypes = new Class[2];
+        parameterTypes[0] = byte[].class;
+        parameterTypes[1] = ProtocolFrameHeader.class;
+        return WiProProtocolTest.class.getMethod(checkMethodName, parameterTypes);
     }
 
     private ProtocolMessage generateMobileNavProtocolMessage(int i) {
@@ -96,8 +105,46 @@ public class WiProProtocolTest extends InstrumentationTestCase {
         return b;
     }
 
-    public void checkCurrentArguments(byte[] data, ProtocolFrameHeader messageHeader) throws Exception {
+    private void callCheck(Method method, byte[] data, ProtocolFrameHeader messageHeader) {
+        Object[] parameters = new Object[2];
+        parameters[0] = data;
+        parameters[1] = messageHeader;
+        try {
+            method.invoke(WiProProtocolTest.this, parameters);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            reconstructAssertionError(e);
+        }
+    }
+
+    private void reconstructAssertionError(InvocationTargetException e) {
+        String methodName = getMethodName(e);
+        assertNull("Should not get here. " + e.getCause().getMessage() + " " + methodName, e);
+    }
+
+    private String getMethodName(InvocationTargetException e) {
+        String methodName = "";
+        if (e.getCause() != null && e.getCause().getStackTrace() != null && e.getCause().getStackTrace().length > 0) {
+            for (int i = 0; i < e.getCause().getStackTrace().length; i++) {
+                if (e.getCause().getStackTrace()[i].toString().contains(this.getClass().getName())) {
+                    methodName = e.getCause().getStackTrace()[i].toString();
+                    break;
+                }
+            }
+        }
+        return methodName;
+    }
+
+    public void checkCurrentArgumentsSmallFrame(byte[] data, ProtocolFrameHeader messageHeader) throws Exception {
         assertTrue(Arrays.equals(currentData, data));
         assertEquals(currentFrameHeader, messageHeader);
+    }
+
+    public void checkCurrentArgumentsSmallBigFrame(byte[] data, ProtocolFrameHeader messageHeader) throws Exception {
+        if (messageHeader.getMessageID() == 1) {
+            assertTrue("Arrays should be equal.", Arrays.equals(currentData, data));
+            assertEquals("Frame headers should be equal.", currentFrameHeader, messageHeader);
+        }
     }
 }
