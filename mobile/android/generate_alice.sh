9abe5a9aa6d6d202c3fdac85813fa43a55f26d22
Eugene Nikolsky <enikolskii@luxoft.com>
2013-08-23 16:54:00 +0300

    APPLINK-2674: add support for second SPT in gradle

--- mobile/android/generate_alice.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rsync -avc --inplace --delete-excluded --progress --exclude='out/' --exclude='gen/' --exclude='bin/' SyncProxyTester/ SPTAlice
-perl -i -p -e 's/com\.ford\.syncV4\.android/com.ford.syncV4.SPTAlice/g' $(ack -lQ 'com.ford.syncV4.android' SPTAlice)
-perl -i -p -e 's/SyncProxyTester/SPTAlice/g' SPTAlice/res/values/strings.xml SPTAlice/src/com/ford/syncV4/android/constants/Const.java
-mv SPTAlice/src/com/ford/syncV4/{android,SPTAlice}
-
