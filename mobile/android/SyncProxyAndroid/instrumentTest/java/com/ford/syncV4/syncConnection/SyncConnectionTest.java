0aff27e3aa11d89fe27491eb30e45b9ecae7292b
Andrew Batutin <abatutin@luxoft.com>
2013-08-29 12:36:19 +0300

    fixed test case for end session

--- mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/syncConnection/SyncConnectionTest.java
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/syncConnection/SyncConnectionTest.java
@@ -33,7 +33,7 @@ public class SyncConnectionTest extends InstrumentationTestCase {
         super.setUp();
         System.setProperty("dexmaker.dexcache", getInstrumentation().getTargetContext().getCacheDir().getPath());
         sut = new SyncConnection(mock(ISyncConnectionListener.class), mock(BaseTransportConfig.class));
-        WiProProtocol protocol = (WiProProtocol)sut.getWiProProtocol();
+        WiProProtocol protocol = (WiProProtocol) sut.getWiProProtocol();
         protocol.setVersion(VERSION);
     }
 
@@ -42,46 +42,48 @@ public class SyncConnectionTest extends InstrumentationTestCase {
         assertNotNull("should not be null", connection);
     }
 
-    public void testStartMobileNavSessionShouldSendAppropriateBytes() throws Exception{
+    public void testStartMobileNavSessionShouldSendAppropriateBytes() throws Exception {
         final ProtocolFrameHeader header = ProtocolFrameHeaderFactory.createStartSession(SessionType.Mobile_Nav, 0x00, VERSION);
-        final SyncConnection connection = new SyncConnection(mock(ISyncConnectionListener.class), mock(BaseTransportConfig.class)){
+        final SyncConnection connection = new SyncConnection(mock(ISyncConnectionListener.class), mock(BaseTransportConfig.class)) {
 
             @Override
             public void onProtocolMessageBytesToSend(byte[] msgBytes, int offset,
-                                                     int length){
-                super.onProtocolMessageBytesToSend(msgBytes,offset,length);
+                                                     int length) {
+                super.onProtocolMessageBytesToSend(msgBytes, offset, length);
                 assertTrue("Arrays should be equal", Arrays.equals(msgBytes, header.assembleHeaderBytes()));
                 assertEquals("Offset should be 0", offset, 0);
                 assertEquals("Length should be 12", length, 12);
             }
         };
-        WiProProtocol protocol = (WiProProtocol)connection.getWiProProtocol();
+        WiProProtocol protocol = (WiProProtocol) connection.getWiProProtocol();
         protocol.setVersion(VERSION);
         connection.startMobileNavSession();
     }
 
     public void testOnTransportBytesReceivedReturnedStartSessionACK() throws Exception {
         final ProtocolFrameHeader header = ProtocolFrameHeaderFactory.createStartSessionACK(SessionType.Mobile_Nav, SESSION_ID, MESSAGE_ID, VERSION);
-        final SyncConnection connection = new SyncConnection(mock(ISyncConnectionListener.class), mock(BaseTransportConfig.class)){
+        final SyncConnection connection = new SyncConnection(mock(ISyncConnectionListener.class), mock(BaseTransportConfig.class)) {
 
             @Override
             public void onProtocolSessionStarted(SessionType sessionType, byte sessionID, byte version, String correlationID) {
                 super.onProtocolSessionStarted(sessionType, sessionID, version, correlationID);
-                assertEquals("Correlation ID is empty string so far", "",correlationID);
+                assertEquals("Correlation ID is empty string so far", "", correlationID);
                 assertEquals("SessionType should be equal.", header.getSessionType(), sessionType);
-                assertEquals("Frame headers should be equal.", header.getSessionID(),sessionID);
+                assertEquals("Frame headers should be equal.", header.getSessionID(), sessionID);
                 assertEquals("Version should be equal.", header.getVersion(), version);
             }
         };
-        WiProProtocol protocol = (WiProProtocol)connection.getWiProProtocol();
+        WiProProtocol protocol = (WiProProtocol) connection.getWiProProtocol();
         protocol.setVersion(VERSION);
         connection.onTransportBytesReceived(header.assembleHeaderBytes(), header.assembleHeaderBytes().length);
     }
 
     public void testCloseMobileNavSessionShouldSendAppropriateBytes() throws Exception {
-        byte[] data = BitConverter.intToByteArray(0xCDEF1234);
+        byte[] data = BitConverter.intToByteArray(0);
         final ProtocolFrameHeader header = ProtocolFrameHeaderFactory.createEndSession(SessionType.Mobile_Nav, SESSION_ID, 0, VERSION, data.length);
-        final SyncConnection connection = new SyncConnection(mock(ISyncConnectionListener.class), mock(BaseTransportConfig.class)){
+        final SyncConnection connection = new SyncConnection(mock(ISyncConnectionListener.class), mock(BaseTransportConfig.class)) {
+
+            private int count = 0;
 
             @Override
             public void closeMobileNavSession(byte rpcSessionID) {
@@ -92,14 +94,17 @@ public class SyncConnectionTest extends InstrumentationTestCase {
 
             @Override
             public void onProtocolMessageBytesToSend(byte[] msgBytes, int offset,
-                                                     int length){
-                super.onProtocolMessageBytesToSend(msgBytes,offset,length);
-                assertTrue("Arrays should be equal", Arrays.equals(msgBytes, header.assembleHeaderBytes()));
-                assertEquals("Offset should be 0", offset, 0);
-                assertEquals("Length should be 12", length, 12);
+                                                     int length) {
+                super.onProtocolMessageBytesToSend(msgBytes, offset, length);
+                if (count == 0) {
+                    assertTrue("Arrays should be equal", Arrays.equals(msgBytes, header.assembleHeaderBytes()));
+                    assertEquals("Offset should be 0", offset, 0);
+                    assertEquals("Length should be 12", length, 12);
+                    count++;
+                }
             }
         };
-        WiProProtocol protocol = (WiProProtocol)connection.getWiProProtocol();
+        WiProProtocol protocol = (WiProProtocol) connection.getWiProProtocol();
         protocol.setVersion(VERSION);
         connection.closeMobileNavSession(SESSION_ID);
 
