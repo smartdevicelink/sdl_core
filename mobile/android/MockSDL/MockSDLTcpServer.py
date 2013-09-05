bfbc34b173a18f14080adbce1fa9e9abeec5f638
Andrew Batutin <abatutin@luxoft.com>
2013-09-05 09:53:59 +0300

    APPLINK-2939: added mock mobile navi session ack response to SPT

--- mobile/android/MockSDL/MockSDLTcpServer.py
+++ mobile/android/MockSDL/MockSDLTcpServer.py
@@ -1,14 +1,14 @@
-
 import socket
 
 
-TCP_IP = '192.168.42.8'
+TCP_IP = '192.168.42.254'
 TCP_PORT = 12345
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
-def main():
+
+def startMobileNavSessionAck():
     """
-    >>> main()
+    >>> startMobileNavSessionAck()
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
@@ -20,9 +20,10 @@ def main():
         data = conn.recv(BUFFER_SIZE)
         if not data: break
         print "received data: %r" % data
-        if compareMobileNavData(data[:len(startMobileNavSession())-1]):
+        if compareMobileNavData(data):
             conn.send(startMobileNaviSessionACK())
     conn.close()
+
 
 def startSessionAck():
     return str(bytearray([32, 7, 2, 48, 0, 0, 0, 0, 0, 0, 0, 48]))
@@ -38,10 +39,12 @@ def startMobileNaviSessionACK():
 
 def compareMobileNavData(expected):
     """test
-    >>> res = compareMobileNavData(startMobileNavSession())
+    >>> compareMobileNavData(startMobileNavSession())
     True
     """
-    actual = startMobileNavSession()
+    if len(expected) > 3:
+        expected = expected[:3]
+    actual = startMobileNavSession()[:3]
     res = expected == actual
     return res
 
