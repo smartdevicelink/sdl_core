adc3897c6641ab7e439ec8997c8659bfc05e6b50
Andriy Melnik <amelnik@luxoft.com>
2013-10-16 16:44:32 +0300

    HMI: deactivateApp behavior changed

--- src/components/HMI/app/controller/sdl/Controller.js
+++ src/components/HMI/app/controller/sdl/Controller.js
@@ -398,11 +398,6 @@ SDL.SDLController = Em.Object
         unregisterApplication: function(appID) {
 
             this.getApplicationModel(appID).onDeleteApplication(appID);
-            this.getApplicationModel(appID).set('active', false);
-            var index = SDL.SDLModel.registeredApps
-                .indexOf(SDL.SDLModel.registeredApps.filterProperty('appID',
-                    appID)[0]);
-            SDL.SDLModel.registeredApps.replace(index, 1);
             this.set('model', null);
         },
         /**
