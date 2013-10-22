8e83fa67b473ad006444841ceaa4a1a010c12d9c
nkhlopkov <nkhlopkov@nkhlopkov-VirtualBox.(none)>
2013-10-22 18:11:44 +0400

    TM socket connection, just logging added trivial change, not a subject for review

--- src/components/transport_manager/src/transport_adapter/threaded_socket_connection.cc
+++ src/components/transport_manager/src/transport_adapter/threaded_socket_connection.cc
@@ -223,7 +223,7 @@ void ThreadedSocketConnection::Transmit() {
     LOG4CXX_INFO(logger_, "exit");
     return;
   }
-  LOG4CXX_INFO(logger_, "poll is ok (#" << pthread_self() << ") " << this);
+  LOG4CXX_INFO(logger_, "poll is ok (#" << pthread_self() << ") " << this << " revents0:" << std::hex << poll_fds[0].revents << " revents1:" << std::hex << poll_fds[1].revents);
   // error check
   if (0 != (poll_fds[1].revents & (POLLERR | POLLHUP | POLLNVAL))) {
     LOG4CXX_ERROR(logger_,
