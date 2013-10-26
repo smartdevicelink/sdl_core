fd5a5a21bbaf4517db2d8fb2ae67d8d0e15ee76f
amelnik <amelnik@luxoft.com>
2013-10-26 05:30:43 -0700

    HMI: APPLINK-3600 fixed

--- src/components/HMI/app/view/sdl/shared/tbtTurnList.js
+++ src/components/HMI/app/view/sdl/shared/tbtTurnList.js
@@ -82,7 +82,7 @@ SDL.TBTTurnList = SDL.SDLAbstractView.create({
             for (var i = 0; i < length; i++) {
                 this.get('tbtTurnListList.list.childViews').pushObject(SDL.Label.create({
                         icon        : turnListArray[i].turnIcon ? turnListArray[i].turnIcon.value : "",
-                        content     : turnListArray[i].navigationText,
+                        content     : turnListArray[i].navigationText.fieldText,
                         templateName: turnListArray[i].turnIcon ? 'icon' : 'text',
                         classNames  : 'list-item'
                     }));
@@ -90,6 +90,9 @@ SDL.TBTTurnList = SDL.SDLAbstractView.create({
             if (SDL.SDLController.getApplicationModel(appID).turnListSoftButtons) {
                 turnListArray = SDL.SDLController.getApplicationModel(appID).turnListSoftButtons;
                 length = turnListArray.length;
+            } else {
+                length = null;
+                turnListArray = [];
             }
             for (var i = 0; i < length; i++) {
                 this.get('tbtTurnListList.list.childViews').pushObject(SDL.Button.create(SDL.PresetEventsCustom, {
