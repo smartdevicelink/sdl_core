b46a4e5df887a3293aa5a7c1a24f3a6e6c5a95f6
YuriyChernyshov <IChernyshov@luxoft.com>
2013-11-21 17:16:30 +0200

    REVSDL-89-Add new classes to path GrandAccess to the web part of the mobile app

---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/responses/GrantAccessResponse.java
@@ -0,0 +1,10 @@
+package com.ford.avarsdl.responses;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11/21/13
+ * Time: 4:31 PM
+ */
+public class GrantAccessResponse {
+}
---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/responses/ResponseCommand.java
@@ -0,0 +1,10 @@
+package com.ford.avarsdl.responses;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11/21/13
+ * Time: 4:30 PM
+ */
+public class ResponseCommand {
+}
