5b48db25b2b9b4ce720bce4c85d269205894bfcb
YuriyChernyshov <IChernyshov@luxoft.com>
2013-11-26 15:27:46 +0200

    REVSDL-105-Add CancelAccessComand to the project

---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/requests/CancelAccessCommand.java
@@ -0,0 +1,10 @@
+package com.ford.avarsdl.requests;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11/26/13
+ * Time: 11:41 AM
+ */
+public class CancelAccessCommand {
+}
