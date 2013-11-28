f8ea350b1c63cc4ee57109966afd9b9f3dc5a692
YuriyChernyshov <IChernyshov@luxoft.com>
2013-11-28 12:28:38 +0200

    REVSDL-113-Implement Tune Up / Down commands

---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/requests/TuneDownCommand.java
@@ -0,0 +1,11 @@
+package com.ford.avarsdl.requests;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11/28/13
+ * Time: 12:26 PM
+ */
+public class TuneDownCommand {
+
+}
---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/requests/TuneUpCommand.java
@@ -0,0 +1,11 @@
+package com.ford.avarsdl.requests;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11/28/13
+ * Time: 12:24 PM
+ */
+public class TuneUpCommand {
+
+}
