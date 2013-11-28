eb00ab282c1e8dd93831107e73065dddf6ebf07c
YuriyChernyshov <IChernyshov@luxoft.com>
2013-11-28 12:28:53 +0200

    REVSDL-113-Inject Tune Up / Down commands into project

--- native/android/avatar/src/main/java/com/ford/avarsdl/jsoncontroller/JSONRevSDLController.java
+++ native/android/avatar/src/main/java/com/ford/avarsdl/jsoncontroller/JSONRevSDLController.java
@@ -5,7 +5,9 @@ import com.ford.avarsdl.requests.GrantAccessCommand;
 import com.ford.avarsdl.requests.RequestCommand;
 import com.ford.avarsdl.requests.StartScanCommand;
 import com.ford.avarsdl.requests.StopScanCommand;
+import com.ford.avarsdl.requests.TuneDownCommand;
 import com.ford.avarsdl.requests.TuneRadioCommand;
+import com.ford.avarsdl.requests.TuneUpCommand;
 import com.ford.avarsdl.util.Logger;
 import com.ford.avarsdl.util.RPCConst;
 import com.ford.syncV4.proxy.constants.Names;
@@ -57,5 +59,11 @@ public class JSONRevSDLController extends JSONController {
 
         TuneRadioCommand tuneRadioCommand = new TuneRadioCommand();
         commandsHashTable.put(Names.TuneRadio, tuneRadioCommand);
+
+        TuneUpCommand tuneUpCommand = new TuneUpCommand();
+        commandsHashTable.put(Names.TuneUp, tuneUpCommand);
+
+        TuneDownCommand tuneDownCommand = new TuneDownCommand();
+        commandsHashTable.put(Names.TuneDown, tuneDownCommand);
     }
 }
--- native/android/avatar/src/main/java/com/ford/avarsdl/requests/TuneUpCommand.java
+++ native/android/avatar/src/main/java/com/ford/avarsdl/requests/TuneUpCommand.java
@@ -1,11 +1,35 @@
 package com.ford.avarsdl.requests;
 
+import com.ford.avarsdl.service.SDLService;
+import com.ford.avarsdl.util.Logger;
+import com.ford.syncV4.exception.SyncException;
+import com.ford.syncV4.proxy.SyncProxyALM;
+import com.ford.syncV4.proxy.rpc.TuneUp;
+
+import org.json.JSONObject;
+
 /**
  * Created with Android Studio.
  * Author: Chernyshov Yuriy - Mobile Development
  * Date: 11/28/13
  * Time: 12:24 PM
  */
-public class TuneUpCommand {
+public class TuneUpCommand implements RequestCommand {
 
+    @Override
+    public void execute(int id, JSONObject jsonParameters) {
+        SyncProxyALM proxy = SDLService.getProxyInstance();
+        if (proxy != null) {
+            TuneUp msg = new TuneUp();
+            msg.setCorrelationID(id);
+
+            try {
+                proxy.sendRPCRequest(msg);
+            } catch (SyncException e) {
+                Logger.e(getClass().getSimpleName() + " can't send message", e);
+            }
+        } else {
+            Logger.e(getClass().getSimpleName() + " proxy is NULL");
+        }
+    }
 }
