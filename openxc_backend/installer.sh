700dd4eda289ea624933c485e9aaa801ece8b417
YuriyChernyshov <IChernyshov@luxoft.com>
2013-11-29 15:30:08 +0200

    Update installer

--- openxc_backend/installer.sh
+++ openxc_backend/installer.sh
@@ -5,6 +5,11 @@ command -v python >/dev/null 2>&1 || { echo >&2 "Python is not installed. Instal
 sudo apt-get install python; }
 echo "   Python is installed"
 
+echo "Check Python Pip installation ..."
+command -v python-pip >/dev/null 2>&1 || { echo >&2 "Python Pip is not installed. Installing Python Pip now ...";
+sudo apt-get install python-pip; }
+echo "   Python Pip is installed"
+
 echo "Check OpenXC installation ..."
 command -v openxc-control version >/dev/null 2>&1 || { echo >&2 "   OpenXC is not installed. Installing OpenXC now ...";
 sudo pip install -U openxc; }
