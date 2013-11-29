a65d831a06c6879698e96a8b2e1183a329e641d4
YuriyChernyshov <IChernyshov@luxoft.com>
2013-11-29 14:48:38 +0200

    REVSDL-126-Implement CancelAccess response

---
+++ native/android/avatar/src/main/java/com/ford/avarsdl/responses/CancelAccessResponse.java
@@ -0,0 +1,11 @@
+package com.ford.avarsdl.responses;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11/29/13
+ * Time: 2:34 PM
+ */
+public class CancelAccessResponse {
+
+}
