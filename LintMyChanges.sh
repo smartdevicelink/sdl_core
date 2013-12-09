2b2bb8d38af1ebbe1121a866decbef088b1c5258
Kozyrenko, Igor <ikozyrenko@luxoft.com>
2013-12-09 18:20:10 +0200

    Reuse code

--- LintMyChanges.sh
+++ LintMyChanges.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ "$1" == "all" ]; then
+	CHECKSCOPE=ALL
+else
+	CHECKSCOPE=MY
+fi
+
 set -e
 
 MACHINE_TYPE=`uname -m`
@@ -21,11 +27,16 @@ echo "--i\"/usr/include/glib-2.0\"" >> ./tools/FlexeLint/gcc-include-path.lnt
 echo "--i\"/usr/lib/i386-linux-gnu/glib-2.0/include\"" >> ./tools/FlexeLint/gcc-include-path.lnt
 echo "--i\"/usr/lib/x86_64-linux-gnu/glib-2.0/include\"" >> ./tools/FlexeLint/gcc-include-path.lnt
 
-(git diff --name-only HEAD ; git ls-files --other --exclude-standard) | grep '.cc\|.cpp' | grep 'src/components\|src/appMain' | awk '{print "../../" $0}' > ./tools/FlexeLint/sdl-changed-modules.lnt
-
-cd ./tools/FlexeLint/
-
-./${FLINT_BINARY} -w1 -zero -u --i../../$1/src/components/ -dOS_POSIX ${WORDSIZE} smartdevicelink-changes.lnt 
+if [ $CHECKSCOPE == "ALL" ]; then
+  find ./src/components/ ./src/appMain/  \( -name "*.cc" -o -name "*.cpp" \) | awk '{print "../../" $0}' > ./tools/FlexeLint/sdl-modules.lnt
+  cd ./tools/FlexeLint/
+  #./${FLINT_BINARY} -w1 -zero -u --i../../$1/src/components/ -dOS_POSIX ${WORDSIZE} smartdevicelink.lnt
+  ./${FLINT_BINARY}  $3 -e830 -e831 -zero -u --i../../$1/src/components/ -os flexelint_result.txt -dOS_POSIX ${WORDSIZE} $2 smartdevicelink.lnt
+else
+  (git diff --name-only HEAD ; git ls-files --other --exclude-standard) | grep '.cc\|.cpp' | grep 'src/components\|src/appMain' | awk '{print "../../" $0}' > ./tools/FlexeLint/sdl-changed-modules.lnt
+  cd ./tools/FlexeLint/
+  ./${FLINT_BINARY} -w1 -zero -u --i../../$1/src/components/ -dOS_POSIX ${WORDSIZE} smartdevicelink-changes.lnt
+fi # if [ $CHECKSCOPE == "ALL" ]
 
 rm ./sdl-include-path.lnt
 rm ./sdl-changed-modules.lnt
