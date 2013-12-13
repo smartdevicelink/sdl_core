a8096be1c4de8c052b8aff68cb923737664dc27b
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-13 16:24:41 +0200

    APPLINK-4613-Test class for the RPCMessage parsing improvement.

---
+++ mobile/android/SyncProxyTester/instrumentTest/java/com/ford/syncV4/proxy/RPCMessageTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.proxy;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/13/13
+ * Time: 12:18 PM
+ */
+public class RPCMessageTest {
+
+}
