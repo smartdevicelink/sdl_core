9bc1edbfd2c3b4e94f067ddf23f50baf44c44337
Pavel Kramchaninov <PKramchaninov@luxoft.com>
2013-12-18 14:29:57 +0200

    Add possibility to lint single file

--- LintMyChanges.sh
+++ LintMyChanges.sh
@@ -5,6 +5,9 @@ set -e
 if [ "$1" == "all" ]; then
 	CHECKSCOPE=ALL
 	shift
+elif [ "$1" == "file" ]; then
+	CHECKSCOPE=FILE
+	shift
 else
 	CHECKSCOPE=MY
 fi
@@ -34,6 +37,13 @@ if [ "$CHECKSCOPE" == "ALL" ]; then
   #./${FLINT_BINARY} -w1 -zero -u --i../../$1/src/components/ -dOS_POSIX ${WORDSIZE} smartdevicelink.lnt
   ./${FLINT_BINARY}  $3 -e830 -e831 -zero -u --i../../$1/src/components/ -os flexelint_result.txt -dOS_POSIX ${WORDSIZE} $2 smartdevicelink.lnt
   rm ./sdl-modules.lnt
+elif [ "$CHECKSCOPE" == "FILE" ]; then
+  echo "$1" | awk '{print "../../" $0}' > ./tools/FlexeLint/sdl-modules.lnt
+  shift
+  cd ./tools/FlexeLint/
+  #./${FLINT_BINARY} -w1 -zero -u --i../../$1/src/components/ -dOS_POSIX ${WORDSIZE} smartdevicelink.lnt
+  ./${FLINT_BINARY}  $3 -e830 -e831 -zero -u --i../../$1/src/components/ -os flexelint_result.txt -dOS_POSIX ${WORDSIZE} $2 smartdevicelink.lnt
+  #rm ./sdl-modules.lnt
 else
   (git diff --name-only HEAD ; git ls-files --other --exclude-standard) | grep '.cc\|.cpp' | grep 'src/components\|src/appMain' | awk '{print "../../" $0}' > ./tools/FlexeLint/sdl-changed-modules.lnt
   cd ./tools/FlexeLint/
