05817880d0deb7be8df39c2452160663fbef1572
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-18 11:36:18 +0200

    APPLINK-2836-Add interface to describe application exit procedure

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/service/ProxyServiceEvent.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.service;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/17/13
+ * Time: 3:46 PM
+ */
+public interface ProxyServiceEvent {
+
+}
