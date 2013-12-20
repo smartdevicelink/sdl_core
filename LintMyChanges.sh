22b615453aaeafdab21af61d98a65cc50ec12505
Pavel Kramchaninov <PKramchaninov@luxoft.com>
2013-12-20 16:58:53 +0200

    Temporary exclude dbus, qnx an qt code.

--- LintMyChanges.sh
+++ LintMyChanges.sh
@@ -30,9 +30,15 @@ echo "--i\"/usr/include/gstreamer-1.0\"" >> ./tools/FlexeLint/gcc-include-path.l
 echo "--i\"/usr/include/glib-2.0\"" >> ./tools/FlexeLint/gcc-include-path.lnt
 echo "--i\"/usr/lib/i386-linux-gnu/glib-2.0/include\"" >> ./tools/FlexeLint/gcc-include-path.lnt
 echo "--i\"/usr/lib/x86_64-linux-gnu/glib-2.0/include\"" >> ./tools/FlexeLint/gcc-include-path.lnt
+echo "--i\"/usr/include/dbus-1.0\"" >> ./tools/FlexeLint/gcc-include-path.lnt
+echo "--i\"/usr/include/dbus-1.0\"" >> ./tools/FlexeLint/gcc-include-path.lnt
+echo "--i\"/usr/lib/i386-linux-gnu/dbus-1.0/include\"" >> ./tools/FlexeLint/gcc-include-path.lnt
+echo "--i\"/usr/lib/x86_64-linux-gnu/dbus-1.0/include\"" >> ./tools/FlexeLint/gcc-include-path.lnt
+
+EXLUDE_FROM_SOURCE_CODE_PATH="./src/components/qt_hmi/\|./src/components/dbus/\|./src/components/transport_manager/include/transport_manager/usb/qnx/"
 
 if [ "$CHECKSCOPE" == "ALL" ]; then
-  find ./src/components/ ./src/appMain/  \( -name "*.cc" -o -name "*.cpp" \) | awk '{print "../../" $0}' > ./tools/FlexeLint/sdl-modules.lnt
+  find ./src/components/ ./src/appMain/  \( -name "*.cc" -o -name "*.cpp" \) | awk '{print "../../" $0}' | grep -v $EXLUDE_FROM_SOURCE_CODE_PATH > ./tools/FlexeLint/sdl-modules.lnt
   cd ./tools/FlexeLint/
   #./${FLINT_BINARY} -w1 -zero -u --i../../$1/src/components/ -dOS_POSIX ${WORDSIZE} smartdevicelink.lnt
   ./${FLINT_BINARY}  $3 -e830 -e831 -zero -u --i../../$1/src/components/ -os flexelint_result.txt -dOS_POSIX ${WORDSIZE} $2 smartdevicelink.lnt
