85755e829d575bde7cb79bcc662aa87e170dd287
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-24 16:55:28 +0200

    Add NSDHelper class

---
+++ mobile/android/SyncProxyAndroid/src/com/ford/syncV4/transport/nsd/NSDHelper.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.transport.nsd;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/24/13
+ * Time: 2:39 PM
+ */
+public class NSDHelper {
+
+}
