2e7bd4b7d5eb01efd5bdab60e5359a528848820a
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-24 13:51:37 +0200

    Create dialog fragment for the app settings

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/AppSetUpDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/24/13
+ * Time: 1:32 PM
+ */
+public class AppSetUpDialog {
+
+}
