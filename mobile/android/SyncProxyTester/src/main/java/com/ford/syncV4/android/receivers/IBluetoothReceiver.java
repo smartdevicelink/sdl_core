3b6cd279861ad63d55fba6391453e98e803a87bb
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-26 18:09:17 +0200

    Implement BT Manager and it's interface

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/receivers/IBluetoothReceiver.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.receivers;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/26/13
+ * Time: 5:39 PM
+ */
+public interface IBluetoothReceiver {
+    void onBluetoothOn();
+}
