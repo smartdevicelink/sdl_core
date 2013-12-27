c46611948404831447ee653c8b2510a44e51e717
YuriyChernyshov <IChernyshov@luxoft.com>
2013-12-27 13:09:05 +0200

    APPLINK-4459-Add Preference manager to hole all preferences requests

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/manager/AppPreferencesManager.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.manager;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12/27/13
+ * Time: 10:51 AM
+ */
+public class AppPreferencesManager {
+
+}
