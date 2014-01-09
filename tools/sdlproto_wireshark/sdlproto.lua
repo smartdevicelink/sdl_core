72ab21fdeb3c1017cff893571e2da64060525b7d
Eugene Nikolsky <enikolskii@luxoft.com>
2014-01-09 10:47:17 +0200

    Parse Frame Info based on Frame Type

---
+++ tools/sdlproto_wireshark/sdlproto.lua
@@ -0,0 +1,100 @@
+-- Put this file into ~/.wireshark/plugins/ directory
+-- source: https://delog.wordpress.com/2010/09/27/create-a-wireshark-dissector-in-lua/
+
+-- create sdlproto protocol and its fields
+local p_protoname = "sdlproto"
+p_sdlproto = Proto(p_protoname, "Ford's Smart Device Link Protocol")
+
+local f_frameTypeValue_ControlFrame = 0x00
+local f_frameTypeValue_ConsecutiveFrame = 0x03
+local f_frameTypeValues = {
+  [f_frameTypeValue_ControlFrame] = "Control Frame",
+  [0x01] = "Single Frame",
+  [0x02] = "First Frame",
+  [f_frameTypeValue_ConsecutiveFrame] = "Consecutive Frame"
+  -- TODO check the values not in the list
+}
+local f_serviceTypeValues = {
+  [0x07] = "Remote Procedure Call [RPC Service]",
+  [0x0F] = "Bulk Data [Hybrid Service]"
+  -- TODO check the values not in the list
+}
+local f_frameInfoValues_ControlFrame = {
+  -- this only applies when f_frameType == ControlFrame
+  [0x00] = "Heart Beat",
+  [0x01] = "Start Service",
+  [0x02] = "Start Service ACK",
+  [0x03] = "Start Service NACK",
+  [0x04] = "End Service",
+  [0x05] = "End Service ACK",
+  [0x06] = "End Service NACK",
+  [0xFE] = "Service Data ACK",
+  [0xFF] = "Heart Beat ACK"
+  -- TODO check the values not in the list
+}
+local f_frameInfoValues_ConsecutiveFrame = {
+  -- this only applies when f_frameType == ConsecutiveFrame
+  [0x00] = "Last frame in sequence"
+  -- TODO check the values not in the list
+}
+
+local f_version = ProtoField.uint8(p_protoname..".version", "Protocol Version", base.DEC, nil, 0xF0)
+local f_compressionFlag = ProtoField.bool(p_protoname..".compression_flag", "Compression Flag", 8, nil, 0x08)
+local f_frameType = ProtoField.uint8(p_protoname..".frame_type", "Frame Type", base.HEX, f_frameTypeValues, 0x07)
+local f_serviceType = ProtoField.uint8(p_protoname..".service_type", "Service Type", base.HEX, f_serviceTypeValues)
+local f_frameInfo = ProtoField.uint8(p_protoname..".frame_info", "Frame Info", base.HEX)
+local f_frameInfo_ControlFrame = ProtoField.uint8(p_protoname..".frame_info", "Frame Info", base.HEX, f_frameInfoValues_ControlFrame)
+local f_frameInfo_ConsecutiveFrame = ProtoField.uint8(p_protoname..".frame_info", "Frame Info", base.HEX, f_frameInfoValues_ConsecutiveFrame)
+local f_sessionID = ProtoField.uint8(p_protoname..".session_id", "Session ID", base.HEX)
+
+p_sdlproto.fields = {
+  f_version, f_compressionFlag, f_frameType, f_serviceType,
+  f_frameInfo, f_frameInfo_ControlFrame, f_frameInfo_ConsecutiveFrame,
+  f_sessionID
+}
+
+-- sdlproto dissector function
+function p_sdlproto.dissector(buf, pkt, root)
+  -- validate packet length is adequate, otherwise quit
+  if buf:len() == 0 then return end
+
+  pkt.cols.protocol = p_sdlproto.name
+
+  -- create subtree for sdlproto
+  subtree = root:add(p_sdlproto, buf(0))
+
+  -- add protocol fields to subtree
+  local l_byte0 = buf(0, 1)
+  subtree:add(f_version, l_byte0):append_text(" (Only version 2 is supported)")
+  subtree:add(f_compressionFlag, l_byte0)
+  subtree:add(f_frameType, l_byte0)
+
+  subtree:add(f_serviceType, buf(1, 1))
+
+  -- Frame Info depends on Frame Type
+  local l_frameType = l_byte0:bitfield(5, 3)
+  local l_byte2 = buf(2, 1)
+  if f_frameTypeValue_ControlFrame == l_frameType then
+    subtree:add(f_frameInfo_ControlFrame, l_byte2)
+  elseif f_frameTypeValue_ConsecutiveFrame == l_frameType then
+    subtree:add(f_frameInfo_ConsecutiveFrame, l_byte2)
+  else
+    subtree:add(f_frameInfo, l_byte2):append_text(" (Reserved, should be 0x00)")
+  end
+
+  subtree:add(f_sessionID, buf(3, 1))
+
+  -- TODO subdissector for RPC messages
+  -- TODO test for 2+ messages in the same TCP packet
+end
+
+-- Initialization routine
+function p_sdlproto.init()
+end
+
+-- register a chained dissector for certain port
+local tcp_port = 12345
+local tcp_dissector_table = DissectorTable.get("tcp.port")
+tcp_dissector_table:add(tcp_port, p_sdlproto)
+
+-- vim: expandtab softtabstop=2 tabstop=2 shiftwidth=2
