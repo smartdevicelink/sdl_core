e6c60564bff8aa54cd2c36e8257e810acdb9af28
Andrew Batutin <abatutin@luxoft.com>
2014-01-16 13:30:52 +0200

    updated APPID for different ape types
    Conflicts:
    	mobile/android/SyncProxyAndroid/src/com/ford/syncV4/proxy/rpc/OnTouchEvent.java

--- mobile/android/SyncProxyTester/src/sptKateFlavor/java/com/ford/syncV4/android/constants/FlavorConst.java
+++ mobile/android/SyncProxyTester/src/sptKateFlavor/java/com/ford/syncV4/android/constants/FlavorConst.java
@@ -2,4 +2,7 @@ package com.ford.syncV4.android.constants;
 
 public class FlavorConst {
     public static final String PREFS_DEFAULT_APPNAME = "SPTKate";
+    public static final String APPID_BT = "12675309";
+    public static final String APPID_TCP = "12675308";
+    public static final String APPID_USB = "12675310";
 }
