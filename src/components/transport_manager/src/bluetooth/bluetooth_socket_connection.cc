e017d3b0ed2362c1b157a7c01c4c31913d324842
Konstantin Kolodiy <KKolodiy@luxoft.com>
2014-01-17 15:25:23 +0400

    Fix warning from Jenkins
    - Remove timer from TM
    - Use timer from utils component
    
    APPLINK-4736

--- src/components/transport_manager/src/bluetooth/bluetooth_socket_connection.cc
+++ src/components/transport_manager/src/bluetooth/bluetooth_socket_connection.cc
@@ -36,6 +36,12 @@
 #include "transport_manager/bluetooth/bluetooth_socket_connection.h"
 
 #include <unistd.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <bluetooth/sdp.h>
+#include <bluetooth/sdp_lib.h>
+#include <bluetooth/rfcomm.h>
 
 #include "transport_manager/bluetooth/bluetooth_device.h"
 #include "transport_manager/transport_adapter/transport_adapter_controller.h"
