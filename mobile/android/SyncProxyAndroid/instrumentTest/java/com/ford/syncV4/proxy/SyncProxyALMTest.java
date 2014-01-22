e90c383414dbf90a4ab3dfe0781908b6d939a5a1
Andrew Batutin <abatutin@luxoft.com>
2014-01-22 11:22:51 +0200

    added heartbeat test case

--- mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/proxy/SyncProxyALMTest.java
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/proxy/SyncProxyALMTest.java
@@ -378,4 +378,58 @@ public class SyncProxyALMTest extends InstrumentationTestCase {
         assertEquals(SESSION_ID, sessionIdCaptor.getValue().byteValue());
         assertEquals("correlationID", correlationIdCaptor.getValue());
     }
+
+    public void testHeartBeatIsSet() throws Exception {
+        SyncMsgVersion syncMsgVersion = new SyncMsgVersion();
+        syncMsgVersion.setMajorVersion(2);
+        syncMsgVersion.setMinorVersion(2);
+        TCPTransportConfig conf = mock(TCPTransportConfig.class);
+        final IProxyListenerALM listenerALM = mock(IProxyListenerALM.class);
+        SyncProxyALM proxyALM = new SyncProxyALM(listenerALM,
+                                /*sync proxy configuration resources*/null,
+                                /*enable advanced lifecycle management true,*/
+                "appName",
+                                /*ngn media app*/null,
+                                /*vr synonyms*/null,
+                                /*is media app*/true,
+                                /*app type*/null,
+                syncMsgVersion,
+                                /*language desired*/Language.EN_US,
+                                /*HMI Display Language Desired*/Language.EN_US,
+                                /*App ID*/"8675308",
+                                /*autoActivateID*/null,
+                                /*callbackToUIThre1ad*/ false,
+                                /*preRegister*/ false,
+                2,
+                conf) {
+
+
+            @Override
+            protected void initializeProxy() throws SyncException {
+                // Reset all of the flags and state variables
+                _haveReceivedFirstNonNoneHMILevel = false;
+                _haveReceivedFirstFocusLevel = false;
+                _haveReceivedFirstFocusLevelFull = false;
+                _syncIntefaceAvailablity = SyncInterfaceAvailability.SYNC_INTERFACE_UNAVAILABLE;
+
+                // Setup SyncConnection
+                synchronized (CONNECTION_REFERENCE_LOCK) {
+                    if (_syncConnection != null) {
+                        _syncConnection.closeConnection(_rpcSessionID, false);
+                        _syncConnection = null;
+                    }
+                    _syncConnection = mock(SyncConnection.class);
+                    when(_syncConnection.getIsConnected()).thenReturn(true);
+                }
+                synchronized (CONNECTION_REFERENCE_LOCK) {
+                    if (_syncConnection != null) {
+                        _syncConnection.startTransport();
+                    }
+                }
+            }
+
+        };
+        SyncProxyALM.setHeartBeatInterval(50);
+        assertEquals("Heartbeat should be 50", 50, SyncProxyALM.getHeartBeatInterval());
+    }
 }
