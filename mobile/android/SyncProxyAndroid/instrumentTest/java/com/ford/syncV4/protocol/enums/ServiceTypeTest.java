1069ef8822fc2e69e5842f963b2397bbb13cf847
Andrew Batutin <abatutin@luxoft.com>
2014-01-22 18:04:27 +0200

    APPLINK-4521: adde audio service type

--- mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/protocol/enums/ServiceTypeTest.java
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/protocol/enums/ServiceTypeTest.java
@@ -12,18 +12,35 @@ public class ServiceTypeTest extends AndroidTestCase {
     public ServiceTypeTest() {
     }
 
-    public void testSessionTypeMobNavNameIsMobile_Nav() throws Exception {
+    public void testServiceTypeMobNavNameIsMobile_Nav() throws Exception {
         String name = ServiceType.Mobile_Nav.getName();
         assertEquals(name, "Mobile_Nav");
     }
 
-    public void testSessionTypeMobNavValueIs0x0B() throws Exception {
+    public void testServiceTypeMobNavValueIs0x0B() throws Exception {
         byte value = ServiceType.Mobile_Nav.getValue();
         assertEquals(value, (byte) 0x0B);
     }
 
-    public void testMobileNavIsInSessionTypeList() throws Exception {
+    public void testMobileNavIsInServiceTypeList() throws Exception {
         Vector sessionTypes = ServiceType.getList();
         assertTrue(sessionTypes.contains(ServiceType.Mobile_Nav));
     }
+
+    public void testServiceTypeAudioNameIsAudio_Service() throws Exception {
+        String name = ServiceType.Audio_Service.getName();
+        assertEquals(name, "Audio_Service");
+    }
+
+    public void testServiceTypeAudioServiceValueIs10() throws Exception {
+        byte value = ServiceType.Audio_Service.getValue();
+        assertEquals(value, (byte) 10);
+    }
+
+    public void testAudioServiceIsInServiceTypeList() throws Exception {
+        Vector sessionTypes = ServiceType.getList();
+        assertTrue(sessionTypes.contains(ServiceType.Audio_Service));
+    }
+
+
 }
