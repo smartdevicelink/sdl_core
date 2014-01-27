7b0724c3fb106efd9ff094dee3da042d93e3b323
YuriyChernyshov <IChernyshov@luxoft.com>
2014-01-27 12:49:04 +0200

    APPLINK-4536-Implement Connection listener to inform all subscribed objects.

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/listener/ConnectionListenersManager.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.listener;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 1/27/14
+ * Time: 11:11 AM
+ */
+public class ConnectionListenersManager {
+
+}
