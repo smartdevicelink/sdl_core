bc16aa334fa171ae622cc263e9936cf571d990ee
YuriyChernyshov <IChernyshov@luxoft.com>
2014-01-27 13:12:19 +0200

    APPLINK-4536-Add ConnectionListenersManagerTest

---
+++ mobile/android/SyncProxyTester/instrumentTest/java/com/ford/syncV4/android/listener/ConnectionListenersManagerTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.listener;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 1/27/14
+ * Time: 12:51 PM
+ */
+public class ConnectionListenersManagerTest {
+
+}
