9f024c430af519063b66b64a40cce716461c5955
Dmitry Klimenko <DKlimenko@luxoft.com>
2014-01-29 17:16:38 -0800

    APPLINK-4551: ProtocolHandler audio service impl

--- src/components/protocol_handler/src/protocol_handler_impl.cc
+++ src/components/protocol_handler/src/protocol_handler_impl.cc
@@ -195,7 +195,8 @@ void ProtocolHandlerImpl::SendMessageToMobileApp(const RawMessagePtr& message,
   LOG4CXX_TRACE_ENTER(logger_);
   if (!message) {
     LOG4CXX_ERROR(logger_,
-        "Invalid message for sending to mobile app is received."); LOG4CXX_TRACE_EXIT(logger_);
+        "Invalid message for sending to mobile app is received.");
+    LOG4CXX_TRACE_EXIT(logger_);
     return;
   }
   raw_ford_messages_to_mobile_.PostMessage(
@@ -213,7 +214,9 @@ void ProtocolHandlerImpl::OnTMMessageReceived(const RawMessagePtr message) {
   if (message.valid()) {
     LOG4CXX_INFO_EXT(
         logger_,
-        "Received from TM " << message->data() << " with connection id " << message->connection_key());
+        "Received from TM " << message->data() <<
+        " with connection id " << message->connection_key());
+
     raw_ford_messages_from_mobile_.PostMessage(
         impl::RawFordMessageFromMobile(message));
   } else {
@@ -268,7 +271,8 @@ RESULT_CODE ProtocolHandlerImpl::SendFrame(ConnectionID connection_id,
 
   LOG4CXX_INFO_EXT(
       logger_,
-      "Packet to be sent: " << packet.packet() << " of size: " << packet.packet_size());
+      "Packet to be sent: " << packet.packet() <<
+      " of size: " << packet.packet_size());
 
   if (!session_observer_) {
     LOG4CXX_WARN(logger_, "No session_observer_ set.");
@@ -350,7 +354,8 @@ RESULT_CODE ProtocolHandlerImpl::SendMultiFrameMessage(
 
   LOG4CXX_INFO_EXT(
       logger_,
-      "Data size " << data_size << " of " << numOfFrames << " frames with last frame " << lastdata_size);
+      "Data size " << data_size << " of " << numOfFrames <<
+      " frames with last frame " << lastdata_size);
 
   uint8_t* outDataFirstFrame = new uint8_t[FIRST_FRAME_DATA_SIZE];
   outDataFirstFrame[0] = data_size >> 24;
@@ -651,7 +656,8 @@ void ProtocolHandlerImpl::Handle(
     const impl::RawFordMessageFromMobile& message) {
   LOG4CXX_INFO_EXT(
       logger_,
-      "Message " << message->data() << " from mobile app received of size " << message->data_size());
+      "Message " << message->data() <<
+      " from mobile app received of size " << message->data_size());
 
   if ((0 != message->data()) && (0 != message->data_size())
       && (MAXIMUM_FRAME_DATA_SIZE + PROTOCOL_HEADER_V2_SIZE
@@ -675,7 +681,9 @@ void ProtocolHandlerImpl::Handle(
 void ProtocolHandlerImpl::Handle(const impl::RawFordMessageToMobile& message) {
   LOG4CXX_INFO_EXT(
       logger_,
-      "Message to mobile app: connection " << message->connection_key() << "; dataSize: " << message->data_size() << " ; protocolVersion " << message->protocol_version());
+      "Message to mobile app: connection " << message->connection_key() << ";"
+      " dataSize: " << message->data_size() << " ;"
+      " protocolVersion " << message->protocol_version());
 
   uint32_t maxDataSize = 0;
   if (PROTOCOL_VERSION_1 == message->protocol_version()) {
@@ -687,7 +695,8 @@ void ProtocolHandlerImpl::Handle(const impl::RawFordMessageToMobile& message) {
   if (!session_observer_) {
     LOG4CXX_ERROR(
         logger_,
-        "Cannot handle message to mobile app:" << " ISessionObserver doesn't exist.");
+        "Cannot handle message to mobile app:" <<
+        " ISessionObserver doesn't exist.");
     return;
   }
   uint32_t connection_handle = 0;
