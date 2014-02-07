8b8e192c3b2aedb536e1bd5ecd2ceb74d1baf258
YuriyChernyshov <IChernyshov@luxoft.com>
2014-02-07 12:14:49 +0200

    APPLINK-5598-Add common base Fragment class for streaming views

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/mobilenav/SyncServiceBaseFragment.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity.mobilenav;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/6/14
+ * Time: 3:18 PM
+ */
+public class SyncServiceBaseFragment {
+
+}
