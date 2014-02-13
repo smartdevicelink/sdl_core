ce3d3daf9f9df8b3498e0a0cdafd39dab2de2b9c
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-13 12:39:37 +0200

    APPLINK-5540-Add Policy File set up dialog view

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/PolicyFilesSetUpDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/13/14
+ * Time: 12:14 PM
+ */
+public class PolicyFilesSetUpDialog {
+
+}
