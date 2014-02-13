198cef79d8cdb40291bc9c1a865ee501fa6ee09b
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-13 11:50:07 +0200

    APPLINK-5540-Add test class for the AppUtils. Add policy files.

---
+++ mobile/android/SyncProxyTester/instrumentTest/java/com/ford/syncV4/android/utils/AppUtilsTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.utils;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/13/14
+ * Time: 11:37 AM
+ */
+public class AppUtilsTest {
+
+}
