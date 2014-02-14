8201ad9b26c550364eaf53dd3c320a21bf37c6df
Eugene Nikolsky <enikolskii@luxoft.com>
2014-02-14 13:23:23 +0200

    APPLINK-5333: add new parameters to SetDisplayLayoutResponse class
    Not a subject for review.

--- mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/proxy/rpc/SetDisplayLayoutResponseTest.java
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/proxy/rpc/SetDisplayLayoutResponseTest.java
@@ -7,6 +7,9 @@ import junit.framework.TestCase;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
+import java.util.Vector;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -20,6 +23,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class SetDisplayLayoutResponseTest extends TestCase {
     private static final byte PROTOCOL_VERSION = (byte) 2;
     private static final String DISPLAY_CAPABILITIES = "displayCapabilities";
+    private static final String BUTTON_CAPABILITIES = "buttonCapabilities";
+    private static final String SOFT_BUTTON_CAPABILITIES =
+            "softButtonCapabilities";
+    private static final String PRESET_BANK_CAPABILITIES =
+            "presetBankCapabilities";
 
     public void testMessageShouldBeCreated() {
         SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
@@ -31,11 +39,27 @@ public class SetDisplayLayoutResponseTest extends TestCase {
         SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
 
         final DisplayCapabilities capabilities = new DisplayCapabilities();
+        final Vector<ButtonCapabilities> buttonCapabilities =
+                new Vector<ButtonCapabilities>(
+                        Arrays.asList(new ButtonCapabilities()));
+        final Vector<SoftButtonCapabilities> softButtonCapabilities =
+                new Vector<SoftButtonCapabilities>(
+                        Arrays.asList(new SoftButtonCapabilities()));
+        final PresetBankCapabilities presetBankCapabilities =
+                new PresetBankCapabilities();
 
         msg.setDisplayCapabilities(capabilities);
+        msg.setButtonCapabilities(buttonCapabilities);
+        msg.setSoftButtonCapabilities(softButtonCapabilities);
+        msg.setPresetBankCapabilities(presetBankCapabilities);
 
         JSONObject jsonObject = msg.serializeJSON(PROTOCOL_VERSION);
         assertThat(jsonObject.has(DISPLAY_CAPABILITIES), is(true));
+        assertThat(jsonObject.getJSONArray(BUTTON_CAPABILITIES).length(),
+                is(buttonCapabilities.size()));
+        assertThat(jsonObject.getJSONArray(SOFT_BUTTON_CAPABILITIES).length(),
+                is(softButtonCapabilities.size()));
+        assertThat(jsonObject.has(PRESET_BANK_CAPABILITIES), is(true));
     }
 
     public void testDeserializedMessageWithoutParamsShouldContainNullFields()
@@ -47,8 +71,12 @@ public class SetDisplayLayoutResponseTest extends TestCase {
 
         assertThat(msg, notNullValue());
         assertThat(msg.getDisplayCapabilities(), nullValue());
+        assertThat(msg.getButtonCapabilities(), nullValue());
+        assertThat(msg.getSoftButtonCapabilities(), nullValue());
+        assertThat(msg.getPresetBankCapabilities(), nullValue());
     }
 
+    // displayCapabilities
     public void testDisplayCapabilitiesGetterShouldReturnSetValue()
             throws JSONException {
         SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
@@ -104,5 +132,189 @@ public class SetDisplayLayoutResponseTest extends TestCase {
                         TestCommon.paramsToRequestObject(jsonObject)));
         assertThat(msg, notNullValue());
         assertThat(msg.getDisplayCapabilities(), nullValue());
+    }
+
+    // buttonCapabilities
+    public void testButtonCapabilitiesGetterShouldReturnSetValue()
+            throws JSONException {
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
+
+        final Vector<ButtonCapabilities> buttonCapabilities =
+                new Vector<ButtonCapabilities>(
+                        Arrays.asList(new ButtonCapabilities()));
+        msg.setButtonCapabilities(buttonCapabilities);
+
+        assertThat(msg.getButtonCapabilities(), is(buttonCapabilities));
+    }
+
+    public void testSettingNullButtonCapabilitiesShouldRemoveValue()
+            throws JSONException {
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
+
+        msg.setButtonCapabilities(new Vector<ButtonCapabilities>());
+        msg.setButtonCapabilities(null);
+
+        assertThat(msg.getButtonCapabilities(), nullValue());
+    }
+
+    public void testDeserializedButtonCapabilitiesAsVectorShouldContainValue()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        final Vector<ButtonCapabilities> buttonCapabilities =
+                new Vector<ButtonCapabilities>(
+                        Arrays.asList(new ButtonCapabilities()));
+        jsonObject.put(BUTTON_CAPABILITIES, buttonCapabilities);
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getButtonCapabilities(), is(buttonCapabilities));
+    }
+
+    public void testDeserializedButtonCapabilitiesAsStringShouldBeNull()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(BUTTON_CAPABILITIES, "4.0f");
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getButtonCapabilities(), nullValue());
+    }
+
+    public void testDeserializedButtonCapabilitiesAsIntShouldBeNull()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(BUTTON_CAPABILITIES, 5);
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getButtonCapabilities(), nullValue());
+    }
+
+    // softButtonCapabilities
+    public void testSoftButtonCapabilitiesGetterShouldReturnSetValue()
+            throws JSONException {
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
+
+        final Vector<SoftButtonCapabilities> softButtonCapabilities =
+                new Vector<SoftButtonCapabilities>(
+                        Arrays.asList(new SoftButtonCapabilities()));
+        msg.setSoftButtonCapabilities(softButtonCapabilities);
+
+        assertThat(msg.getSoftButtonCapabilities(), is(softButtonCapabilities));
+    }
+
+    public void testSettingNullSoftButtonCapabilitiesShouldRemoveValue()
+            throws JSONException {
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
+
+        msg.setSoftButtonCapabilities(new Vector<SoftButtonCapabilities>());
+        msg.setSoftButtonCapabilities(null);
+
+        assertThat(msg.getSoftButtonCapabilities(), nullValue());
+    }
+
+    public void testDeserializedSoftButtonCapabilitiesAsVectorShouldContainValue()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        final Vector<SoftButtonCapabilities> softButtonCapabilities =
+                new Vector<SoftButtonCapabilities>(
+                        Arrays.asList(new SoftButtonCapabilities()));
+        jsonObject.put(SOFT_BUTTON_CAPABILITIES, softButtonCapabilities);
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getSoftButtonCapabilities(), is(softButtonCapabilities));
+    }
+
+    public void testDeserializedSoftButtonCapabilitiesAsStringShouldBeNull()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(SOFT_BUTTON_CAPABILITIES, "4.0f");
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getSoftButtonCapabilities(), nullValue());
+    }
+
+    public void testDeserializedSoftButtonCapabilitiesAsIntShouldBeNull()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(SOFT_BUTTON_CAPABILITIES, 5);
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getSoftButtonCapabilities(), nullValue());
+    }
+
+    // presetBankCapabilities
+    public void testPresetBankCapabilitiesGetterShouldReturnSetValue()
+            throws JSONException {
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
+
+        final PresetBankCapabilities capabilities =
+                new PresetBankCapabilities();
+        msg.setPresetBankCapabilities(capabilities);
+
+        assertThat(msg.getPresetBankCapabilities(), is(capabilities));
+    }
+
+    public void testSettingNullPresetBankCapabilitiesShouldRemoveValue()
+            throws JSONException {
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse();
+
+        msg.setPresetBankCapabilities(new PresetBankCapabilities());
+        msg.setPresetBankCapabilities(null);
+
+        assertThat(msg.getPresetBankCapabilities(), nullValue());
+    }
+
+    public void testDeserializedPresetBankCapabilitiesAsStructShouldContainValue()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        final PresetBankCapabilities capabilities =
+                new PresetBankCapabilities();
+        jsonObject.put(PRESET_BANK_CAPABILITIES, capabilities);
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getPresetBankCapabilities(), is(capabilities));
+    }
+
+    public void testDeserializedPresetBankCapabilitiesAsStringShouldBeNull()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(PRESET_BANK_CAPABILITIES, "4.0f");
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getPresetBankCapabilities(), nullValue());
+    }
+
+    public void testDeserializedPresetBankCapabilitiesAsIntShouldBeNull()
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(PRESET_BANK_CAPABILITIES, 5);
+
+        SetDisplayLayoutResponse msg = new SetDisplayLayoutResponse(
+                JsonRPCMarshaller.deserializeJSONObject(
+                        TestCommon.paramsToRequestObject(jsonObject)));
+        assertThat(msg, notNullValue());
+        assertThat(msg.getPresetBankCapabilities(), nullValue());
     }
 }
--- mobile/android/SyncProxyAndroid/src/com/ford/syncV4/proxy/rpc/SetDisplayLayoutResponse.java
+++ mobile/android/SyncProxyAndroid/src/com/ford/syncV4/proxy/rpc/SetDisplayLayoutResponse.java
@@ -1,15 +1,17 @@
 package com.ford.syncV4.proxy.rpc;
 
-import java.util.Hashtable;
-
 import com.ford.syncV4.proxy.RPCResponse;
 import com.ford.syncV4.proxy.constants.Names;
+
+import java.util.Hashtable;
+import java.util.Vector;
 
 public class SetDisplayLayoutResponse extends RPCResponse {
 
     public SetDisplayLayoutResponse() {
         super("SetDisplayLayout");
     }
+
     public SetDisplayLayoutResponse(Hashtable hash) {
         super(hash);
     }
@@ -24,11 +26,95 @@ public class SetDisplayLayoutResponse extends RPCResponse {
         return null;
     }
 
-    public void setDisplayCapabilities(DisplayCapabilities displayCapabilities) {
+    public void setDisplayCapabilities(
+            DisplayCapabilities displayCapabilities) {
         if (displayCapabilities != null) {
             parameters.put(Names.displayCapabilities, displayCapabilities);
         } else {
             parameters.remove(Names.displayCapabilities);
+        }
+    }
+
+    public Vector<ButtonCapabilities> getButtonCapabilities() {
+        final Object o = parameters.get(Names.buttonCapabilities);
+        if (o instanceof Vector<?>) {
+            Vector<?> list = (Vector<?>) o;
+            if (list != null && list.size() > 0) {
+                Object obj = list.get(0);
+                if (obj instanceof ButtonCapabilities) {
+                    return (Vector<ButtonCapabilities>) list;
+                } else if (obj instanceof Hashtable) {
+                    Vector<ButtonCapabilities> newList =
+                            new Vector<ButtonCapabilities>();
+                    for (Object hashObj : list) {
+                        newList.add(
+                                new ButtonCapabilities((Hashtable) hashObj));
+                    }
+                    return newList;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void setButtonCapabilities(
+            Vector<ButtonCapabilities> buttonCapabilities) {
+        if (buttonCapabilities != null) {
+            parameters.put(Names.buttonCapabilities, buttonCapabilities);
+        } else {
+            parameters.remove(Names.buttonCapabilities);
+        }
+    }
+
+    public Vector<SoftButtonCapabilities> getSoftButtonCapabilities() {
+        final Object o = parameters.get(Names.softButtonCapabilities);
+        if (o instanceof Vector<?>) {
+            Vector<?> list = (Vector<?>) o;
+            if (list != null && list.size() > 0) {
+                Object obj = list.get(0);
+                if (obj instanceof SoftButtonCapabilities) {
+                    return (Vector<SoftButtonCapabilities>) list;
+                } else if (obj instanceof Hashtable) {
+                    Vector<SoftButtonCapabilities> newList =
+                            new Vector<SoftButtonCapabilities>();
+                    for (Object hashObj : list) {
+                        newList.add(new SoftButtonCapabilities(
+                                (Hashtable) hashObj));
+                    }
+                    return newList;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void setSoftButtonCapabilities(
+            Vector<SoftButtonCapabilities> softButtonCapabilities) {
+        if (softButtonCapabilities != null) {
+            parameters.put(Names.softButtonCapabilities,
+                    softButtonCapabilities);
+        } else {
+            parameters.remove(Names.softButtonCapabilities);
+        }
+    }
+
+    public PresetBankCapabilities getPresetBankCapabilities() {
+        Object obj = parameters.get(Names.presetBankCapabilities);
+        if (obj instanceof PresetBankCapabilities) {
+            return (PresetBankCapabilities) obj;
+        } else if (obj instanceof Hashtable) {
+            return new PresetBankCapabilities((Hashtable) obj);
+        }
+        return null;
+    }
+
+    public void setPresetBankCapabilities(
+            PresetBankCapabilities presetBankCapabilities) {
+        if (presetBankCapabilities != null) {
+            parameters.put(Names.presetBankCapabilities,
+                    presetBankCapabilities);
+        } else {
+            parameters.remove(Names.presetBankCapabilities);
         }
     }
 }
