538c2bd68abfb168a32e93bb0e860ab65ff26ab4
Eugene Nikolsky <enikolskii@luxoft.com>
2014-02-20 11:44:31 +0200

    old-branch-nagger.sh: fix date filtering

--- tools/coding-style/old-branch-nagger.sh
+++ tools/coding-style/old-branch-nagger.sh
@@ -3,19 +3,21 @@
 # All the branches excluding release branches and virtual HEAD branch
 ALL_BRANCHES=`git branch -r | grep -v '\(HEAD\|origin/release\)'`
 
-# Branch names, access dates and last commiter names
+# Branch names, access dates and last committer names
 # The format is "<unix-date>  <oldness-text>  <author>  <branch-name>"
-# Soreted by date, freshes branches first
-BRANCH_COMMITS=$(for branch in $ALL_BRANCHES;do echo -e $(git show --format="%at\t%ar\t%aE\t" $branch | head -n 1)$branch; done)
-BRANCH_COMMITS=$(echo "$BRANCH_COMMITS"|sort -rn)
+# Sorted by date, freshes branches first
+BRANCH_COMMITS=$(for branch in $ALL_BRANCHES;do echo -e $(git log -1 --format="%at\t%ar\t%aE\t" $branch)$branch; done | sort -rn)
 
 # List of all branch owners
 BRANCH_OWNERS=$(echo "$BRANCH_COMMITS"|cut -f3|sort|uniq)
 
+# 2 weeks here
+TIME_THRESHOLD=$((60 * 60 * 24 * 7 * 2))
+DATE_THRESHOLD=$(( $(date +%s) - $TIME_THRESHOLD ))
 
 for OWNER in $BRANCH_OWNERS; do
   # Oldness and names of branches which belong to $OWNER and that are more than 2 weeks old
-  OLD_BRANCHES=$(echo "$BRANCH_COMMITS"|grep $OWNER|grep -v 'days ago'|cut -f 2,4)
+  OLD_BRANCHES=$(echo "$BRANCH_COMMITS"|grep $OWNER|awk -F'\t' 'int($1) < '$DATE_THRESHOLD' {print $2 " " $4}')
   if [ -n "$OLD_BRANCHES" ];
   then
     cat <<EOF
