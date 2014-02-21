718b91880e6a87afb5418bef10a0ff3bff4affdf
Dmitry Chmerev <dchmerev@luxoft.com>
2014-02-21 13:17:57 +0400

    Incomplete code moving

--- src/components/utils/src/threads/posix_thread.cc
+++ src/components/utils/src/threads/posix_thread.cc
@@ -126,16 +126,16 @@ bool Thread::startWithOptions(const ThreadOptions& options) {
                    "Error code = " << pthread_result);
     }
   }
-#ifdef __QNXNTO__
-  if (pthread_result != EOK) {
-    LOG4CXX_INFO(logger_,"Couldn't set pthread name"
-                 " Error code = " << pthread_result);
-  }
-#endif
   success = !pthread_create(&thread_handle_, &attributes, threadFunc,
                             delegate_);
   if (success) {
     pthread_result = pthread_setname_np(thread_handle_, name_.c_str());
+#   ifdef __QNXNTO__
+    if (pthread_result != EOK) {
+      LOG4CXX_INFO(logger_,"Couldn't set pthread name"
+                   " Error code = " << pthread_result);
+    }
+#   endif
     LOG4CXX_INFO(logger_,"Created thread: " << name_);
     ThreadManager::instance()->RegisterName(thread_handle_, name_);
   }
