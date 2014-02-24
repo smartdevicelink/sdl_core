215dba3276aba65c291b12919096f399cafca156
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-24 11:17:06 +0200

    APPLINK-5846-Set AddSubMenu creation in separate dialog

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/AddSubMenuDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/24/14
+ * Time: 10:35 AM
+ */
+public class AddSubMenuDialog {
+
+}
