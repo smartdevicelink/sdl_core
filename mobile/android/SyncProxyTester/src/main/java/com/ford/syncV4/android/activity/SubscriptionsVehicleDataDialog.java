142345be8a39e9e5cab3f4fe70cb1cb83d355e93
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-24 16:02:03 +0200

    APPLINK-5846-Set Subscription for the Vehicle Data in separate dialog

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/SubscriptionsVehicleDataDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/24/14
+ * Time: 2:19 PM
+ */
+public class SubscriptionsVehicleDataDialog {
+
+}
