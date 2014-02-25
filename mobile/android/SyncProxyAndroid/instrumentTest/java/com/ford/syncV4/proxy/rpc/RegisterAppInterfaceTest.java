5a529cae95328e6b3b9cf50c169d7cdce32d713c
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-25 12:43:27 +0200

    APPLINK-5890-Add RegisterAppInterface test class

---
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/proxy/rpc/RegisterAppInterfaceTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.proxy.rpc;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/25/14
+ * Time: 11:55 AM
+ */
+public class RegisterAppInterfaceTest {
+
+}
