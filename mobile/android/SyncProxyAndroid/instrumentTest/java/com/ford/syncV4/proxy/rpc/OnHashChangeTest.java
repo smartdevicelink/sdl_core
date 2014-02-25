d855e2d811da7d69364f6e3f5f37aab0054af215
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-25 14:18:50 +0200

    APPLINK-5889-Implement OnHashId notification and it's test cases

---
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/proxy/rpc/OnHashChangeTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.proxy.rpc;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/25/14
+ * Time: 1:50 PM
+ */
+public class OnHashChangeTest {
+
+}
