351816beb4ff9c93d91cb5d25047b223a559c868
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-26 11:01:44 +0200

    APPLINK-5906-Add Policy file manager

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/policies/PolicyFilesManager.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.policies;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/26/14
+ * Time: 9:59 AM
+ */
+public class PolicyFilesManager {
+
+}
