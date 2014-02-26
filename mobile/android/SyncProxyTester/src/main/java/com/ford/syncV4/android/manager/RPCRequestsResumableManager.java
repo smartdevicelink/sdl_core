0cd12c968b664644709e3c0da88d84feb004a9e8
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-26 15:31:39 +0200

    APPLINK-5888-Add manager to process RPC requests in order to implment application resumption

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/manager/RPCRequestsResumableManager.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.manager;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/26/14
+ * Time: 12:25 PM
+ */
+public class RPCRequestsResumableManager {
+
+}
