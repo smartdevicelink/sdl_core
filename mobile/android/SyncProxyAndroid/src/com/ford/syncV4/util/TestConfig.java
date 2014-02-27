ffa660950c1b98b7724b80651d27bb3c96bcdc83
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-27 11:37:37 +0200

    APPLINK-5935-Add HashId set up dialog

---
+++ mobile/android/SyncProxyAndroid/src/com/ford/syncV4/util/TestConfig.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.util;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/27/14
+ * Time: 9:58 AM
+ */
+public class TestConfig {
+
+}
---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/HashIdSetUpDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/26/14
+ * Time: 4:46 PM
+ */
+public class HashIdSetUpDialog {
+
+}
