0c469ffc4286e69d04d87ee52e96a2b3563ad18d
YuriyChernyshov <YChernyshov@luxoft.com>
2014-02-28 16:39:33 +0200

    APPLINK-5966-Add last used Hash Id manager

---
+++ mobile/android/SyncProxyTester/instrumentTest/java/com/ford/syncV4/android/manager/LastUsedHashIdsManagerTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.manager;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 2/28/14
+ * Time: 1:56 PM
+ */
+public class LastUsedHashIdsManagerTest {
+
+}
