26a5fd4101f2c9bffb799044583d08aff8744336
YuriyChernyshov <YChernyshov@luxoft.com>
2014-03-04 13:34:15 +0200

    APPLINK-5801-Add TestCase when StartSecureService is callinf when ransport is connected.

---
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/integrationtest/TransportConnect_StartSecurityServiceTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.integrationtest;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 3/4/14
+ * Time: 1:10 PM
+ */
+public class TransportConnect_StartSecurityServiceTest {
+
+}
