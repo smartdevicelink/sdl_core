5149aa6446b5ed96df27f0defc581e2b6330ce6b
YuriyChernyshov <YChernyshov@luxoft.com>
2014-03-05 12:55:18 +0200

    APPLINK-5801-Add secure service interfaces and structures (https://adc.luxoft.com/confluence/display/APPLINK/Android+SDK#Secure Service)

---
+++ mobile/android/SyncProxyAndroid/src/com/ford/syncV4/service/ISecureServiceMessageCallback.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.service;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 3/5/14
+ * Time: 12:43 PM
+ */
+public class ISecureServiceMessageCallback {
+
+}
