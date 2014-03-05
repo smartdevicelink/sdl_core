3091f4570b30537153e0368ac4f8cef15900b01c
ykazakov <ykazakov@luxoft.com>
2014-03-05 15:34:27 +0400

    no subject for review trivial changes Jenkins integration script update

--- jenkins.integration.multiple.config.run.sh
+++ jenkins.integration.multiple.config.run.sh
@@ -8,10 +8,10 @@ fi
 if [ $OS_TYPE ] && [ $OS_TYPE = "QNX" ]; then
      TOOLCHAIN_OPTION="-DCMAKE_TOOLCHAIN_FILE=./qnx_6.5.0_linux_x86.cmake"
 fi
-if [ $HMI_TYPE ] && [ $HMI_TYPE = "QML2" ]; then
+if [ $HMI_TYPE ] && [ $HMI_TYPE != "HTML5" ]; then
      HMI_OPTION="-DHMI2=ON"
 fi
 
-echo cmake ${DBG_OPTION} ${TOOLCHAIN_OPTION} ${HMI_OPTION} .
-cmake ${DBG_OPTION} ${TOOLCHAIN_OPTION} ${HMI_OPTION} .
-make install
+echo cmake ${DBG_OPTION} ${TOOLCHAIN_OPTION} ${HMI_OPTION} -DBUILD_TESTS=On .
+cmake ${DBG_OPTION} ${TOOLCHAIN_OPTION} ${HMI_OPTION} -DBUILD_TESTS=On .
+make install 
