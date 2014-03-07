1cec7e2e1bd509cedf8722111d4242bf7da6e8d4
YuriyChernyshov <YChernyshov@luxoft.com>
2014-03-07 09:33:09 +0200

    APPLINK-5801-Create SecureService payload class.

---
+++ mobile/android/SyncProxyAndroid/instrumentTest/java/com/ford/syncV4/protocol/secure/SecureServicePayloadTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol.secure;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 3/6/14
+ * Time: 3:32 PM
+ */
+public class SecureServicePayloadTest {
+
+}
---
+++ mobile/android/SyncProxyAndroid/src/com/ford/syncV4/protocol/secure/SecureServicePayload.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol.secure;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 3/6/14
+ * Time: 1:24 PM
+ */
+public class SecureServicePayload {
+
+}
