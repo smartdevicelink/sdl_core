c3adbcf1fdec960f2958576b82bf008d0148a259
YuriyChernyshov <YChernyshov@luxoft.com>
2014-03-12 14:21:47 +0200

    APPLINK-5853-Add protocol const class

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/protocol/ProtocolConst.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12.03.14
+ * Time: 13:22
+ */
+public class ProtocolConst {
+
+}
