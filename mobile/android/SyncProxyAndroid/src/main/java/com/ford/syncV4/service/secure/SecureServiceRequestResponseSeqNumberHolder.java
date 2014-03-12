061d59efd2d0776fd81dd4f8c7c2408f359e6a71
YuriyChernyshov <YChernyshov@luxoft.com>
2014-03-12 16:27:07 +0200

    APPLINK-5801-Add a TMP Class which allows to manage req / res seq number to associate it with Service Type

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/service/secure/SecureServiceRequestResponseSeqNumberHolder.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.service.secure;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 12.03.14
+ * Time: 15:16
+ */
+public class SecureServiceRequestResponseSeqNumberHolder {
+
+}
