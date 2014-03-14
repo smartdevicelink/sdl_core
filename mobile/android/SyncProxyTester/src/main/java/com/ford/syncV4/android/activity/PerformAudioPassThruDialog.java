e3d3a27448194842f60169cfd2c0605ed627ebbf
YuriyChernyshov <YChernyshov@luxoft.com>
2014-03-14 11:27:30 +0200

    APPLINK-6199-Implement dialog fragment for the PerformAudioPassThru

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/PerformAudioPassThruDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 14.03.14
+ * Time: 10:51
+ */
+public class PerformAudioPassThruDialog {
+
+}
