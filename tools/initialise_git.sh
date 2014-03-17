c49a8aeeb8b38e371542b2987a976db033ecd372
Alexander Kutsan <akutsan@akutsan>
2014-03-17 18:04:20 +0200

    Git re-configure
    Question about username to set git username and email.

--- tools/initialise_git.sh
+++ tools/initialise_git.sh
@@ -1,19 +1,51 @@
 #/usr/bin/sh
 
 GIT_REPO=$1
-MESSAGE_DATA="Not a subject for review"
-MESSGAGE_FILE_NAME="default_message"
-GIT_CONFIG_DATA="[commit]\n\ttemplate = .git/$MESSGAGE_FILE_NAME"
+
 
 if [ "$GIT_REPO" = "" ]
 then
     echo "You should setup your git folder with first paraameter"
     echo "Example:"
-    echo -e "\t ./initialise_git.sh /home/{USER_NAME}/applink"    
+    echo -e "\t ./initialise_git.sh /home/{USER_NAME}/applink"
     exit
 fi
+cd $GIT_REPO
+echo "enter first name" 
+read FIRST_NAME
+echo "enter last name" 
+read LAST_NAME
 
-cd $GIT_REPO/.git 
-echo $MESSAGE_DATA > $MESSGAGE_FILE_NAME 
-echo $GIT_CONFIG_DATA >> ./config 
+EMAIL=${FIRST_NAME:0:1}$LAST_NAME"@luxoft.com"
+echo "Your email is: "$EMAIL
+echo "Correct? [yes or no] "
+
+read answer
+
+while [[ $answer != yes && $answer != no ]]
+do
+	echo you have entered an invalid response. Please try again
+	read answer
+done
+
+if [ $answer != yes ] 
+then	
+	echo "Enter your domain ";
+	read EMAIL
+fi
+
+git config user.name $FIRST_NAME" "$LAST_NAME
+echo git config user.email $EMAIL
+
+
+MESSAGE_DATA="Reason:\n\nNot a subject for review"
+MESSAGE_DATA="Reason:\n\nNot a subject for review"
+MESSGAGE_FILE_NAME="default_message"
+
+echo -e $MESSAGE_DATA > .git/$MESSGAGE_FILE_NAME 
+git config commit.template .git/$MESSGAGE_FILE_NAME 
 echo "Done"
+
+
+
+
