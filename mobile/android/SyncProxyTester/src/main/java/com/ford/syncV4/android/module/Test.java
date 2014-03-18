ad5cbd0debab77429c0baf119b12790626b9a1f6
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-03-18 16:41:30 +0200

    Not a subject for review
    	Reason:No review for mobile team until 10th of April 2014

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/module/Test.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.module;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 18.03.14
+ * Time: 12:35
+ */
+public class Test {
+
+}
---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/module/TestResult.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.module;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 18.03.14
+ * Time: 12:32
+ */
+public class TestResult {
+
+}
