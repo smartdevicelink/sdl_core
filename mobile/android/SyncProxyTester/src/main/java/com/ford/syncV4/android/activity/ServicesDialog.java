332943be124ab6e7dfb989cd67374fcb43738c20
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-03-25 18:38:36 +0200

    Not a subject for review
    	Reason:No review for mobile team until 10th of April 2014 - APPLINK-6277 - Intermediate state

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/ServicesDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 25.03.14
+ * Time: 17:11
+ */
+public class ServicesDialog {
+
+}
