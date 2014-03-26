3911426cb4102113316779c53dbddac1c2bc7d68
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-03-26 16:18:54 +0200

    Not a subject for review
    	Reason:APPLINK-5881-Add processor to process notifications

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/proxy/CertificateRecallProcessor.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.proxy;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 26.03.14
+ * Time: 12:30
+ */
+public class CertificateRecallProcessor {
+
+}
