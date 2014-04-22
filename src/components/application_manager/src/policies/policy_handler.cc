520d447afddce1fbdf4e4c845fe2f82938bf08b5
Andrey Oleynik <AOleynik@luxoft.com>
2014-04-22 09:26:26 +0300

    APPLINK-6901. Limit applications to pre_DataConsent, if device disallowed. Draft implementation of unpaired device cleanup from DB.

--- src/components/application_manager/src/policies/policy_handler.cc
+++ src/components/application_manager/src/policies/policy_handler.cc
@@ -451,6 +451,9 @@ bool PolicyHandler::ReceiveMessageFromSDK(const BinaryMessage& pt_string) {
     event_observer_.get()->subscribe_on_event(
       hmi_apis::FunctionID::VehicleInfo_GetVehicleData, correlation_id);
     application_manager::MessageHelper::CreateGetDeviceData(correlation_id);
+    if (policy_manager_->CleanupUnpairedDevices(unpaired_device_ids_)) {
+     unpaired_device_ids_.clear();
+    }
   }
   return ret;
 }
@@ -522,6 +525,22 @@ void PolicyHandler::OnAllowSDLFunctionalityNotification(bool is_allowed,
     }
     policy_manager_->SetUserConsentForDevice(device_params.device_mac_address,
         is_allowed);
+
+    // In case of changed consent for device, related applications will be
+    // limited to pre_DataConsent permissions, if device disallowed, or switch
+    // back to their own permissions, if device allowed again, and must be
+    // notified about these changes
+    typedef std::set<application_manager::ApplicationSharedPtr> ApplicationList;
+    ApplicationList app_list =
+        application_manager::ApplicationManagerImpl::instance()->applications();
+    ApplicationList::const_iterator it_app_list = app_list.begin();
+    ApplicationList::const_iterator it_app_list_end = app_list.end();
+    for (; it_app_list != it_app_list_end; ++it_app_list) {
+      if (device_id == (*it_app_list).get()->device()) {
+        policy_manager_->SendNotificationOnPermissionsUpdated(
+              (*it_app_list).get()->mobile_app_id()->asString());
+      }
+    }
 
     DeviceHandles::iterator it = std::find(pending_device_handles_.begin(),
                                            pending_device_handles_.end(),
