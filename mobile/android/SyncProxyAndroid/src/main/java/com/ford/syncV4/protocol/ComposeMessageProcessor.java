d495616f0d4f5c03e19b5da6b5268cf2bda9257c
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-04-22 12:57:47 +0300

    Not a subject for review
          Reason: Temporary progress changes

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/protocol/ComposeMessageProcessor.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 22.04.14
+ * Time: 11:36
+ */
+public class ComposeMessageProcessor {
+
+}
