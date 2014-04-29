ff83a5548754b38781da86035a6af52e0cae4d23
Elisey Zamakhov <EZamakhov@luxoft.com>
2014-04-29 18:45:34 +0400

    Not a subject for review emove double include 	Reason: trivial changes

--- src/components/connection_handler/src/heartbeat_monitor.cc
+++ src/components/connection_handler/src/heartbeat_monitor.cc
@@ -29,7 +29,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <unistd.h>
 #include "connection_handler/heartbeat_monitor.h"
 #include <unistd.h>
 #include "connection_handler/connection.h"
