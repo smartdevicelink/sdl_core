0b2acd98bc1e406261a8a9a1d21cdffb5ce2e561
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-05-05 13:31:02 +0300

    Not a subject for review
    	Reason:trivial changes

---
+++ mobile/android/SyncProxyAndroid/androidTest/java/com/ford/syncV4/protocol/SendProtocolMessageProcessorTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 05.05.14
+ * Time: 10:31
+ */
+public class SendProtocolMessageProcessorTest {
+
+}
