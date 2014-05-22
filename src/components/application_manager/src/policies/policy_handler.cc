2d4cf9be827e57853145ee65e578004231af2700
Polina Vyshnevska <PVyshnevska@luxoft.com>
2014-05-22 20:14:55 +0300

    APPLINK-7375: fix

--- src/components/application_manager/src/policies/policy_handler.cc
+++ src/components/application_manager/src/policies/policy_handler.cc
@@ -302,8 +302,8 @@ void PolicyHandler::OnGetListOfPermissions(const uint32_t connection_key,
     LOG4CXX_WARN(logger_, "Couldn't find application to get permissions.");
   } else {
     policy_manager_->GetUserConsentForApp(device_params.device_mac_address,
-        app->mobile_app_id()->asString(),
-        group_permissions);
+                                          app->mobile_app_id()->asString(),
+                                          group_permissions);
   }
 
   application_manager::MessageHelper::SendGetListOfPermissionsResponse(
@@ -450,7 +450,7 @@ BinaryMessageSptr PolicyHandler::AddHttpHeader(const BinaryMessageSptr& pt_strin
   packet["HTTPRequest"]["body"] = Json::Value(Json::objectValue);
   packet["HTTPRequest"]["body"]["data"] = Json::Value(Json::arrayValue);
   packet["HTTPRequest"]["body"]["data"][0] = Json::Value(std::string(pt_string->begin(),
-                                                                     pt_string->end()));
+      pt_string->end()));
 
   Json::FastWriter writer;
   std::string message = writer.write(packet);
@@ -561,7 +561,7 @@ void PolicyHandler::StartPTExchange(bool skip_device_selection) {
   }
 
   if (policy_manager_->GetPolicyTableStatus() ==
-        PolicyTableStatus::StatusUpdatePending) {
+      PolicyTableStatus::StatusUpdatePending) {
     LOG4CXX_INFO(logger_, "Starting exchange skipped, since another exchange "
                  "is in progress.");
     return;
@@ -699,7 +699,10 @@ void PolicyHandler::OnActivateApp(uint32_t connection_key,
 
     usage.RecordAppUserSelection();
 
-    DeviceConsent consent = GetDeviceForSending(permissions.deviceInfo);
+    application_manager::MessageHelper::GetDeviceInfoForApp(connection_key,
+        &permissions.deviceInfo);
+    DeviceConsent consent = policy_manager_->GetUserConsentForDevice(
+                              permissions.deviceInfo.device_mac_address);
     permissions.isSDLAllowed = kDeviceAllowed == consent ? true : false;
 
     if (permissions.appRevoked) {
