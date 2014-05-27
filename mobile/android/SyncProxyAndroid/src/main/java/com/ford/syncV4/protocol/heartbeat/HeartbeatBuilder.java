2457efc529d7d36c4d3322b6bae7ea8d60173987
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-05-27 11:40:16 +0300

    Not a subject for review
    	Reason:Trivial changes

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/protocol/heartbeat/HeartbeatBuilder.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol.heartbeat;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 26.05.14
+ * Time: 14:45
+ */
+public class HeartbeatBuilder {
+
+}
