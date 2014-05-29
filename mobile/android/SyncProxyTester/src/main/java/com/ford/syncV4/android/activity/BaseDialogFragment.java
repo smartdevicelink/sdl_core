67f9a22feb350c3652db629951931b34a7fa39d9
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-05-29 11:23:23 +0300

    APPLINK-7427-Add base dialog fragment to manage AppId over fragments

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/BaseDialogFragment.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 28.05.14
+ * Time: 13:32
+ */
+public class BaseDialogFragment {
+
+}
