d3a88b13b58bd47dc5d8c9605c7de8699a5992b2
Alexander Kutsan <AKutsan@luxoft.com>
2014-05-29 18:42:50 +0300

    APPLINK-6707 Configuration Managment

--- tools/Utils/prepare_binaries_for_release.sh
+++ tools/Utils/prepare_binaries_for_release.sh
@@ -3,16 +3,14 @@
 # You should setup build path for your system 
 # and release name
 #
-RELEASE_NAME=SDL_RB_B3.2
+RELEASE_NAME=SDL_RB_B3.5
 
-DEST=/media/Media/development/release/CM/$RELEASE_NAME
+DEST=/media/Media/development/CM/release/$RELEASE_NAME
 PROJ_DIR=/media/Media/development/applink
 
 WEB_BUILD_DIR=/media/Media/development/CM/applink-build
 QT_BUILD_DIR=/media/Media/development/CM/qtcreator-build
 WEB_BUILD_DIR_QNX=/media/Media/development/CM/applink-build-qnx
-
-
 
 # mkdir, cd into it
 mkcd () {
@@ -51,7 +49,7 @@ mkdir -pv $DEST/binaries/{ubuntu1204/{webHMI,qtHMI},qnx650/webHMI}
 chapter "archive HMI and copy to dest"
 mkcd $PROJ_DIR/src/components
 tar -pczf $DEST/HMI.tar.gz HMI
-mkcd $DEST
+cd $DEST
 cp ./HMI.tar.gz $DEST/binaries/ubuntu1204/webHMI/
 cp ./HMI.tar.gz $DEST/binaries/qnx650/webHMI/
 rm $DEST/HMI.tar.gz HMI
@@ -60,11 +58,11 @@ chapter "copy binaries LinuxWEB"
 cp -v $WEB_BUILD_DIR/bin/* $DEST/binaries/ubuntu1204/webHMI/
 echo "HMI/index.html" >  $DEST/binaries/ubuntu1204/webHMI/hmi_link 
 
-chapter "copy binaries LinuxQt"
-cp -v $WEB_BUILD_DIR_QNX/bin/* $DEST/binaries/qnx650/webHMI/*
+chapter "copy binaries QnxWeb"
+cp -v $WEB_BUILD_DIR_QNX/bin/* $DEST/binaries/qnx650/webHMI/
 echo "HMI/index.html" > $DEST/binaries/qnx650/webHMI/hmi_link 
 
-#chapter "copy binaries QnxWeb"
+#chapter "copy binaries LinuxQt"
 #cp -v $QT_BUILD_DIR/bin/smartDeviceLinkCore $DEST/binaries/ubuntu1204/qtHMI/smartDeviceLinkCore
 #cp -v $QT_BUILD_DIR/bin/smartDeviceLink.ini  $DEST/binaries/ubuntu1204/qtHMI/smartDeviceLink.ini 
 #cp -v $QT_BUILD_DIR/bin/policy_table.json $DEST/binaries/ubuntu1204/qtHMI/policy_table.json
