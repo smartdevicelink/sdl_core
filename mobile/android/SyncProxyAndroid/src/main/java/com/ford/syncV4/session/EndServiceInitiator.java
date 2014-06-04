de664addbb35659bce6992748cfb526f6e277df0
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-06-04 16:45:01 +0300

    Not a subject for review
    	Reason:Trivial changes

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/session/EndServiceInitiator.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.session;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 03.06.14
+ * Time: 15:35
+ */
+public enum EndServiceInitiator {
+
+}
