28d3d4ae5d98cc82ded66cf4f345ca715ac46dd2
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-06-05 17:31:17 +0300

    Not a subject for review
    	Reason:Trivial changes

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/util/BTHelper.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.util;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 05.06.14
+ * Time: 13:35
+ */
+public class BTHelper {
+
+}
