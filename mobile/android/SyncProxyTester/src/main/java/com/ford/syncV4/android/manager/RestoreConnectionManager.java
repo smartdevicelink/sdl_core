8dea319b35bea51791b64db7031e2b3a156098d4
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-06-11 17:46:56 +0300

    APPLINK-6558-Add semaphore object to controll session restoring

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/manager/RestoreConnectionManager.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.manager;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11.06.14
+ * Time: 17:31
+ */
+public class RestoreConnectionManager {
+
+}
