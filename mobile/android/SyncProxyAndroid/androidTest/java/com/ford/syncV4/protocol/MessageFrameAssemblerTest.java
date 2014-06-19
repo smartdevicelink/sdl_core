681834b5a23c4ef498142c2384949adf01e2eae2
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-06-19 14:09:06 +0300

    APPLINK-7772-Add MessageFrameAssemblerTest

---
+++ mobile/android/SyncProxyAndroid/androidTest/java/com/ford/syncV4/protocol/MessageFrameAssemblerTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 19.06.14
+ * Time: 11:49
+ */
+public class MessageFrameAssemblerTest {
+
+}
