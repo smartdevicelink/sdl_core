8a5ad3f0b1652911b05012bfa21783a275dce030
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-06-19 14:53:41 +0300

    APPLINK-7772-Add HeartbeatMonitorsManagerTest

---
+++ mobile/android/SyncProxyAndroid/androidTest/java/com/ford/syncV4/protocol/heartbeat/HeartbeatMonitorsManagerTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol.heartbeat;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 19.06.14
+ * Time: 14:11
+ */
+public class HeartbeatMonitorsManagerTest {
+
+}
