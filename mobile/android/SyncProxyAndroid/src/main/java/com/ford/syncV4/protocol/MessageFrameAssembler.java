51edf2b12c2885264660d2767ce419a62b6bb308
Yuriy Chernyshov <YChernyshov@luxoft.com>
2014-06-19 11:47:20 +0300

    APPLINK-7772-Shift class from inner to standalone

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/protocol/MessageFrameAssembler.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 18.06.14
+ * Time: 13:06
+ */
+public class MessageFrameAssembler {
+
+}
---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/protocol/heartbeat/HeartbeatMonitorsManager.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.protocol.heartbeat;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 17.06.14
+ * Time: 12:56
+ */
+public class HeartbeatMonitorsManager {
+
+}
