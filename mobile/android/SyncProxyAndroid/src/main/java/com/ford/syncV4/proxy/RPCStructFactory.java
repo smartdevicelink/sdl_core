8ba96ad7866eadc2ebf7130432033f884b98f2ed
Yuriy <YChernyshov@luxoft.com>
2014-06-24 13:59:52 +0300

    APPLINK-6240-Add struct builder

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/proxy/RPCStructFactory.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.proxy;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 24.06.14
+ * Time: 13:45
+ */
+public class RPCStructFactory {
+
+}
