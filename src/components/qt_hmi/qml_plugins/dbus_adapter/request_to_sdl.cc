c584dd29e68f0fe254032e756d1885aff8907c01
Anatoly Leshin <ALeshin@luxoft.com>
2014-06-26 18:27:33 +0400

    APPLINK-7488 Script gen RequestToSDL header

--- src/components/qt_hmi/qml_plugins/dbus_adapter/request_to_sdl.cc
+++ src/components/qt_hmi/qml_plugins/dbus_adapter/request_to_sdl.cc
@@ -1,1 +1,53 @@
-#include "request_to_sdl.h"
+#include <request_to_sdl.h>
+
+
+RequestToSDL::RequestToSDL(QObject *parent) {
+    QDBusConnection bus = QDBusConnection::sessionBus();
+    interface2 = new QDBusInterface("org.freedesktop.DBus",
+                                   "/",
+                                   "org.freedesktop.DBus",
+                                   bus,
+                                   this);
+    interface1 = new QDBusInterface("org.xfce.PowerManager",
+                                   "/org/xfce/PowerManager",
+                                   "org.xfce.Power.Manager",
+                                   bus,
+                                   this);
+    interface3 = new QDBusInterface("com.ford.sdl.core",
+                                   "/",
+                                   "com.ford.sdl.core.SDL",
+                                   bus,
+                                   this);
+}
+
+RequestToSDL::~RequestToSDL() {
+    interface1->deleteLater();
+    interface2->deleteLater();
+    interface3->deleteLater();
+    this->deleteLater();
+}
+
+void RequestToSDL::Request1(QString name, QJSValue hmi_callback) {
+    QList<QVariant> args;
+    new HMIRequest_Request1(interface1, name, args, hmi_callback);
+}
+
+void RequestToSDL::Request2(QString name, QJSValue hmi_callback) {
+    QList<QVariant> args;
+    new HMIRequest_Request2(interface2, name, args, hmi_callback);
+}
+
+void RequestToSDL::Request3(QString name, QJSValue hmi_callback) {
+    QList<QVariant> args;
+    args << QVariant(7);
+    new HMIRequest_Request3(interface3, name, args, hmi_callback);
+}
+
+
+
+
+
+
+
+
+
--- src/components/qt_hmi/qml_plugins/dbus_adapter/request_to_sdl.h
+++ src/components/qt_hmi/qml_plugins/dbus_adapter/request_to_sdl.h
@@ -12,48 +12,13 @@ class RequestToSDL : public QObject
 {
     Q_OBJECT
   public:
-    explicit RequestToSDL(QObject *parent = 0) {
-        QDBusConnection bus = QDBusConnection::sessionBus();
-        interface2 = new QDBusInterface("org.freedesktop.DBus",
-                                       "/",
-                                       "org.freedesktop.DBus",
-                                       bus,
-                                       this);
-        interface1 = new QDBusInterface("org.xfce.PowerManager",
-                                       "/org/xfce/PowerManager",
-                                       "org.xfce.Power.Manager",
-                                       bus,
-                                       this);
-        interface3 = new QDBusInterface("com.ford.sdl.core",
-                                       "/",
-                                       "com.ford.sdl.core.SDL",
-                                       bus,
-                                       this);
-    }
+    explicit RequestToSDL(QObject *parent = 0);
 
-    ~RequestToSDL() {
-        interface1->deleteLater();
-        interface2->deleteLater();
-        interface3->deleteLater();
+    ~RequestToSDL();
 
-        this->deleteLater();
-    }
-
-    Q_INVOKABLE void Request1(QString name, QJSValue hmi_callback) {
-        QList<QVariant> args;
-        new HMIRequest_Request1(interface1, name, args, hmi_callback);
-    }
-
-    Q_INVOKABLE void Request2(QString name, QJSValue hmi_callback) {
-        QList<QVariant> args;
-        new HMIRequest_Request2(interface2, name, args, hmi_callback);
-    }
-
-    Q_INVOKABLE void Request3(QString name, QJSValue hmi_callback) {
-        QList<QVariant> args;
-        args << QVariant(7);
-        new HMIRequest_Request3(interface3, name, args, hmi_callback);
-    }
+    Q_INVOKABLE void Request1(QString name, QJSValue hmi_callback);
+    Q_INVOKABLE void Request2(QString name, QJSValue hmi_callback);
+    Q_INVOKABLE void Request3(QString name, QJSValue hmi_callback);
 
   private:
     QDBusInterface *interface1;
