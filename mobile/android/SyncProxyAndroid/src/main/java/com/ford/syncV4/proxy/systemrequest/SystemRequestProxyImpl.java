d4d2b2a9ed77ed9a1d95c19e2fd8626fb82f31d0
Yuriy <YChernyshov@luxoft.com>
2014-07-11 15:31:27 +0300

    APPLINK-6712-Add implementation of the ISystemRequestProxy

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/proxy/systemrequest/SystemRequestProxyImpl.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.proxy.systemrequest;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 11.07.14
+ * Time: 10:22
+ */
+public class SystemRequestProxyImpl {
+
+}
