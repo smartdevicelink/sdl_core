ae3155f1baddc1ed8c0ba4a4a7b58b416165042d
Alexander Kutsan <AKutsan@luxoft.com>
2014-07-15 17:29:41 +0300

    APPLINK-8169 Add Policy table validator

---
+++ test/tools/policy_table_validator/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "policy_table/table_struct/types.h"
+#include "json/reader.h"
+#include "utils/file_system.h"
+
+namespace policy_table = rpc::policy_table_interface_base;
+
+enum ResultCode {
+  SUCCES = 0,
+  MISSED_FILE_NAME,
+  READ_ERROR,
+  PARCE_ERROR
+};
+
+void help() {
+  std::cout << "Usage:" << std::endl <<
+               "./policy_validator {file_name}" << std::endl;
+}
+
+int main(int argc, char** argv) {
+  if (argc == 1) {
+    // TODO(AKutsan): No filename
+    std::cout << "file name was missed"<<std::endl;
+    help();
+    exit(MISSED_FILE_NAME);
+  }
+
+  std::string file_name = argv[1];
+  std::string json_string;
+  bool read_result = file_system::ReadFile(file_name, json_string);
+  if (false == read_result) {
+    std::cout << "Read file error "<<std::endl;
+    exit(READ_ERROR);
+  }
+
+  Json::Reader reader;
+  Json::Value value;
+  bool parce_result = reader.parse(json_string, value);
+  if (false == parce_result) {
+    std::cout << "Json parce fails"<<std::endl;
+    exit(PARCE_ERROR);
+  }
+
+  policy_table::Table* table = new policy_table::Table(&value);
+  bool is_valid = table->is_valid();
+  if (true == is_valid ) {
+    std::cout << "Table is valid" << std::endl;
+    exit(SUCCES);
+  }
+  std::cout << "Table is not valid" << std::endl;
+  rpc::ValidationReport report("policy_table");
+  table->ReportErrors(&report);
+  std::cout << "Errors: " << std::endl << rpc::PrettyFormat(report) << std::endl;
+
+  return SUCCES;
+}
