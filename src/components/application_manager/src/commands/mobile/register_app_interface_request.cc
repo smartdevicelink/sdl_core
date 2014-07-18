269a6688101c3f5824353a722154ad921ae817b5
vslobodyanik <vslobodyanik@luxoft.com>
2014-07-18 09:30:01 +0100

    APPLINK-8205:RegisterAppInterface DUPLICATE_NAME if VRSynonyms equal app_name

--- src/components/application_manager/src/commands/mobile/register_app_interface_request.cc
+++ src/components/application_manager/src/commands/mobile/register_app_interface_request.cc
@@ -533,6 +533,26 @@ RegisterAppInterfaceRequest::CheckCoincidence() {
       }
     }
 
+    // vr check
+    if (msg_params.keyExists(strings::vr_synonyms)) {
+      const std::vector<smart_objects::SmartObject>* new_vr =
+          msg_params[strings::vr_synonyms].asArray();
+
+      std::vector<smart_objects::SmartObject>::const_iterator it_vr =
+          new_vr->begin();
+
+      std::vector<smart_objects::SmartObject>::const_iterator it_vr_End =
+          new_vr->end();
+
+      for (; it_vr != it_vr_End; ++it_vr) {
+        std::string vr_synonym = it_vr->asString();
+        if (!strcasecmp(cur_name.c_str(), vr_synonym.c_str())) {
+          LOG4CXX_ERROR(logger_, "Some VR synonyms are known already.");
+          return mobile_apis::Result::DUPLICATE_NAME;
+        }
+      }
+    }  // end vr check
+
   }  // application for end
 
   return mobile_apis::Result::SUCCESS;
