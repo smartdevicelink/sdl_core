9d6874926c51cd1d381ec0684b371c7dfa294deb
Andrew Batutin <ABatutin@luxoft.com>
2014-07-24 16:41:06 +0300

    probably fixed issue with race condition for decryption frame

--- mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/protocol/WiProProtocol.java
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/protocol/WiProProtocol.java
@@ -7,7 +7,6 @@ import com.ford.syncV4.proxy.constants.ProtocolConstants;
 import com.ford.syncV4.session.Session;
 import com.ford.syncV4.util.logger.Logger;
 
-import java.util.HashMap;
 import java.util.Hashtable;
 
 public class WiProProtocol extends AbstractProtocol {
@@ -207,7 +206,7 @@ public class WiProProtocol extends AbstractProtocol {
     }
 
     @Override
-    public void HandleReceivedBytes(byte[] receivedBytes, int receivedBytesLength) {
+    public synchronized void HandleReceivedBytes(byte[] receivedBytes, int receivedBytesLength) {
         int receivedBytesReadPos = 0;
 
         //Logger.d(CLASS_NAME + " -> current protocol ver:" + getProtocolVersion());
