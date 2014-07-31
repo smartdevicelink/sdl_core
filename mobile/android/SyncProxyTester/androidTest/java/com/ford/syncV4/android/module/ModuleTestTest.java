405087eca3f13c3a768a6e443b16e29b412aa846
Yuriy <YChernyshov@luxoft.com>
2014-07-31 15:45:10 +0300

    APPLINK-8449-Add Unit testing for the request - responses comparing

---
+++ mobile/android/SyncProxyTester/androidTest/java/com/ford/syncV4/android/module/ModuleTestTest.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.module;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 31.07.14
+ * Time: 15:14
+ */
+public class ModuleTestTest {
+
+}
