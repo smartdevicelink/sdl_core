9c2eeffe875ecb06dfe818922e8deef97a336f02
Yurii Chernyshov <ychernyshov@luxoft.com>
2014-08-05 10:43:17 +0300

    Not a subject for review
       Reason: Code refactoring

---
+++ mobile/android/SyncProxyTester/src/main/java/com/ford/syncV4/android/activity/AlertDialog.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.android.activity;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 05.08.14
+ * Time: 9:50
+ */
+public class AlertDialog {
+
+}
