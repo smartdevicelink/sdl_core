556bf6e0311338e3cfb1d84b3aff79bcc8598b02
Yurii Chernyshov <ychernyshov@luxoft.com>
2014-08-07 15:10:52 +0300

    APPLINK-8529-Update API to v4_protocol_v3_1_revA

---
+++ mobile/android/SyncProxyAndroid/src/main/java/com/ford/syncV4/proxy/rpc/SendLocation.java
@@ -0,0 +1,11 @@
+package com.ford.syncV4.proxy.rpc;
+
+/**
+ * Created with Android Studio.
+ * Author: Chernyshov Yuriy - Mobile Development
+ * Date: 07.08.14
+ * Time: 14:33
+ */
+public class SendLocation {
+
+}
