ee633de557f350bf2e0c3ff3329fbfed6b9eae01
Dmitriy Klimenko <dklimenko@luxoft.com>
2014-08-25 02:24:02 -0700

    Mqueue OnAudioPassThru fix

--- src/components/media_manager/src/audio/audio_stream_sender_thread.cc
+++ src/components/media_manager/src/audio/audio_stream_sender_thread.cc
@@ -54,9 +54,7 @@ namespace media_manager {
 using sync_primitives::AutoLock;
 
 const int32_t AudioStreamSenderThread::kAudioPassThruTimeout = 1;
-#ifdef CUSTOMER_PASA
-const uint32_t kMqueueMessageSize = 65536;
-#endif // CUSTOMER_PASA
+const uint32_t kMqueueMessageSize = 4095;
 
 CREATE_LOGGERPTR_GLOBAL(logger_, "AudioPassThruThread")
 
@@ -69,9 +67,7 @@ AudioStreamSenderThread::AudioStreamSenderThread(
 }
 
 AudioStreamSenderThread::~AudioStreamSenderThread() {
-#ifdef CUSTOMER_PASA
   mq_unlink(fileName_.c_str());
-#endif // CUSTOMER_PASA
 }
 
 void AudioStreamSenderThread::threadMain() {
@@ -79,20 +75,18 @@ void AudioStreamSenderThread::threadMain() {
 
   offset_ = 0;
 
-  while (false == getShouldBeStopped()) {
-
-#ifndef CUSTOMER_PASA
-    usleep(kAudioPassThruTimeout * 1000000);
+  while (true) {
     if (getShouldBeStopped()) {
       break;
     }
-#endif
 
-#ifdef CUSTOMER_PASA
+    usleep(kAudioPassThruTimeout * 1000000);
+
+    if (getShouldBeStopped()) {
+      break;
+    }
+
     mqSendAudioChunkToMobile();
-#else
-    sendAudioChunkToMobile();
-#endif // CUSTOMER_PASA
   }
 
   LOG4CXX_TRACE_EXIT(logger_);
@@ -137,7 +131,6 @@ void AudioStreamSenderThread::sendAudioChunkToMobile() {
 #endif
 }
 
-#ifdef CUSTOMER_PASA
 void AudioStreamSenderThread::mqSendAudioChunkToMobile() {
   mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
   const mqd_t handle = mq_open(fileName_.c_str(), O_RDWR, mode, 0);
@@ -148,11 +141,25 @@ void AudioStreamSenderThread::mqSendAudioChunkToMobile() {
     return;
   }
 
-  std::vector<uint8_t> data;
-  data.reserve(kMqueueMessageSize);
-  const ssize_t dataSize = mq_receive(handle,
-                                    reinterpret_cast<char*>(&data.front()),
-                                    data.size(), 0);
+  struct mq_attr attr;
+  char* buffer;
+
+  if (-1 == mq_getattr(handle, &attr)) {
+    LOG4CXX_ERROR(logger_, "Unable to read mq_attributes: "
+                    << strerror(errno));
+    return;
+  } else {
+    LOG4CXX_INFO(logger_, "mq_msgsize = " << attr.mq_msgsize);
+  }
+
+  buffer = new char[attr.mq_msgsize];
+  if (buffer == NULL) {
+    LOG4CXX_INFO(logger_, "Memory allocation error");
+    return;
+  }
+
+  const ssize_t dataSize = mq_receive(handle, buffer, attr.mq_msgsize, 0);
+
   if (-1 == dataSize) {
     LOG4CXX_ERROR(logger_, "Unable to recieve data from mqueue: "
                   << strerror(errno));
@@ -162,12 +169,15 @@ void AudioStreamSenderThread::mqSendAudioChunkToMobile() {
   LOG4CXX_INFO(logger_, "The " << dataSize
                 << " bytes have been successfully obtained.");
 
-  data.resize(dataSize);
+  std::vector<uint8_t> data(buffer, buffer + dataSize);
+
+  delete[] buffer;
+
+  LOG4CXX_INFO(logger_, "data.size() =  " << data.size());
 
   application_manager::ApplicationManagerImpl::instance()->
   SendAudioPassThroughNotification(session_key_, data);
 }
-#endif // CUSTOMER_PASA
 
 bool AudioStreamSenderThread::getShouldBeStopped() {
   AutoLock auto_lock(shouldBeStoped_lock_);
