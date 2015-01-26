b9911296f7b8b10bed62c5a77e3bdbe2d9e38308
Konstantin Kolodiy <KKolodiy@luxoft.com>
2015-01-26 17:11:10 +0300

    New test of CanAppExtension

--- test/components/can_cooperation/src/can_app_extension_test.cc
+++ test/components/can_cooperation/src/can_app_extension_test.cc
@@ -1,15 +1,25 @@
 #include "gtest/gtest.h"
-#include "gmock/gmock.h"
 #include "can_cooperation/can_app_extension.h"
 
-using namespace can_cooperation;
+using can_cooperation::CANAppExtension;
 
-TEST(can_app_extension, create) {
+namespace test {
+namespace components {
+namespace can_cooperation {
+
+TEST(CanAppExtensionTest, Create) {
   CANAppExtension extension(7);
   ASSERT_TRUE(extension.uid() == 7);
 }
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleMock(&argc, argv);
-  return RUN_ALL_TESTS();
+TEST(CanAppExtensionTest, Control) {
+  CANAppExtension extension(3);
+  ASSERT_EQ(3, extension.uid());
+  ASSERT_FALSE(extension.IsControlGiven());
+  extension.GiveControl(true);
+  ASSERT_TRUE(extension.IsControlGiven());
 }
+
+}  // namespace can_cooperation
+}  // namespace components
+}  // namespace test
