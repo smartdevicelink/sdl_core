ea63a0c677b8175a1b7452c6555c2932990b32ee
Kozoriz <kozorizandriy@gmail.com>
2015-12-23 15:28:14 +0200

    Enabled security_query_test in security_manager
    	Corrected CMakefile(enabled test)
    	Corrected #include of mock_security_manager

--- src/components/security_manager/test/security_query_test.cc
+++ src/components/security_manager/test/security_query_test.cc
@@ -36,7 +36,7 @@
 #include "security_manager/security_query.h"
 #include "protocol_handler/protocol_payload.h"
 #include "utils/byte_order.h"
-#include "include/security_manager_mock.h"
+#include "security_manager/mock_security_manager.h"
 
 // Test values for compare after serialization and byteorder conversion
 #define SEQ_NUMBER    0x12345678u
