35abe5200347d11e7b6012fd45176f2bd731abe4
Kozoriz <kozorizandriy@gmail.com>
2016-02-29 10:38:32 +0200

    Remove singleton from ProtocolHandler
    	Remove singletone.
    	Corrected tests.
    	Created new mocks and settings.
    
    	Related to : APPLINK-21440

--- src/components/include/protocol_handler/protocol_handler.h
+++ src/components/include/protocol_handler/protocol_handler.h
@@ -33,6 +33,7 @@
 #define SRC_COMPONENTS_INCLUDE_PROTOCOL_HANDLER_PROTOCOL_HANDLER_H_
 
 #include "protocol/common.h"
+#include "protocol_handler/protocol_handler_settings.h"
 
 /**
  *\namespace protocol_handlerHandler
@@ -100,6 +101,11 @@ class ProtocolHandler {
   virtual void SendEndService(int32_t connection_id,
                               uint8_t session_id,
                               uint8_t service_type) = 0;
+  /**
+   * \brief Protocol handler settings getter
+   * \return pointer to protocol handler settings class
+   */
+  virtual const ProtocolHandlerSettings& get_settings() const = 0;
 
  protected:
   /**
