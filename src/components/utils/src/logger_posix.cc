415bc94192355e934009174f6e6392a753dfb401
Artem Nosach <ANosach@luxoft.com>
2016-04-14 14:45:07 +0300

    Forbid logger sharing between modules
    Related-issues: APPLINK-22713, APPLINK-22716

--- src/components/utils/src/logger_posix.cc
+++ src/components/utils/src/logger_posix.cc
@@ -39,7 +39,7 @@
 #include <log4cxx/propertyconfigurator.h>
 #include <log4cxx/spi/loggingevent.h>
 
-logger::Logger::Pimpl logger::Logger::impl_;
+__attribute__((visibility("hidden"))) logger::Logger::Pimpl logger::Logger::impl_;
 
 ////////////////////////////////////////////////////////////////////////////////
 /// logger::Logger::Impl
