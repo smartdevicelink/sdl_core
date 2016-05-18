8924a0e181d7c05c5eb5c4d5b5da4d0a17ad2f8d
Artem Nosach <ANosach@luxoft.com>
2016-05-18 10:14:08 +0300

    Make google test buildable on Windows
    Related-issues: APPLINK-22713, APPLINK-22716

--- src/3rd_party-static/gmock-1.7.0/gtest/cmake/internal_utils.cmake
+++ src/3rd_party-static/gmock-1.7.0/gtest/cmake/internal_utils.cmake
@@ -24,16 +24,18 @@ macro(fix_default_compiler_settings_)
     foreach (flag_var
              CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
              CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-      if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt)
-        # When Google Test is built as a shared library, it should also use
-        # shared runtime libraries.  Otherwise, it may end up with multiple
-        # copies of runtime library data in different modules, resulting in
-        # hard-to-find crashes. When it is built as a static library, it is
-        # preferable to use CRT as static libraries, as we don't have to rely
-        # on CRT DLLs being available. CMake always defaults to using shared
-        # CRT libraries, so we override that default here.
-        string(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
-      endif()
+      # When Google Test is built as a shared library, it should also use
+      # shared runtime libraries.  Otherwise, it may end up with multiple
+      # copies of runtime library data in different modules, resulting in
+      # hard-to-find crashes. When it is built as a static library, it is
+      # preferable to use CRT as static libraries, as we don't have to rely
+      # on CRT DLLs being available. CMake always defaults to using shared
+      # CRT libraries, so we override that default here.
+	  
+      # ANosach: /MD flag should be enabled for SDL build
+      # if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt)
+        # string(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
+      # endif()
 
       # We prefer more strict warning checking for building Google Test.
       # Replaces /W3 with /W4 in defaults.
