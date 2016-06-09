ac83683ec87c95ca261f089a8e3bb4dd5b33bde4
Artem Nosach <ANosach@luxoft.com>
2016-06-09 17:41:20 +0300

    Add cmake tools
    Related-issue: APPLINK-21604

--- ms/cmake_generate.cmd
+++ ms/cmake_generate.cmd
@@ -8,13 +8,13 @@ pushd %SDL_BUILD_DIR%
 if %errorlevel% == 1 goto builddir_error
 
 if %GEN_TYPE% == WinQt_x86 (
-  set CMAKE_COMMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% -DCMAKE_INSTALL_PREFIX:PATH="%SDL_QT_DIR%\5.5\msvc2010" -DQT_PORT=1 -DCMAKE_BUILD_TYPE=Debug %SDL_ROOT%
+  set CMAKE_COMMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% -DCMAKE_INSTALL_PREFIX:PATH="%SDL_QT_DIR%\5.5\msvc2010" -DQT_PORT=1 -DARCH_X86=1 -DSDK_QT=1 -DCMAKE_BUILD_TYPE=Debug %SDL_ROOT%
   @echo Generating for the win Qt x86: !CMAKE_COMMAND!
   call !CMAKE_COMMAND!
   goto end
 )
 if %GEN_TYPE% == WinNative_x64 (
-  set CMAKE_COMMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% %SDL_ROOT%
+  set CMAKE_COMMAND=cmake -G %SDL_CMAKE_GENERATOR_NAME% -DARCH_X64=1 %SDL_ROOT%
   @echo Generating for the win native x64: !CMAKE_COMMAND!
   call !CMAKE_COMMAND!
   goto end
