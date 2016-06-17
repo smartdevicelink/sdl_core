643293a41bf3ee5183b049ec164ff1a76c865432
Alexander Kutsan <akutsan@luxoft.com>
2016-06-17 17:50:18 +0300

    Exclude commands from coverage report (#617)
    Currently commands are not covered and even do not included in coverage
     report.
     In case if any unit tests will include commands object files, coverage
     will fail to 40 - 50%
     Commit  will shadow this failing until mout of commands won't be
     covered with UT
    
     Related issue: APPLINK-25332

--- tools/infrastructure/collect_coverage.sh
+++ tools/infrastructure/collect_coverage.sh
@@ -18,7 +18,7 @@ rm -rf $REPORTS_DIR -
 
 mkdir $COVERAGE_DIR
 lcov --quiet --capture --directory . --output-file $COVERAGE_DIR/full_report.info
-lcov --quiet --remove $COVERAGE_DIR/full_report.info '/usr/*' '*/test/*' '*/src/3rd*' '*/build/src/*'  --output-file $COVERAGE_DIR/coverage.info
+lcov --quiet --remove $COVERAGE_DIR/full_report.info '/usr/*' '*/test/*' '*/commands/*' '*/src/3rd*' '*/build/src/*'  --output-file $COVERAGE_DIR/coverage.info
 
 mkdir $REPORTS_DIR
 genhtml --quiet $COVERAGE_DIR/coverage.info --output-directory $REPORTS_DIR
