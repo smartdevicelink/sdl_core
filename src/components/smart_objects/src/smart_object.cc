d1d0909aaebc55164f232188060299b9c276e11b
BrandonHe <lovinghesl@hotmail.com>
2016-07-06 11:00:47 +0800

    remove NULL check before deleting

--- src/components/smart_objects/src/smart_object.cc
+++ src/components/smart_objects/src/smart_object.cc
@@ -732,28 +732,20 @@ void SmartObject::duplicate(const SmartObject& OtherObject) {
 void SmartObject::cleanup_data() {
   switch (m_type) {
     case SmartType_String:
-      if (m_data.str_value) {
-        delete m_data.str_value;
-        m_data.str_value = NULL;
-      }
+      delete m_data.str_value;
+      m_data.str_value = NULL;
       break;
     case SmartType_Map:
-      if (m_data.map_value) {
-        delete m_data.map_value;
-        m_data.map_value = NULL;
-      }
+      delete m_data.map_value;
+      m_data.map_value = NULL;
       break;
     case SmartType_Array:
-      if (m_data.array_value) {
-        delete m_data.array_value;
-        m_data.array_value = NULL;
-      }
+      delete m_data.array_value;
+      m_data.array_value = NULL;
       break;
     case SmartType_Binary:
-      if (m_data.binary_value) {
-        delete m_data.binary_value;
-        m_data.array_value = NULL;
-      }
+      delete m_data.binary_value;
+      m_data.array_value = NULL;
       break;
     default:
       break;
