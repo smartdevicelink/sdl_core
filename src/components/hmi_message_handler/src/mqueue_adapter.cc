5e11013e4453ce2c8efd5abce781a8ab2fcde706
VProdanov <VProdanov@luxoft.com>
2016-07-15 08:59:08 +0300

    Activate TestTransportManagerDefault_Init_LastStateUsed test
    The reported deadlock on thread create
    under Windows Qt was not reproduced.
    
    Related issue: APPLINK-27939

--- src/components/hmi_message_handler/src/mqueue_adapter.cc
+++ src/components/hmi_message_handler/src/mqueue_adapter.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Ford Motor Company
+ * Copyright (c) 2016, Ford Motor Company
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,6 +29,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <time.h>
 
 #include "hmi_message_handler/mqueue_adapter.h"
 #include "hmi_message_handler/hmi_message_handler.h"
