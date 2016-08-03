0fcddeec5056ac56da8d221f119dd4f42db8fcc1
Viktoriya Lambova <vlambova@luxoft.com>
2016-08-03 02:38:46 -0700

    Cover HMI commands with Unit Tests. Part 8
    Following HMI Covered With Unit Tests:
    ui_add_command_request
    ui_add_command_response
    ui_add_submenu_request
    ui_add_submenu_response
    ui_alert_request
    ui_alert_response
    ui_change_registration_request
    ui_change_registration_response
    ui_delete_command_request
    ui_delete_command_response
    
    Related issue: APPLINK-25922

--- src/components/application_manager/test/commands/hmi/simple_requests_to_hmi_test.cc
+++ src/components/application_manager/test/commands/hmi/simple_requests_to_hmi_test.cc
@@ -41,6 +41,18 @@
 #include "hmi/vi_is_ready_request.h"
 #include "hmi/vi_read_did_request.h"
 #include "hmi/vi_subscribe_vehicle_data_request.h"
+#include "hmi/dial_number_request.h"
+#include "hmi/tts_is_ready_request.h"
+#include "hmi/tts_set_global_properties_request.h"
+#include "hmi/tts_speak_request.h"
+#include "hmi/tts_stop_speaking_request.h"
+#include "hmi/tts_get_supported_languages_request.h"
+#include "hmi/close_popup_request.h"
+#include "hmi/ui_add_command_request.h"
+#include "hmi/ui_add_submenu_request.h"
+#include "hmi/ui_alert_request.h"
+#include "hmi/ui_change_registration_request.h"
+#include "hmi/ui_delete_command_request.h"
 
 namespace test {
 namespace components {
@@ -87,7 +99,19 @@ class RequestToHMICommandsTest
 typedef Types<commands::VIIsReadyRequest,
               commands::VIGetVehicleTypeRequest,
               commands::VIReadDIDRequest,
-              commands::VISubscribeVehicleDataRequest> RequestCommandsList;
+              commands::VISubscribeVehicleDataRequest,
+              commands::hmi::DialNumberRequest,
+              commands::ClosePopupRequest,
+              commands::TTSIsReadyRequest,
+              commands::TTSSetGlobalPropertiesRequest,
+              commands::TTSSpeakRequest,
+              commands::TTSStopSpeakingRequest,
+              commands::TTSGetSupportedLanguagesRequest,
+              commands::UIAddCommandRequest,
+              commands::UIAddSubmenuRequest,
+              commands::UIAlertRequest,
+              commands::UIChangeRegistrationRequest,
+              commands::UIDeleteCommandRequest> RequestCommandsList;
 
 TYPED_TEST_CASE(RequestToHMICommandsTest, RequestCommandsList);
 
