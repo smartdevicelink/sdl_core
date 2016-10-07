35f2026273fd7983b20cbdbdb05f2ed6a4f08834
Vladislav Antonov (GitHub) <vlantonov@luxoft.com>
2016-10-07 19:15:08 +0300

    Fix defects found by application_manager_test under WinQt (#878)
    Refactored application_manager_test
    Fixed thread pool size under WinQt
    
    Related issue: APPLINK-27148

--- src/components/utils/src/threads/thread_qt.cc
+++ src/components/utils/src/threads/thread_qt.cc
@@ -126,6 +126,12 @@ Thread::Thread(const char* name, ThreadDelegate* delegate, QObject* parent)
     , thread_command_(kThreadCommandNone)
     , thread_state_(kThreadStateNone) {
   qRegisterMetaType<QThread*>("QThread*");
+  // The number of the threads was set to the number
+  // of threads used in SDL according to ListOfThreads.
+  // See APPLINK-27148 for details.
+  // This is done in order to prevent the blocking
+  // of thread at start due to insufficient pool size.
+  QThreadPool::globalInstance()->setMaxThreadCount(45);
 #ifdef THREAD_COUNT
   ThreadCounter::Increment();
 #endif  // THREAD_COUNT
