94be7067ee72ec0c17aed74924a8ac8f9e9a4eb0
Alexander Kutsan <AKutsan@luxoft.com>
2016-11-28 16:17:37 +0200

    Remove unused files

--- src/components/application_manager/include/application_manager/policies/pt_exchange_handler_impl.h
+++
@@ -1,6 +0,0 @@
-#ifdef EXTERNAL_PROPRIETARY
-#error "pt_exchange_handler_impl.h is not available in policy premium"
-#else
-#include "application_manager/policies/regular/pt_exchange_handler_impl.h"
-#endif
-
