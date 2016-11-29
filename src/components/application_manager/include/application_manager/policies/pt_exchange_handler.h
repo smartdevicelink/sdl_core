e5c846dc57f59c828d596dad0e53e70a07412620
Andrey Oleynik <aoleynik@luxoft.com>
2016-11-29 13:22:52 +0200

    Renames folder missed by PR#1068
    Closes-bug: APPLINK-30334

---
+++ src/components/application_manager/include/application_manager/policies/pt_exchange_handler.h
@@ -0,0 +1,6 @@
+#ifdef EXTERNAL_PROPRIETARY
+#error "pt_exchange_handler.h is not available in policy external"
+#else
+#include "application_manager/policies/regular/pt_exchange_handler.h"
+#endif
+
