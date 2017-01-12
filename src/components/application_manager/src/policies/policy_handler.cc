4a573369794156ee3c398c8b191a51ac1a46fa38
atimchenko <atymchenko@luxoft.com>
2017-01-12 13:18:34 +0200

    Fix SDL omits URL during retry sequence
     Fix SDL omits one of the URLs while cyclically changing during retry sequence
    
     Related defect APPLINK-31325

--- src/components/application_manager/src/policies/policy_handler.cc
+++ src/components/application_manager/src/policies/policy_handler.cc
@@ -1388,6 +1388,7 @@ void PolicyHandler::OnSnapshotCreated(const BinaryMessage& pt_string) {
   }
 
   SendMessageToSDK(pt_string, urls.at(current_app).url.at(current_url));
+  if(!(urls.at(current_app).app_id == policy::kDefaultId))
   current_url++;
 #endif  // PROPRIETARY_MODE
   // reset update required false
