2fc8995412a0f9df117b76ae71696fea6b91e402
Sergey Levchenko (GitHub) <slevchenko.work@gmail.com>
2017-03-09 17:18:44 +0200

    Fix unit tests

--- src/components/application_manager/test/commands/mobile/unsubscribe_button_request_test.cc
+++ src/components/application_manager/test/commands/mobile/unsubscribe_button_request_test.cc
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2017, Ford Motor Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ford Motor Company nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <stdint.h>
 #include <string>
 
@@ -6,6 +38,7 @@
 #include "application_manager/commands/command_request_test.h"
 #include "application_manager/mock_application_manager.h"
 #include "application_manager/mock_message_helper.h"
+#include "application_manager/mock_hmi_capabilities.h"
 #include "application_manager/commands/mobile/unsubscribe_button_request.h"
 
 namespace test {
@@ -21,6 +54,7 @@ using ::testing::_;
 
 using am::commands::UnsubscribeButtonRequest;
 using am::commands::MessageSharedPtr;
+using application_manager_test::MockHMICapabilities;
 
 typedef ::utils::SharedPtr<UnsubscribeButtonRequest> CommandPtr;
 
@@ -30,7 +64,14 @@ const mobile_apis::ButtonName::eType kButtonId = mobile_apis::ButtonName::OK;
 }  // namespace
 
 class UnsubscribeButtonRequestTest
-    : public CommandRequestTest<CommandsTestMocks::kIsNice> {};
+    : public CommandRequestTest<CommandsTestMocks::kIsNice> {
+ public:
+  MOCK(MockHMICapabilities) mock_hmi_capabilities_;
+  UnsubscribeButtonRequestTest() {
+    ON_CALL(app_mngr_, hmi_capabilities())
+        .WillByDefault(ReturnRef(mock_hmi_capabilities_));
+  }
+};
 
 TEST_F(UnsubscribeButtonRequestTest, Run_AppNotRegistered_UNSUCCESS) {
   CommandPtr command(CreateCommand<UnsubscribeButtonRequest>());
@@ -47,7 +88,7 @@ TEST_F(UnsubscribeButtonRequestTest, Run_AppNotRegistered_UNSUCCESS) {
 }
 
 TEST_F(UnsubscribeButtonRequestTest,
-       Run_UnsubscribeNotSubscribedButton_UNSUCCESS) {
+       Run_UnsubscribeNotSubscribedButton_UNSUPPORTED_RESOURCE) {
   MessageSharedPtr command_msg(CreateMessage(smart_objects::SmartType_Map));
   (*command_msg)[am::strings::params][am::strings::connection_key] =
       kConnectionKey;
@@ -59,12 +100,9 @@ TEST_F(UnsubscribeButtonRequestTest,
   EXPECT_CALL(app_mngr_, application(kConnectionKey))
       .WillOnce(Return(mock_app));
 
-  EXPECT_CALL(*mock_app, UnsubscribeFromButton(kButtonId))
-      .WillOnce(Return(false));
-
-  EXPECT_CALL(
-      app_mngr_,
-      ManageMobileCommand(MobileResultCodeIs(mobile_result::IGNORED), _));
+  EXPECT_CALL(app_mngr_,
+              ManageMobileCommand(
+                  MobileResultCodeIs(mobile_result::UNSUPPORTED_RESOURCE), _));
 
   command->Run();
 }
@@ -82,6 +120,13 @@ TEST_F(UnsubscribeButtonRequestTest, Run_SUCCESS) {
   MockAppPtr mock_app(CreateMockApp());
   EXPECT_CALL(app_mngr_, application(kConnectionKey))
       .WillOnce(Return(mock_app));
+
+  EXPECT_CALL(mock_hmi_capabilities_, is_ui_cooperating())
+      .WillOnce(Return(true));
+  MessageSharedPtr button_cap(CreateMessage(smart_objects::SmartType_Array));
+  (*button_cap)[0][am::hmi_response::button_name] = kButtonId;
+  EXPECT_CALL(mock_hmi_capabilities_, button_capabilities())
+      .WillOnce(Return(button_cap.get()));
 
   EXPECT_CALL(*mock_app, UnsubscribeFromButton(kButtonId))
       .WillOnce(Return(true));
