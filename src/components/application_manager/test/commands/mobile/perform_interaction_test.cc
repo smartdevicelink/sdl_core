55aa81c67695205de3d2a3fa8be814ac3e9b46d7
pold500 <paveld500@gmail.com>
2017-03-29 11:51:06 +0300

    Fix codestyle for project files

--- src/components/application_manager/test/commands/mobile/perform_interaction_test.cc
+++ src/components/application_manager/test/commands/mobile/perform_interaction_test.cc
@@ -237,7 +237,9 @@ TEST_F(PerformInteractionRequestTest, DISABLED_OnTimeout_VR_GENERIC_ERROR) {
   EXPECT_EQ(
       (*vr_command_result)[strings::msg_params][strings::result_code].asInt(),
       static_cast<int32_t>(am::mobile_api::Result::GENERIC_ERROR));
-  EXPECT_FALSE((*vr_command_result)[strings::msg_params][strings::info].asString().empty());
+  EXPECT_FALSE((*vr_command_result)[strings::msg_params][strings::info]
+                   .asString()
+                   .empty());
 }
 
 TEST_F(
