97583341890a91223bd122275e79f7caa60260f2
JackLivio <jack@livio.io>
2017-06-07 17:04:42 -0400

    Install tornado if does not exist.

--- src/appMain/start_external_proprietary.sh
+++ src/appMain/start_external_proprietary.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+pip list | grep -F tornado
+if [ $? -eq 1 ]
+  then
+    sudo pip install tornado
+fi
 python sample_policy_manager.py --pack_port 8088 --unpack_port 8089 --add_http_header --encryption &
 POLICY_MANAGER=$!
 
