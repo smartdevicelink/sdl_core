9d2148a9ba9edc814654b97b70c4a0f999ebcf8b
jacobkeeler <jacob.keeler@livioradio.com>
2017-06-08 13:32:22 -0400

    Updated README and included messages in start script for clarity

--- src/appMain/start_external_proprietary.sh
+++ src/appMain/start_external_proprietary.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-pip list | grep -F tornado
+pip list | grep -F tornado > /dev/null
 if [ $? -eq 1 ]
   then
+    echo "Installing tornado python package"
     sudo pip install tornado
 fi
+echo "Starting Policy Manager"
 python sample_policy_manager.py --pack_port 8088 --unpack_port 8089 --add_http_header --encryption &
 POLICY_MANAGER=$!
 
@@ -15,4 +17,5 @@ function ctrl_c() {
         kill -9 $POLICY_MANAGER
 }
 
+echo "Starting SmartDeviceLinkCore"
 LD_LIBRARY_PATH=. ./smartDeviceLinkCore
