62729343d52060d7265bf48c6733f261e2e47af6
Alexander Kutsan <akutsan@luxoft.com>
2017-07-26 18:26:18 +0300

    Test

---
+++ src/components/remote_control/include/remote_control/resource_allocation_manager.h
@@ -0,0 +1,10 @@
+#ifndef RESOURCE_ALLOCATION_H
+#define RESOURCE_ALLOCATION_H
+namespace remote_control {
+
+namespace AquireResult {
+enum eType { ALLOWED; DISALLOWED; ASK_DRIVER; };
+}
+class ResourceAlocationManager {};
+}  // namespace remote_control
+#endif  // RESOURCE_ALLOCATION_H
