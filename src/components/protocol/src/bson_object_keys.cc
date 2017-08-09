8762264dc82437687a00fca7140187dfc919904a
jacobkeeler <jacob.keeler@livioradio.com>
2017-08-09 16:30:42 -0400

    Add constants for BSON object keys

---
+++ src/components/protocol/src/bson_object_keys.cc
@@ -0,0 +1,14 @@
+#include <protocol/bson_object_keys.h>
+
+namespace protocol_handler {
+
+namespace strings {
+
+const char* hash_id = "hashId";
+const char* protocol_version = "protocolVersion";
+const char* mtu = "mtu";
+const char* rejected_params = "rejectedParams";
+
+}  // namespace strings
+
+}  // namespace protocol_handler
