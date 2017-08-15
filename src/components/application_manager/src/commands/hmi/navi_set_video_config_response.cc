fb4f46d24fb968d36b52a2239900771781353bdb
Sho Amano <samano@xevo.com>
2017-08-15 13:47:27 +0900

    fix: inconsistency in license headers

--- src/components/application_manager/src/commands/hmi/navi_set_video_config_response.cc
+++ src/components/application_manager/src/commands/hmi/navi_set_video_config_response.cc
@@ -13,7 +13,7 @@
  * disclaimer in the documentation and/or other materials provided with the
  * distribution.
  *
- * Neither the name of the Ford Motor Company nor the names of its contributors
+ * Neither the names of the copyright holders nor the names of its contributors
  * may be used to endorse or promote products derived from this software
  * without specific prior written permission.
  *
