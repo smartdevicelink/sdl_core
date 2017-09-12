007aeedc6553767d79f2e79a3e2edab80a309b65
Takamitsu Yoshii <tyoshii@xevo.com>
2017-09-12 14:28:58 +0900

    Do not send OnVideoDataStreaming before streaming is approved

--- src/components/application_manager/src/application_impl.cc
+++ src/components/application_manager/src/application_impl.cc
@@ -526,6 +526,10 @@ void ApplicationImpl::WakeUpStreaming(
   LOG4CXX_AUTO_TRACE(logger_);
 
   if (ServiceType::kMobileNav == service_type) {
+    if (!video_streaming_approved()) {
+      LOG4CXX_INFO(logger_, "Video streaming not approved yet");
+      return;
+    }
     sync_primitives::AutoLock lock(video_streaming_suspended_lock_);
     if (video_streaming_suspended_) {
       application_manager_.OnAppStreaming(app_id(), service_type, true);
@@ -536,6 +540,10 @@ void ApplicationImpl::WakeUpStreaming(
     video_stream_suspend_timer_.Start(video_stream_suspend_timeout_,
                                       timer::kPeriodic);
   } else if (ServiceType::kAudio == service_type) {
+    if (!audio_streaming_approved()) {
+      LOG4CXX_INFO(logger_, "Audio streaming not approved yet");
+      return;
+    }
     sync_primitives::AutoLock lock(audio_streaming_suspended_lock_);
     if (audio_streaming_suspended_) {
       application_manager_.OnAppStreaming(app_id(), service_type, true);
