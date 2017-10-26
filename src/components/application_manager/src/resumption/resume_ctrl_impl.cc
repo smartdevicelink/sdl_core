58af36f00d9af10d18274251d750cf43b4b9e190
Frank <fronneburg@xevo.com>
2017-10-26 16:56:48 -0700

    handle crash during cleanup after failed init

--- src/components/application_manager/src/resumption/resume_ctrl_impl.cc
+++ src/components/application_manager/src/resumption/resume_ctrl_impl.cc
@@ -265,8 +265,10 @@ void ResumeCtrlImpl::OnSuspend() {
   LOG4CXX_AUTO_TRACE(logger_);
   StopSavePersistentDataTimer();
   SaveAllApplications();
-  resumption_storage_->OnSuspend();
-  resumption_storage_->Persist();
+  if (resumption_storage_) {
+    resumption_storage_->OnSuspend();
+    resumption_storage_->Persist();
+  }
 }
 
 void ResumeCtrlImpl::OnAwake() {
