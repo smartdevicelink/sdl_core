b18c1943cf3a8cf5046d1a223cddbd8f9a75858f
Alexander Kutsan <akutsan@luxoft.com>
2018-02-09 11:37:49 +0200

    Add header stubs for RC plugin

---
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/hmi/rc_get_interior_vehicle_data_response.h
@@ -0,0 +1,19 @@
+#ifndef SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_HMI_RC_GET_INTERIOR_VEHICLE_DATA_RESPONSE_H
+#define SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_HMI_RC_GET_INTERIOR_VEHICLE_DATA_RESPONSE_H
+
+#include "application_manager/commands/response_to_hmi.h"
+#include "utils/macro.h"
+
+namespace rc_rpc_plugin {
+namespace commands {
+class RCGetInteriorVehicleDataResponse
+    : public application_manager::commands::ResponseToHMI {
+ public:
+  bool Init() OVERRIDE;
+  void Run() OVERRIDE;
+  void on_event(const application_manager::event_engine::Event& event) OVERRIDE;
+};
+}  // namespace commands
+}  // namespace rc_rpc_plugin
+
+#endif  // SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_HMI_RC_GET_INTERIOR_VEHICLE_DATA_RESPONSE_H
---
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/hmi/rc_on_remote_control_settings_notification.h
@@ -0,0 +1,19 @@
+#ifndef SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_HMI_RC_ON_REMOTE_CONTROL_SETTINGS_NOTIFICATION_H
+#define SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_HMI_RC_ON_REMOTE_CONTROL_SETTINGS_NOTIFICATION_H
+
+#include "application_manager/commands/notification_from_hmi.h"
+#include "utils/macro.h"
+
+namespace rc_rpc_plugin {
+namespace commands {
+class RCOnRemoteControlSettingsNotification
+    : public application_manager::commands::NotificationFromHMI {
+ public:
+  bool Init() OVERRIDE;
+  void Run() OVERRIDE;
+  void on_event(const application_manager::event_engine::Event& event) OVERRIDE;
+};
+}  // namespace commands
+}  // namespace rc_rpc_plugin
+
+#endif  // SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_HMI_RC_ON_REMOTE_CONTROL_SETTINGS_NOTIFICATION_H
