e658f7040429f38bcd89f0d1eaf3c517209b113b
Ira Lytvynenko <ILytvynenko@luxoft.com>
2018-02-09 17:14:21 +0200

    RC Command Factory implementation

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/hmi/rc_get_interior_vehicle_data_response.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/hmi/rc_get_interior_vehicle_data_response.cc
@@ -4,6 +4,11 @@
 namespace rc_rpc_plugin {
 namespace commands {
 
+RCGetInteriorVehicleDataResponse::RCGetInteriorVehicleDataResponse(
+    const app_mngr::commands::MessageSharedPtr& message,
+    app_mngr::ApplicationManager& application_manager)
+    :app_mngr::commands::ResponseToHMI(message, application_manager) {}
+
 bool RCGetInteriorVehicleDataResponse::Init() {
   return true;
 }
--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/hmi/rc_set_interior_vehicle_data_response.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/hmi/rc_set_interior_vehicle_data_response.cc
@@ -4,10 +4,16 @@
 namespace rc_rpc_plugin {
 namespace commands {
 
+RCSetInteriorVehicleDataResponse::RCSetInteriorVehicleDataResponse(
+    const app_mngr::commands::MessageSharedPtr& message,
+    app_mngr::ApplicationManager& application_manager)
+    :app_mngr::commands::ResponseToHMI(message, application_manager) {}
+
 bool RCSetInteriorVehicleDataResponse::Init() {
   return true;
 }
 void RCSetInteriorVehicleDataResponse::Run() {}
+
 void RCSetInteriorVehicleDataResponse::on_event(
     const application_manager::event_engine::Event& event) {}
 
