c7365b7fc10e9ad09248d19188f3b9fc33c50304
Alexander Kutsan <akutsan@luxoft.com>
2018-02-09 11:37:49 +0200

    Add header stubs for RC plugin

---
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/get_interior_vehicle_data_response.h
@@ -0,0 +1,19 @@
+#ifndef SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_GET_INTERIOR_VEHICLE_DATA_RESPONSE_H
+#define SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_GET_INTERIOR_VEHICLE_DATA_RESPONSE_H
+
+#include "application_manager/commands/command_response_impl.h"
+#include "utils/macro.h"
+
+namespace rc_rpc_plugin {
+namespace commands {
+class GetInteriorVehicleDataResponse
+    : public application_manager::commands::CommandResponseImpl {
+ public:
+  bool Init() OVERRIDE;
+  void Run() OVERRIDE;
+  void on_event(const application_manager::event_engine::Event& event) OVERRIDE;
+};
+}  // namespace commands
+}  // namespace rc_rpc_plugin
+
+#endif  // SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_GET_INTERIOR_VEHICLE_DATA_RESPONSE_H
