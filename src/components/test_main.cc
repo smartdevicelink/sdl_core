6eae14d31617094034e8d4e61f2c1e3cad987d95
Alexander Kutsan <akutsan@luxoft.com>
2018-02-12 13:01:51 +0200

    Add Resource allocation manager stub
    Implementation of resource allocaiton manager
    
    Resource allocation manager tests
    
    Fix unit test fo RA
    
    Add logger in unit test target to have ability analyze unit tests run without debug
    
    Init logger in tests

--- src/components/test_main.cc
+++ src/components/test_main.cc
@@ -1,12 +1,17 @@
 #include "gmock/gmock.h"
 #include "utils/logger.h"
 #include "utils/custom_string.h"
+#include <iostream>
+
+CREATE_LOGGERPTR_GLOBAL(logger_, "SDLMain")
 int main(int argc, char** argv) {
   namespace custom_str = utils::custom_string;
+  INIT_LOGGER("./log4cxx.properties", true);
   testing::InitGoogleMock(&argc, argv);
   ::testing::DefaultValue<custom_str::CustomString>::Set(
       custom_str::CustomString(""));
   const int result = RUN_ALL_TESTS();
+
   DEINIT_LOGGER();
   return result;
 }
