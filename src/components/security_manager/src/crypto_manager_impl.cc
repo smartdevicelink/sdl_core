530d905b0aa8279a62100661d1cc2627b29fa6ad
Andriy Byzhynar <AByzhynar@luxoft.com>
2018-02-13 17:32:44 +0200

    Add support for DTLS encryption protocol
    Added initial implementation

--- src/components/security_manager/src/crypto_manager_impl.cc
+++ src/components/security_manager/src/crypto_manager_impl.cc
@@ -145,13 +145,16 @@ bool CryptoManagerImpl::Init() {
       LOG4CXX_WARN(logger_, "OpenSSL does not support SSL3 protocol");
       return false;
 #else
+      LOG4CXX_DEBUG(logger_, "SSLv3 is used");
       method = is_server ? SSLv3_server_method() : SSLv3_client_method();
       break;
 #endif
     case TLSv1:
+      LOG4CXX_DEBUG(logger_, "TLSv1 is used");
       method = is_server ? TLSv1_server_method() : TLSv1_client_method();
       break;
     case TLSv1_1:
+      LOG4CXX_DEBUG(logger_, "TLSv1_1 is used");
 #if OPENSSL_VERSION_NUMBER < TLS1_1_MINIMAL_VERSION
       LOG4CXX_WARN(
           logger_,
@@ -162,6 +165,7 @@ bool CryptoManagerImpl::Init() {
 #endif
       break;
     case TLSv1_2:
+      LOG4CXX_DEBUG(logger_, "TLSv1_2 is used");
 #if OPENSSL_VERSION_NUMBER < TLS1_1_MINIMAL_VERSION
       LOG4CXX_WARN(
           logger_,
@@ -170,6 +174,10 @@ bool CryptoManagerImpl::Init() {
 #else
       method = is_server ? TLSv1_2_server_method() : TLSv1_2_client_method();
 #endif
+      break;
+    case DTLSv1:
+      LOG4CXX_DEBUG(logger_, "DTLSv1 is used");
+      method = is_server ? DTLSv1_server_method() : DTLSv1_client_method();
       break;
     default:
       LOG4CXX_ERROR(logger_,
