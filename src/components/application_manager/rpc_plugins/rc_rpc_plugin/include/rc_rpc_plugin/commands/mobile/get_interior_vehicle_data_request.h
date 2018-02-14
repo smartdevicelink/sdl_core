ff092cbf2257dc5cdb0b5b810068645e81765d26
Ira Lytvynenko <ILytvynenko@luxoft.com>
2018-02-14 17:13:16 +0200

    GetInteriorVehicleData implementation and some fixes

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/get_interior_vehicle_data_request.h
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/get_interior_vehicle_data_request.h
@@ -1,22 +1,58 @@
 #ifndef SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_GET_INTERIOR_VEHICLE_DATA_REQUEST_H
 #define SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_GET_INTERIOR_VEHICLE_DATA_REQUEST_H
 
-#include "application_manager/commands/command_request_impl.h"
-#include "utils/macro.h"
+#include "rc_rpc_plugin/commands/rc_command_request.h"
 
 namespace rc_rpc_plugin {
 namespace app_mngr = application_manager;
 
 namespace commands {
-class GetInteriorVehicleDataRequest
-    : public application_manager::commands::CommandRequestImpl {
+class GetInteriorVehicleDataRequest : public RCCommandRequest {
  public:
   GetInteriorVehicleDataRequest(
       const app_mngr::commands::MessageSharedPtr& message,
-      app_mngr::ApplicationManager& application_manager);
-  bool Init() OVERRIDE;
-  void Run() OVERRIDE;
-  void on_event(const application_manager::event_engine::Event& event) OVERRIDE;
+      app_mngr::ApplicationManager& application_manager,
+      rc_rpc_plugin::ResourceAllocationManager& resource_allocation_manager);
+
+  /**
+   * @brief Execute command
+   */
+  void Execute() FINAL;
+
+  /**
+   * @brief Interface method that is called whenever new event received
+   *
+   * @param event The received event
+   */
+  void on_event(const app_mngr::event_engine::Event& event) FINAL;
+
+  /**
+   * @brief Check if app wants to proceed with already setup subscription
+   * @param request_params request parameters to check
+   * @return true if app already subscribed(unsubsribed) for module type but
+   * wants to subscribe(unsubscribe) for the same module again
+   * otherwise - false
+   */
+  bool HasRequestExcessiveSubscription();
+
+  /**
+   * @brief GetInteriorVehicleDataRequest class destructor
+   */
+  virtual ~GetInteriorVehicleDataRequest();
+
+ private:
+  /**
+    * @brief Handle subscription to vehicle data
+    * @param hmi_response json message with response from HMI
+    */
+  void ProccessSubscription(
+          const NsSmartDeviceLink::NsSmartObjects::SmartObject& hmi_response);
+
+  /**
+   * @brief Cuts off subscribe parameter
+   * @param request_params request parameters to handle
+   */
+   void RemoveExcessiveSubscription();
 };
 }  // namespace commands
 }  // namespace rc_rpc_plugin
