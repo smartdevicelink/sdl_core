b183749c9d45e179ea6bad87ba802778dfb86201
Litvinenko Ira <LitvinenkoIra@luxoft.com>
2018-02-15 13:35:10 +0000

    SetInteriorVehicleData implementation and some fixes
    Remove unused code

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/set_interior_vehicle_data_request.h
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/set_interior_vehicle_data_request.h
@@ -2,7 +2,6 @@
 #define SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_COMMANDS_SET_INTERIOR_VEHICLE_DATA_REQUEST_H
 
 #include "rc_rpc_plugin/commands/rc_command_request.h"
-#include "utils/macro.h"
 
 namespace rc_rpc_plugin {
 namespace app_mngr = application_manager;
@@ -17,9 +16,65 @@ class SetInteriorVehicleDataRequest : public RCCommandRequest {
       app_mngr::rpc_service::RPCService& rpc_service,
       app_mngr::HMICapabilities& hmi_capabilities,
       policy::PolicyHandlerInterface& policy_handle);
-  bool Init() OVERRIDE;
-  void Run() OVERRIDE;
-  void on_event(const application_manager::event_engine::Event& event) OVERRIDE;
+
+  /**
+   * @brief Execute command
+   */
+  void Execute() FINAL;
+
+  /**
+   * @brief AcquireResource proxy AcquireResource to Resource allocation manager
+   * @param message message of requires contatin module types
+   * @return result of acauiring resources
+   */
+  AcquireResult::eType AcquireResource(
+      const app_mngr::commands::MessageSharedPtr& message) FINAL;
+
+  /**
+   * @brief IsResourceFree check resource state
+   * @param module_type Resource name
+   * @return True if free, otherwise - false
+   */
+  bool IsResourceFree(const std::string& module_type) const FINAL;
+
+  /**
+   * @brief SetResourceState changes state of resource
+   * @param state State to set for resource
+   */
+  void SetResourceState(const std::string& module_type,
+                        const ResourceState::eType state) FINAL;
+
+  /**
+   * @brief Interface method that is called whenever new event received
+   *
+   * @param event The received event
+   */
+  void on_event(const app_mngr::event_engine::Event& event) FINAL;
+
+  /**
+   * @brief Method that check if READ_ONLY parameters present
+   * @param request_params params from received message
+   * @return true if present , false - otherwise
+   */
+  bool AreReadOnlyParamsPresent(const smart_objects::SmartObject& module_data);
+
+  /**
+   * @brief Method that check if all request parameters are READ_ONLY
+   * @param request_params params from received message
+   * @return true if all are read only , false - otherwise
+   */
+  bool AreAllParamsReadOnly(const smart_objects::SmartObject& module_data);
+
+  /**
+   * @brief Method that cuts-off READ_ONLY parameters
+   * @param request_params params to handle
+   */
+  void CutOffReadOnlyParams(smart_objects::SmartObject& module_data);
+
+  /**
+   * @brief SetInteriorVehicleDataRequest class destructor
+   */
+virtual ~SetInteriorVehicleDataRequest();
 };
 }  // namespace commands
 }  // namespace rc_rpc_plugin
