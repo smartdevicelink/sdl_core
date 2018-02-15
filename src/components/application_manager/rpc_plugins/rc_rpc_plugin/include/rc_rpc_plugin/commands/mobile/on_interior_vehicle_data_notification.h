2edafd74954c31e560aa88f5fdd37623e9679412
Ira Lytvynenko <ILytvynenko@luxoft.com>
2018-02-15 15:50:57 +0000

    OnRemoteControlSettings and OnInteriorVehicleData notifications impl

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/on_interior_vehicle_data_notification.h
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/on_interior_vehicle_data_notification.h
@@ -13,15 +13,15 @@ class OnInteriorVehicleDataNotification
     : public application_manager::commands::CommandNotificationImpl {
  public:
   OnInteriorVehicleDataNotification(
-      ResourceAllocationManager& resource_allocation_manager,
       const app_mngr::commands::MessageSharedPtr& message,
       app_mngr::ApplicationManager& application_manager,
       app_mngr::rpc_service::RPCService& rpc_service,
       app_mngr::HMICapabilities& hmi_capabilities,
-      policy::PolicyHandlerInterface& policy_handle);
-  bool Init() OVERRIDE;
+      policy::PolicyHandlerInterface& policy_handler);
+
   void Run() OVERRIDE;
-  void on_event(const application_manager::event_engine::Event& event);
+
+  virtual ~OnInteriorVehicleDataNotification();
 };
 }  // namespace commands
 }  // namespace rc_rpc_plugin
