34699239c2fadfba68464ef90c6ece0572e8bbe3
BSolonenko <BSolonenko@luxoft.com>
2018-02-16 16:43:15 +0200

    Refactoring rc rpc plugin's factory

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/set_interior_vehicle_data_request.h
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/commands/mobile/set_interior_vehicle_data_request.h
@@ -10,12 +10,12 @@ namespace commands {
 class SetInteriorVehicleDataRequest : public RCCommandRequest {
  public:
   SetInteriorVehicleDataRequest(
-      rc_rpc_plugin::ResourceAllocationManager& resource_allocation_manager,
       const app_mngr::commands::MessageSharedPtr& message,
       app_mngr::ApplicationManager& application_manager,
       app_mngr::rpc_service::RPCService& rpc_service,
       app_mngr::HMICapabilities& hmi_capabilities,
-      policy::PolicyHandlerInterface& policy_handle);
+      policy::PolicyHandlerInterface& policy_handle,
+      rc_rpc_plugin::ResourceAllocationManager& resource_allocation_manager);
 
   /**
    * @brief Execute command
@@ -74,7 +74,7 @@ class SetInteriorVehicleDataRequest : public RCCommandRequest {
   /**
    * @brief SetInteriorVehicleDataRequest class destructor
    */
-virtual ~SetInteriorVehicleDataRequest();
+  ~SetInteriorVehicleDataRequest();
 };
 }  // namespace commands
 }  // namespace rc_rpc_plugin
