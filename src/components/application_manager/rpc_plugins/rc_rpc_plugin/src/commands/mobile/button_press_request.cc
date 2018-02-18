9f7c11c4d9e36e5995e39279ac6524bf2cec7471
Ira Lytvynenko <ILytvynenko@luxoft.com>
2018-02-18 18:32:17 +0200

    UTS for ButtonPress
    Fix UT of the rc commands
    
    -Get interior vehicle data request test
    
    Fix UTs
    
    Fixed application_manager_mock_hmi_test (SEGFAULT)
    
    Added mock_rpc_plugin.h & mock_rpc_plugin_manager.h
    Changed application_manager_impl.h, application_manager.h &
        mock_application_manager.h
    Fixed application_manager_impl.cc, rpc_plugin_manager.h
        application_manager_impl_mock_hmi_test.cc

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/mobile/button_press_request.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/mobile/button_press_request.cc
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2018, Ford Motor Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ford Motor Company nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "rc_rpc_plugin/commands/mobile/button_press_request.h"
 #include "rc_rpc_plugin/rc_module_constants.h"
 #include "smart_objects/enum_schema_item.h"
@@ -166,7 +198,7 @@ void ButtonPressRequest::Execute() {
   }
 
   const smart_objects::SmartObject* rc_capabilities =
-      application_manager_.hmi_capabilities().rc_capability();
+      hmi_capabilities_.rc_capability();
   const bool button_name_matches_module_type =
       CheckButtonName(module_type, button_name, rc_capabilities);
   const bool button_id_exist_in_caps =
