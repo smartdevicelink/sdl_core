c88e323622100786c2f7ab20b06b375529f1e1ff
Ira Lytvynenko <ILytvynenko@luxoft.com>
2018-03-04 15:18:07 +0200

    Fix OnRCStatus sending after app registration
    Fix OnRCStatusNotification

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/rc_rpc_plugin.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/rc_rpc_plugin.cc
@@ -47,6 +47,7 @@ void RCRPCPlugin::OnApplicationEvent(
   switch (event) {
     case plugins::kApplicationRegistered: {
       application->AddExtension(new RCAppExtension(kRCPluginID));
+      resource_allocation_manager_->SendOnRCStatusNotification();
       break;
     }
     default:
