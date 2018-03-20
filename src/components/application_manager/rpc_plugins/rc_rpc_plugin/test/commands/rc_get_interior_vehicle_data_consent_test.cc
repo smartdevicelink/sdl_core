b0739695591558fe44442cbae360cc103b4025b0
LitvinenkoIra <ilytvynenko654@gmail.com>
2018-03-20 23:56:29 +0200

    Fix UTs

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/test/commands/rc_get_interior_vehicle_data_consent_test.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/test/commands/rc_get_interior_vehicle_data_consent_test.cc
@@ -130,6 +130,8 @@ class RCGetInteriorVehicleDataConsentTest
             CheckHMIType(
                 _, mobile_apis::AppHMIType::eType::REMOTE_CONTROL, nullptr))
         .WillByDefault(Return(true));
+    ON_CALL(mock_allocation_manager_, is_rc_enabled())
+        .WillByDefault(Return(true));
   }
 
   template <class Command>
