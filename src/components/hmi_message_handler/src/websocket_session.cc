a769e4eb2d5844b70d37bbee9aa9c4c3abf2e507
JackLivio <jack@livio.io>
2018-03-26 15:09:21 -0400

    Add boost license

--- src/components/hmi_message_handler/src/websocket_session.cc
+++ src/components/hmi_message_handler/src/websocket_session.cc
@@ -1,3 +1,8 @@
+// Copyright Livio, Inc. 2018.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include "hmi_message_handler/websocket_session.h"
 #include "hmi_message_handler/mb_controller.h"
 #include <unistd.h>
