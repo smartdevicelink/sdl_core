8380529cac9cb1f75d3ee0cdbbac38c21cf46f39
Ira Lytvynenko (GitHub) <ILytvynenko@luxoft.com>
2018-03-26 15:05:16 +0300

    Fix cutting off READ ONLY param channelName

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/rc_module_constants.h
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/rc_module_constants.h
@@ -178,6 +178,8 @@ const char kSRGBColor[] = "sRGBColor";
 // AudioControlData
 const char kSource[] = "source";
 const char kKeepContext[] = "keepContext";
+const char kEqualizerSettings[] = "equalizerSettings";
+const char kChannelName[] = "channelName";
 
 // ModuleData struct
 const char kRadioControlData[] = "radioControlData";
