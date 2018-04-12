1e1c90e6871f4ea1e795cb25b944b9ca2274fda9
Andriy Byzhynar <AByzhynar@luxoft.com>
2018-04-12 20:10:35 +0300

    Add correct certificate expiration handling

--- src/components/protocol_handler/src/protocol_handler_impl.cc
+++ src/components/protocol_handler/src/protocol_handler_impl.cc
@@ -1425,17 +1425,11 @@ RESULT_CODE ProtocolHandlerImpl::HandleControlMessageStartSession(
   }
 
 #ifdef ENABLE_SECURITY
+  const uint8_t protocol_version = packet->protocol_version();
   const bool protection =
-<<<<<<< HEAD
-      // Protocol version 1 is not support protection
-      (packet->protocol_version() > PROTOCOL_VERSION_1)
-          ? packet->protection_flag()
-          : false;
-=======
       // Protocol version 1 does not support protection
       (protocol_version > PROTOCOL_VERSION_1) ? packet->protection_flag()
                                               : false;
->>>>>>> Add handling of wrong force protection settings in ini file
 #else
   const bool protection = false;
 #endif  // ENABLE_SECURITY
