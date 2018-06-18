0a1e969a12ddf8aaebc21a6ef98f732ef3cd7567
Alexander <akutsan@luxoft.com>
2018-06-18 13:19:52 +0300

    fixup! Add resumption processing in VehicleInfoAppExtension

--- src/components/application_manager/rpc_plugins/vehicle_info_plugin/include/vehicle_info_plugin/vehicle_info_plugin.h
+++ src/components/application_manager/rpc_plugins/vehicle_info_plugin/include/vehicle_info_plugin/vehicle_info_plugin.h
@@ -65,8 +65,6 @@ class VehicleInfoPlugin : public plugins::RPCPlugin {
   void ProcessResumptionSubscription(app_mngr::Application& app,
                                      VehicleInfoAppExtension& ext);
 
-  static const VehicleData vehicle_data_;
-
  private:
   void DeleteSubscriptions(app_mngr::ApplicationSharedPtr app);
 
