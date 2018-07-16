891060ef895d608d0c7aeb677e0a1509570b520a
Ira Lytvynenko (GitHub) <ILytvynenko@luxoft.com>
2018-07-16 16:28:57 +0300

    Add SEAT module to all supported modules

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/resource_allocation_manager_impl.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/resource_allocation_manager_impl.cc
@@ -337,6 +337,7 @@ ResourceAllocationManagerImpl::all_supported_modules() {
   std::vector<std::string> result;
   result.push_back(enums_value::kClimate);
   result.push_back(enums_value::kRadio);
+  result.push_back(enums_value::kSeat);
   return result;
 }
 
