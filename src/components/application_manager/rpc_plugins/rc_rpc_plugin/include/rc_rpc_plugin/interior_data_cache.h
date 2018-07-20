c3466b57c8c783f0919101f9d350870a14859b02
Alexander <akutsan@luxoft.com>
2018-07-20 13:06:09 +0300

    Add Interior data cache and interior data manager
    Add helpers functions for RC

---
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/interior_data_cache.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2018, Ford Motor Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ford Motor Company nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_RC_RPC_PLUGIN_INTERIOR_DATA_CACHE_H_
+#define SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_RC_RPC_PLUGIN_INTERIOR_DATA_CACHE_H_
+#include <string>
+#include "smart_objects/smart_object.h"
+
+namespace rc_rpc_plugin {
+
+/**
+ * @brief The InteriorDataCache interface for caching data class
+ * Provide ability to cache module data by module type name and clear cache
+ */
+class InteriorDataCache {
+ public:
+  /**
+   * @brief Add module data to cache
+   * @param module_type module type name
+   * @param module_data data to be cached
+   */
+  virtual void Add(const std::string& module_type,
+                   const smart_objects::SmartObject& module_data) = 0;
+
+  /**
+   * @brief Retrieve Get cached data
+   * @param module_type data type to get from cache
+   * @return smart object with cached data, or nulll smart object
+   */
+  virtual smart_objects::SmartObject Retrieve(
+      const std::string& module_type) const = 0;
+
+  /**
+   * @brief Contains check if data exists in cache
+   * @param module_type module type name to check in cache
+   * @return true if cached, false otherwize
+   */
+  virtual bool Contains(const std::string& module_type) const = 0;
+
+  /**
+   * @brief Remove cached data
+   * @param module_type data type to remove from cache
+   */
+  virtual void Remove(const std::string& module_type) = 0;
+
+  /**
+   * @brief Clear clear all cached data
+   */
+  virtual void Clear() = 0;
+};
+}  // rc_rpc_plugin
+
+#endif  // SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_RC_RPC_PLUGIN_INCLUDE_RC_RPC_PLUGIN_INTERIOR_DATA_CACHE_H_
---
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/rc_helpers.cc
@@ -0,0 +1,88 @@
+#include "rc_rpc_plugin/rc_helpers.h"
+#include "rc_rpc_plugin/rc_module_constants.h"
+#include "rc_rpc_plugin/rc_rpc_plugin.h"
+#include "application_manager/smart_object_keys.h"
+#include "application_manager/commands/command_impl.h"
+#include "application_manager/message.h"
+
+namespace rc_rpc_plugin {
+CREATE_LOGGERPTR_GLOBAL(logger_, "RemoteControlModule");
+
+const std::function<std::string(const std::string& module_type)>
+RCHelpers::GetModuleTypeToDataMapping() {
+  auto mapping_lambda = [](const std::string& module_type) -> std::string {
+    static std::map<std::string, std::string> mapping = {
+        {enums_value::kRadio, message_params::kRadioControlData},
+        {enums_value::kClimate, message_params::kClimateControlData},
+        {enums_value::kSeat, message_params::kSeatControlData}};
+    auto it = mapping.find(module_type);
+    if (mapping.end() == it) {
+      return std::string();
+    }
+    return it->second;
+  };
+
+  return mapping_lambda;
+}
+
+RCAppExtensionPtr RCHelpers::GetRCExtension(
+    application_manager::Application& app) {
+  auto extension_interface = app.QueryInterface(RCRPCPlugin::kRCPluginID);
+  auto extension =
+      std::static_pointer_cast<RCAppExtension>(extension_interface);
+  return extension;
+}
+
+smart_objects::SmartObjectSPtr RCHelpers::CreateUnsubscribeRequestToHMI(
+    const std::string& module_type, const uint32_t correlation_id) {
+  using namespace smart_objects;
+  namespace commands = application_manager::commands;
+  namespace am_strings = application_manager::strings;
+
+  SmartObjectSPtr message = std::make_shared<SmartObject>(SmartType_Map);
+  SmartObject& params = (*message)[am_strings::params];
+  SmartObject& msg_params = (*message)[am_strings::msg_params];
+
+  params[am_strings::message_type] =
+      static_cast<int>(application_manager::kRequest);
+  params[am_strings::protocol_version] =
+      commands::CommandImpl::protocol_version_;
+  params[am_strings::protocol_type] = commands::CommandImpl::hmi_protocol_type_;
+  params[am_strings::correlation_id] = correlation_id;
+  params[am_strings::function_id] =
+      hmi_apis::FunctionID::RC_GetInteriorVehicleData;
+  msg_params[message_params::kSubscribe] = false;
+  msg_params[message_params::kModuleType] = module_type;
+  return message;
+}
+
+std::vector<application_manager::ApplicationSharedPtr>
+RCHelpers::AppsSubscribedToModuleType(
+    application_manager::ApplicationManager& app_mngr,
+    const std::string& module_type) {
+  std::vector<application_manager::ApplicationSharedPtr> result;
+  auto rc_apps = RCRPCPlugin::GetRCApplications(app_mngr);
+  for (auto& app : rc_apps) {
+    auto rc_ext = RCHelpers::GetRCExtension(*app);
+    DCHECK_OR_RETURN(rc_ext, result);
+    if (rc_ext->IsSubscibedToInteriorVehicleData(module_type)) {
+      result.push_back(app);
+    }
+  }
+  return result;
+}
+
+RCHelpers::AppsModules RCHelpers::GetApplicaitonsAllowedModules(
+    app_mngr::ApplicationManager& app_mngr) {
+  auto apps_list = RCRPCPlugin::GetRCApplications(app_mngr);
+  RCHelpers::AppsModules result;
+  for (auto& app_ptr : apps_list) {
+    std::vector<std::string> allowed_modules;
+    app_mngr.GetPolicyHandler().GetModuleTypes(app_ptr->policy_app_id(),
+                                               &allowed_modules);
+    std::sort(allowed_modules.begin(), allowed_modules.end());
+    result[app_ptr] = allowed_modules;
+  }
+  return result;
+}
+}
