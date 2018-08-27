51123ed929def963ef5640e82db008adc570c286
ZhdanovP <pzhdanov@luxoft.com>
2018-08-27 18:44:20 +0300

    Fix sdl core dump when plugins are not loaded

--- src/appMain/life_cycle_impl.cc
+++ src/appMain/life_cycle_impl.cc
@@ -163,7 +163,7 @@ bool LifeCycleImpl::StartComponents() {
   // It's important to initialise TM after setting up listener chain
   // [TM -> CH -> AM], otherwise some events from TM could arrive at nowhere
   app_manager_->set_protocol_handler(protocol_handler_);
-  if (!transport_manager_->Init(*last_state_)) {
+  if (transport_manager::E_SUCCESS != transport_manager_->Init(*last_state_)) {
     LOG4CXX_ERROR(logger_, "Transport manager init failed.");
     return false;
   }
