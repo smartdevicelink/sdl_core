e85b2afb5c46b8cbb2dd608ed16cfeec200fc6c8
jacobkeeler <jacob.keeler@livioradio.com>
2018-08-29 17:16:57 -0400

    Fix Start Stream retry count

--- src/components/application_manager/rpc_plugins/sdl_rpc_plugin/src/commands/hmi/navi_start_stream_request.cc
+++ src/components/application_manager/rpc_plugins/sdl_rpc_plugin/src/commands/hmi/navi_start_stream_request.cc
@@ -172,11 +172,11 @@ void NaviStartStreamRequest::RetryStartSession() {
   }
 
   uint32_t curr_retry_number = app->video_stream_retry_number();
-  LOG4CXX_DEBUG(
-      logger_, "Retry number " << curr_retry_number << " of " << retry_number_);
 
-  if (curr_retry_number < retry_number_) {
-    LOG4CXX_DEBUG(logger_, "Send NaviStartStream retry");
+  if (curr_retry_number <= retry_number_) {
+    LOG4CXX_DEBUG(logger_,
+                  "Retry number " << curr_retry_number << " of "
+                                  << retry_number_);
     MessageHelper::SendNaviStartStream(app->app_id(), application_manager_);
     app->set_video_stream_retry_number(++curr_retry_number);
   } else {
