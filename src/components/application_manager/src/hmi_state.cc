17e933840868e098c88f8097904d9a0c6b218379
Mykola Korniichuk <mkorniichuk@luxoft.com>
2018-09-07 16:22:43 +0300

    SDL sends wrong OnHMIStatus for voice app in case of a phone call.
    Once SDL received OnEventChanged(Phone_Call, true) from HMI, it should send
    OnHMIStatusUpdate with new HMILevel. Expected HMILevel for voice
    communication application is "LIMITED", but it is "FULL".
    
    PhoneCallHmiState should take into account voice communication app
    as an additional check.

--- src/components/application_manager/src/hmi_state.cc
+++ src/components/application_manager/src/hmi_state.cc
@@ -171,7 +171,7 @@ mobile_apis::HMILevel::eType PhoneCallHmiState::hmi_level() const {
   if (is_navi_app() || is_mobile_projection_app()) {
     return HMILevel::HMI_LIMITED;
   }
-  if (!is_media_app()) {
+  if (!is_media_app() && !is_voice_communication_app()) {
     return parent()->hmi_level();
   }
   return HMILevel::HMI_BACKGROUND;
