cf49c819de5180afcc870cec67160678a991997e
Alexander <akutsan@luxoft.com>
2018-09-09 21:51:54 +0300

    Analyse response on vehicle data subscriptions and revert if it is partiay failed

--- src/components/application_manager/rpc_plugins/vehicle_info_plugin/src/vehicle_info_plugin.cc
+++ src/components/application_manager/rpc_plugins/vehicle_info_plugin/src/vehicle_info_plugin.cc
@@ -88,8 +88,17 @@ void VehicleInfoPlugin::ProcessResumptionSubscription(
     VehicleInfoAppExtension& ext,
     resumption::Subscriber subscriber) {
   LOG4CXX_AUTO_TRACE(logger_);
+
+  std::set<std::string> subscriptions;
+  for (auto& ivi : application_manager::MessageHelper::vehicle_data()) {
+    const auto it = ext.Subscriptions().find(ivi.second);
+    if (ext.Subscriptions().end() != it) {
+      subscriptions.insert(ivi.first);
+    }
+  }
+
   smart_objects::SmartObjectSPtr request =
-      CreateSubscriptionRequest(app.app_id(), ext, SUBSCRIBE);
+      CreateSubscriptionRequest(app.app_id(), subscriptions, SUBSCRIBE);
 
   resumption::ResumptionRequest resumption_request;
   resumption_request.request_ids.correlation_id =
@@ -103,28 +112,29 @@ void VehicleInfoPlugin::ProcessResumptionSubscription(
   application_manager_->GetRPCService().ManageHMICommand(request);
 }
 
-void VehicleInfoPlugin::RevertResumption(application_manager::Application& app,
-                                         VehicleInfoAppExtension& ext) {
+void VehicleInfoPlugin::RevertResumption(
+    application_manager::Application& app,
+    std::set<std::string> list_of_subscriptions) {
   LOG4CXX_AUTO_TRACE(logger_);
-  smart_objects::SmartObjectSPtr request =
-      CreateSubscriptionRequest(app.app_id(), ext, UNSUBSCRIBE);
+
+  if (list_of_subscriptions.empty()) {
+    LOG4CXX_DEBUG(logger_, "No data to unsubscribe");
+    return;
+  }
+  smart_objects::SmartObjectSPtr request = CreateSubscriptionRequest(
+      app.app_id(), list_of_subscriptions, UNSUBSCRIBE);
   application_manager_->GetRPCService().ManageHMICommand(request);
 }
 
 smart_objects::SmartObjectSPtr VehicleInfoPlugin::CreateSubscriptionRequest(
     const uint32_t app_id,
-    VehicleInfoAppExtension& ext,
+    std::set<std::string> list_of_subscriptions,
     const SubscribeStatus subscribe_status) {
   LOG4CXX_AUTO_TRACE(logger_);
   smart_objects::SmartObject msg_params =
       smart_objects::SmartObject(smart_objects::SmartType_Map);
-  const auto& subscriptions = ext.Subscriptions();
-  for (auto& ivi_data : application_manager::MessageHelper::vehicle_data()) {
-    mobile_apis::VehicleDataType::eType type_id = ivi_data.second;
-    if (subscriptions.end() != subscriptions.find(type_id)) {
-      std::string key_name = ivi_data.first;
-      msg_params[key_name] = true;
-    }
+  for (auto& ivi_data : list_of_subscriptions) {
+    msg_params[ivi_data] = true;
   }
   const auto function_id =
       (subscribe_status == SUBSCRIBE)
