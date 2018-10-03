8d2f777498d744e890a2b5148c69969940ccd655
JackLivio <jack@livio.io>
2018-10-03 10:12:53 -0400

    Coverity Fixes for 5.0.0 (#2638)
    * Fix 188268 Non-virtual destructor
    
    * Fix 188267 Non-virtual destructor
    
    * Fix 188246 Non-virtual destructor
    
    * Fix 188236 Buffer not null terminated
    
    * Fix 188295 Uncaught exception
    
    * Fix 188234 Uncaught exception
    
    * Reverted fix 188234
    
    * Fix Size Calculation and Style
    
    * Fix error
    
    * Fix errors
    
    * Fix typo

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/interior_data_cache.h
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/interior_data_cache.h
@@ -43,6 +43,11 @@ namespace rc_rpc_plugin {
 class InteriorDataCache {
  public:
   /**
+   * @brief Class destructor
+   */
+  virtual ~InteriorDataCache() {}
+
+  /**
    * @brief Add module data to cache
    * @param module_type module type name
    * @param module_data data to be cached
