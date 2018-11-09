e20bcd89b10b911b1183046a30b8a15fdce5942e
ValeriiMalkov <vmalkov@luxoft.com>
2018-11-09 15:22:06 +0200

    Add start_external_proprietary.sh script when SDL builds with PROPRIETARY flow

---
+++ src/appMain/start_proprietary.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+pip list | grep -F tornado > /dev/null
+if [ $? -eq 1 ]
+  then
+    echo "Installing tornado python package"
+    sudo pip install tornado
+fi
+echo "Starting Policy Manager"
+python sample_policy_manager.py --pack_port 8088 --unpack_port 8089 --encryption &
+POLICY_MANAGER=$!
+
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "Stopping SmartDeviceLinkCore"
+        kill -INT $POLICY_MANAGER
+        kill -9 $POLICY_MANAGER
+}
+
+echo "Starting SmartDeviceLinkCore"
+LD_LIBRARY_PATH=. ./smartDeviceLinkCore
