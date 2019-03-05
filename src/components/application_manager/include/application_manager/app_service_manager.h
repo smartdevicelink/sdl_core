8cf79941ec77aea0233e72dd93fac49bca484fcc
Shobhit Adlakha <adlakhashobhit@gmail.com>
2019-03-05 18:06:21 -0500

    Addressed review comments

--- src/components/application_manager/include/application_manager/app_service_manager.h
+++ src/components/application_manager/include/application_manager/app_service_manager.h
@@ -108,8 +108,11 @@ class AppServiceManager {
   std::vector<smart_objects::SmartObject> GetAllServices();
 
   /**
-   * @brief TODO
-   * @param service_id
+   * @brief Get AppService information based on service id
+   * @param service_id app service id
+   * @param app_service_info reference to AppService object to be overwritten
+   * with AppService information
+   * @return true if app service with id=service_id exists
    */
   bool GetAppServiceInfo(std::string service_id, AppService& app_service_info);
 
--- src/components/application_manager/rpc_plugins/sdl_rpc_plugin/include/sdl_rpc_plugin/commands/hmi/bc_get_file_from_hmi_response.h
+++ src/components/application_manager/rpc_plugins/sdl_rpc_plugin/include/sdl_rpc_plugin/commands/hmi/bc_get_file_from_hmi_response.h
@@ -54,7 +54,7 @@ class BCGetFileFromHMIResponse : public app_mngr::commands::ResponseFromHMI {
                            app_mngr::ApplicationManager& application_manager,
                            app_mngr::rpc_service::RPCService& rpc_service,
                            app_mngr::HMICapabilities& hmi_capabilities,
-                           policy::PolicyHandlerInterface& policy_handle);
+                           policy::PolicyHandlerInterface& policy_handler);
 
   /**
    * @brief BCGetFileFromHMIResponse class destructor
