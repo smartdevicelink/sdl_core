1e5581872dc14ebcdde71cbcb9cd7eee9b98338a
jacobkeeler <jacob.keeler@livioradio.com>
2019-03-13 10:35:33 -0400

    Add unit tests for cloud policy handler additions

--- src/components/include/test/application_manager/policies/mock_policy_handler_observer.h
+++ src/components/include/test/application_manager/policies/mock_policy_handler_observer.h
@@ -49,6 +49,9 @@ class MockPolicyHandlerObserver : public ::policy::PolicyHandlerObserver {
   MOCK_METHOD1(OnUpdateHMIAppType,
                void(std::map<std::string, std::vector<std::string> >));
   MOCK_METHOD1(OnCertificateUpdated, bool(const std::string&));
+  MOCK_METHOD2(OnAuthTokenUpdated,
+               void(const std::string& policy_app_id,
+                    const std::string& auth_token));
   MOCK_METHOD1(OnPTUFinished, void(const bool ptu_result));
 };
 }  //  namespace application_manager_test
