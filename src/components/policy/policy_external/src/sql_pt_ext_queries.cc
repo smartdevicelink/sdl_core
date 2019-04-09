e06c810fda3514fd345075455ea91a2341811e6a
JackLivio <jack@livio.io>
2019-04-09 10:55:56 -0400

    Add missing comma in pt query strings

--- src/components/policy/policy_external/src/sql_pt_ext_queries.cc
+++ src/components/policy/policy_external/src/sql_pt_ext_queries.cc
@@ -237,7 +237,7 @@ const std::string kSelectPreconsentedGroupsId =
 const std::string kSelectAppPolicies =
     "SELECT `id`, `priority_value`, `default_hmi`, `keep_context`, "
     " `steal_focus`, `memory_kb`, `heart_beat_timeout_ms`, `certificate`, "
-    " `hybrid_app_preference_value`, `endpoint`, `enabled`, `auth_token` "
+    " `hybrid_app_preference_value`, `endpoint`, `enabled`, `auth_token`, "
     " `cloud_transport_type`, `icon_url`, `allow_unknown_rpc_passthrough` "
     "FROM `application`";
 
