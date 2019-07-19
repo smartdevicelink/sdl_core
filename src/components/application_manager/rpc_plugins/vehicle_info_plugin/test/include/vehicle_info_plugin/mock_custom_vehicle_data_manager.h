e3c74609787dc891bf588aef0db6d3583b17d259
Alexander Kutsan <akutsan@luxoft.com>
2019-07-19 17:32:16 +0300

    CustomVehicleDataManager implementation

---
+++ src/components/application_manager/rpc_plugins/vehicle_info_plugin/test/include/vehicle_info_plugin/mock_custom_vehicle_data_manager.h
@@ -0,0 +1,23 @@
+#ifndef SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_VEHICLE_INFO_PLUGIN_TEST_INCLUDE_VEHICLE_INFO_PLUGIN_MOCK_CUSTOM_VEHICLE_DATA_MANAGER_H_
+#define SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_VEHICLE_INFO_PLUGIN_TEST_INCLUDE_VEHICLE_INFO_PLUGIN_MOCK_CUSTOM_VEHICLE_DATA_MANAGER_H_
+
+#include "gmock/gmock.h"
+#include "vehicle_info_plugin/custom_vehicle_data_manager.h"
+
+namespace vehicle_info_plugin {
+
+class MockCustomVehicleDataManager : public CustomVehicleDataManager {
+ public:
+  MOCK_METHOD1(ValidateVehicleDataItems,
+               bool(const smart_objects::SmartObject& msg_params));
+  MOCK_METHOD1(
+      CreateHMIMessageParams,
+      smart_objects::SmartObject(const std::set<std::string>& msg_params));
+  MOCK_METHOD1(CreateMobileMessageParams,
+               void(smart_objects::SmartObject& msg_params));
+  MOCK_METHOD1(IsVehicleDataName, bool(const std::string& name));
+  MOCK_METHOD1(IsVehicleDataKey, bool(const std::string& key));
+};
+
+}  // namespace vehicle_info_plugin
+#endif  // SRC_COMPONENTS_APPLICATION_MANAGER_RPC_PLUGINS_VEHICLE_INFO_PLUGIN_TEST_INCLUDE_VEHICLE_INFO_PLUGIN_MOCK_CUSTOM_VEHICLE_DATA_MANAGER_H_
