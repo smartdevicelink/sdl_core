01b83de8916912bf3444d4a7e436553eea958ba8
Mykola Korniichuk <mkorniichuk@luxoft.com>
2019-08-06 12:50:56 +0300

    Fix messages conversion to json

--- src/components/policy/policy_external/src/policy_table/types.cc
+++ src/components/policy/policy_external/src/policy_table/types.cc
@@ -1313,8 +1313,7 @@ ConsumerFriendlyMessages::ConsumerFriendlyMessages(const Json::Value* value__)
 Json::Value ConsumerFriendlyMessages::ToJsonValue() const {
   Json::Value result__(Json::objectValue);
   impl::WriteJsonField("version", version, &result__);
-  // According to requirements, it is not necessary to provide this to PTS
-  // impl::WriteJsonField("messages", messages, &result__);
+  impl::WriteJsonField("messages", messages, &result__);
   return result__;
 }
 
