7bf50b2b65723278b0224883d951b7c39bac56c2
Igor Gapchuk <igapchuck@luxoft.com>
2019-11-19 10:00:16 +0200

    Add the new ApplicationEvent.
    Add the new ApplicationEvent (kRCStatusChanged) for informing RcRPCPlugin
    that OnRCStatus notification should be sent.
    Split adding RcExtension to application and sending OnRCStatus notification
    to the separate actions.

--- src/components/application_manager/include/application_manager/plugin_manager/rpc_plugin.h
+++ src/components/application_manager/include/application_manager/plugin_manager/rpc_plugin.h
@@ -65,7 +65,8 @@ enum ApplicationEvent {
   kApplicationRegistered,
   kApplicationUnregistered,
   kDeleteApplicationData,
-  kGlobalPropertiesUpdated
+  kGlobalPropertiesUpdated,
+  kRCStatusChanged
 };
 
 class RPCPlugin {
