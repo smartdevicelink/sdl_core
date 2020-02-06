493a706079e29071db3452a0237a1cabdccdf230
sniukalov <sniukaov@luxoft.com>
2020-02-06 20:03:01 +0200

    Remove rudement for UpdateNeededStatus

--- src/components/policy/policy_external/src/status.cc
+++ src/components/policy/policy_external/src/status.cc
@@ -73,9 +73,6 @@ void policy::UpdateNeededStatus::ProcessEvent(
     case kOnResetPolicyTableNoUpdate:
       manager->SetNextStatus(std::make_shared<UpToDateStatus>());
       break;
-    case kOnNewAppRegistered:
-      manager->SetNextStatus(std::make_shared<UpdateNeededStatus>());
-      break;
     case kPendingUpdate:
       manager->SetNextStatus(std::make_shared<UpdatePendingStatus>());
       break;
