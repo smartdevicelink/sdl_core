11aa607b20b23796669665cbe2ac3c5e1d37e43d
YarikMamykin (GitHub) <ymamykin@luxoft.com>
2020-03-16 13:26:59 +0200

    Fix usage non-valid policy_manager pointer in case of EnablePolicy=false

--- src/components/application_manager/src/policies/policy_handler.cc
+++ src/components/application_manager/src/policies/policy_handler.cc
@@ -470,7 +470,7 @@ void PolicyHandler::PushAppIdToPTUQueue(const uint32_t app_id) {
   LOG4CXX_AUTO_TRACE(logger_);
   sync_primitives::AutoLock lock(app_id_queue_lock_);
   const auto result = applications_ptu_queue_.insert(app_id);
-  if (result.second) {
+  if (result.second && PolicyEnabled()) {
     policy_manager_->UpdatePTUReadyAppsCount(applications_ptu_queue_.size());
   }
 }
