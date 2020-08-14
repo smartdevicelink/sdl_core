ae12891efcf5b7fd74cdc79c33878693be335018
Yana Chernysheva <ychernysheva@luxoft.com>
2020-08-14 13:27:40 +0300

    Add changes related to AppExtension and RCAppExtension

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/rc_app_extension.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/rc_app_extension.cc
@@ -81,11 +81,14 @@ bool RCAppExtension::IsSubscribedToInteriorVehicleData(
 }
 
 void RCAppExtension::SaveResumptionData(
-    ns_smart_device_link::ns_smart_objects::SmartObject& resumption_data) {}
+    smart_objects::SmartObject& resumption_data) {}
 
 void RCAppExtension::ProcessResumption(
-    const ns_smart_device_link::ns_smart_objects::SmartObject&
-        resumption_data) {}
+    const smart_objects::SmartObject& saved_app,
+    resumption::Subscriber subscriber) {}
+
+void RCAppExtension::RevertResumption(
+    const smart_objects::SmartObject& subscriptions) {}
 
 std::set<ModuleUid> RCAppExtension::InteriorVehicleDataSubscriptions() const {
   return subscribed_interior_vehicle_data_;
