78a5677fa6189fb2a528976514eabefe9124f9a1
Yana Chernysheva <ychernysheva@luxoft.com>
2020-10-02 13:04:39 +0300

    Delete module_types_str_mapping

--- src/components/application_manager/src/resumption/resumption_data_processor_impl.cc
+++ src/components/application_manager/src/resumption/resumption_data_processor_impl.cc
@@ -49,14 +49,6 @@ namespace commands = app_mngr::commands;
 namespace message_params = rc_rpc_plugin::message_params;
 
 SDL_CREATE_LOG_VARIABLE("Resumption")
-std::map<hmi_apis::Common_ModuleType::eType, std::string>
-    module_types_str_mapping{
-        {hmi_apis::Common_ModuleType::eType::CLIMATE, {"CLIMATE"}},
-        {hmi_apis::Common_ModuleType::eType::RADIO, {"RADIO"}},
-        {hmi_apis::Common_ModuleType::eType::SEAT, {"SEAT"}},
-        {hmi_apis::Common_ModuleType::eType::AUDIO, {"AUDIO"}},
-        {hmi_apis::Common_ModuleType::eType::LIGHT, {"LIGHT"}},
-        {hmi_apis::Common_ModuleType::eType::HMI_SETTINGS, {"HMI_SETTINGS"}}};
 
 bool ResumptionRequestID::operator<(const ResumptionRequestID& other) const {
   return correlation_id < other.correlation_id ||
@@ -1117,7 +1109,7 @@ void ResumptionDataProcessorImpl::CheckModuleDataSubscription(
           response_module_data_so[message_params::kModuleType].asUInt());
 
   const auto responsed_module_type_str =
-      module_types_str_mapping[responsed_module_type_int];
+      application_manager::EnumToString(responsed_module_type_int);
 
   const auto response_module_id =
       response_module_data_so[message_params::kModuleId].asString();
