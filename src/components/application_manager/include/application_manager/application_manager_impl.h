404e8f97d75883010f5dd7380acba8c947d4f3dd
Yana Chernysheva (GitHub) <59469418+ychernysheva@users.noreply.github.com>
2021-01-15 20:50:31 +0200

    Fix for a potential SDL crash on boot  (#3581)
    * Fix for a potential SDL crash on boot
    
    One of the reasons why SDL is crashing on start is
    related to lazy initialization of the HMI/Mobile/v4
    API classes.
    These classes are initializated on demand when some
    of threads are trying to get the object of that class
    first time. Most likely, at SDL start might be a
    situation when HMI/Mobile/v4 API classes are
    constructing in the one thread and at that moment another
    thread is trying to get access to the inner data of
    not-yet-fully-constructed object and this causes a core
    crash with ~ISchemaItem() calls in stack trace. That is
    possible because access to these factories is not
    synchronized.
    
    To avoid such situation, factory creation was moved to
    AM ctor and all other sub components just use getters
    when they need to get access to API instances.
    
    * fixup! Fix for a potential SDL crash on boot
    
    Co-authored-by: Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
    Co-authored-by: LitvinenkoIra <ilytvynenko@luxoft.com>

--- src/components/application_manager/include/application_manager/application_manager_impl.h
+++ src/components/application_manager/include/application_manager/application_manager_impl.h
@@ -1181,6 +1181,9 @@ class ApplicationManagerImpl
   void ApplyFunctorForEachPlugin(
       std::function<void(plugin_manager::RPCPlugin&)> functor) OVERRIDE;
 
+  ns_smart_device_link_rpc::V1::v4_protocol_v1_2_no_extra&
+  mobile_v4_protocol_so_factory() OVERRIDE;
+
  private:
   /**
    * @brief Adds application to registered applications list and marks it as
@@ -1604,8 +1607,10 @@ class ApplicationManagerImpl
     mobile_apis::SystemContext::eType system_context;
   };
 
-  hmi_apis::HMI_API* hmi_so_factory_;
-  mobile_apis::MOBILE_API* mobile_so_factory_;
+  hmi_apis::HMI_API hmi_so_factory_;
+  mobile_apis::MOBILE_API mobile_so_factory_;
+  ns_smart_device_link_rpc::V1::v4_protocol_v1_2_no_extra
+      mobile_v4_protocol_so_factory_;
 
   static uint32_t mobile_corelation_id_;
   static uint32_t corelation_id_;
