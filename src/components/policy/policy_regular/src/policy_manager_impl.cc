daf95399a6a1f6b7b100fc563efca11019dcf0ec
collin <collin+i@collinmcqueen.com>
2021-05-12 11:32:54 -0400

    validate value[data].isArray()

--- src/components/policy/policy_regular/src/policy_manager_impl.cc
+++ src/components/policy/policy_regular/src/policy_manager_impl.cc
@@ -112,7 +112,7 @@ std::shared_ptr<policy_table::Table> PolicyManagerImpl::ParseArray(
 
   if (reader.parse(json, &value)) {
     // For PT Update received from SDL Server.
-    if (value.isObject() && 0 != value["data"].size()) {
+    if (value.isObject() && value["data"].isArray() && !value["data"].empty()) {
       Json::Value data = value["data"];
       return std::make_shared<policy_table::Table>(&data[0]);
     } else {
