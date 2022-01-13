7b7ce52112d2b6d8328df5b6022d06daf6cd7e99
OlhaVorobiova <olha.vorobiova@dxc.com>
2022-01-13 16:50:27 +0200

    Delete useless members in EventDispatcher

--- src/components/application_manager/include/application_manager/event_engine/event_dispatcher_impl.h
+++ src/components/application_manager/include/application_manager/event_engine/event_dispatcher_impl.h
@@ -68,12 +68,6 @@ class EventDispatcherImpl : public EventDispatcher {
   EventObserverMap get_observers() const {
     return observers_event_;
   }
-  MobileEventObserverMap get_mobile_observers() const {
-    return mobile_observers_event_;
-  }
-  ObserverVector get_observers_list() const {
-    return observers_;
-  }
 #endif  // BUILD_TESTS
 
   /*
@@ -82,9 +76,6 @@ class EventDispatcherImpl : public EventDispatcher {
    * @param event Received event
    */
   void raise_event(const Event& event) OVERRIDE;
-
-  void remove_observer(const Event::EventID& event_id,
-                       const int32_t hmi_correlation_id) OVERRIDE;
 
   /*
    * @brief Subscribe the observer to event
@@ -149,20 +140,6 @@ class EventDispatcherImpl : public EventDispatcher {
   void remove_mobile_observer(EventObserver& observer) OVERRIDE;
 
  private:
-  /*
-   * @brief removes observer
-   * when occurs unsubscribe from event
-   * @param observer to be removed
-   */
-  void remove_observer_from_vector(EventObserver& observer);
-
-  /*
-   * @brief removes observer
-   * when occurs unsubscribe from event
-   * @param observer to be removed
-   */
-  void remove_mobile_observer_from_vector(EventObserver& observer);
-
   DISALLOW_COPY_AND_ASSIGN(EventDispatcherImpl);
 
  private:
@@ -171,8 +148,6 @@ class EventDispatcherImpl : public EventDispatcher {
   sync_primitives::RecursiveLock mobile_observer_lock_;
   EventObserverMap observers_event_;
   MobileEventObserverMap mobile_observers_event_;
-  ObserverVector observers_;
-  ObserverVector mobile_observers_;
 };
 
 }  // namespace event_engine
