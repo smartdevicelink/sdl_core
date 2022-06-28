cc0ab6d75b7bef33864b8aadc1beeedc592e829e
Olha Vorobiova (GitHub) <86727408+OlhaVorobiova@users.noreply.github.com>
2022-06-28 15:16:45 +0200

    Added a lock in StartPTExchange (#3921)

--- src/components/policy/policy_regular/src/policy_manager_impl.cc
+++ src/components/policy/policy_regular/src/policy_manager_impl.cc
@@ -629,6 +629,9 @@ void PolicyManagerImpl::StartPTExchange() {
     return;
   }
 
+  sync_primitives::AutoLock policy_table_exchange_lock(
+      policy_table_exchange_lock_);
+
   if (update_status_manager_.IsUpdatePending() && update_required) {
     if (trigger_ptu_)
       update_status_manager_.ScheduleUpdate();
